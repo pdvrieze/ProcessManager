02a50dbac1f8ab6717c5ad31788dcc002d53f392
paul <paul@dbe36f43-c5b9-0310-9b8b-9d51405ec3fa>
2009-08-27 20:21:05 +0000

    Work on XML mapping for process models
    git-svn-id: file:///home/svn/programming/ProcessEngine@3016 dbe36f43-c5b9-0310-9b8b-9d51405ec3fa

--- ProcessEngine/src/nl/adaptivity/process/engine/ProcessModel.java
+++ ProcessEngine/src/nl/adaptivity/process/engine/ProcessModel.java
@@ -1,13 +1,14 @@
 package nl.adaptivity.process.engine;
 
+import java.io.CharArrayWriter;
+import java.io.File;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
+import java.util.*;
 
-import nl.adaptivity.process.engine.processModel.EndNode;
-import nl.adaptivity.process.engine.processModel.ProcessNode;
-import nl.adaptivity.process.engine.processModel.StartNode;
+import javax.xml.bind.JAXB;
+
+import nl.adaptivity.process.engine.processModel.*;
+import nl.adaptivity.process.processmodel.jaxb.*;
 
 
 public class ProcessModel implements Serializable{
@@ -28,8 +29,68 @@ public class ProcessModel implements Serializable{
   public ProcessModel(EndNode... pEndNodes) {
     this(Arrays.asList(pEndNodes));
   }
+  
+  public ProcessModel(XmlProcessModel pXmlModel) {
+    Map<String, ProcessNode> seenNodes = new HashMap<String, ProcessNode>();
+    Map<String,XmlProcessModelNode> unseenNodes = new HashMap<String, XmlProcessModelNode>();
+    Collection<XmlEndNode> endNodes = new ArrayList<XmlEndNode>();
+    
+    for(XmlProcessModelNode node:pXmlModel.getNodes()) {
+      unseenNodes.put(node.getId(), node);
+      if (node instanceof XmlEndNode) {
+        endNodes.add((XmlEndNode) node);
+      }
+    }
 
-  private Collection<StartNode> reverseGraph(Collection<EndNode> pEndNodes) {
+    Collection<EndNode> result = new ArrayList<EndNode>(endNodes.size());
+    aEndNodeCount = endNodes.size();
+    
+    for(XmlEndNode endNode:endNodes) {
+      XmlProcessModelNode pred = endNode.getPredecessor();
+      ProcessNode predNode = toProcessNode(seenNodes, pred);
+      result.add(new EndNode(predNode));
+    }
+    
+    aStartNodes = reverseGraph(result);
+  }
+
+  private static ProcessNode toProcessNode(Map<String, ProcessNode> pSeenNodes, XmlProcessModelNode node) {
+    ProcessNode result = pSeenNodes.get(node.getId());
+    if (result != null) { return result; }
+    if (node instanceof XmlStartNode) {
+      result = new StartNode();
+    } else if (node instanceof XmlActivity) {
+      result = toProcessNode2(pSeenNodes, (XmlActivity) node);
+    } else if (node instanceof XmlJoin) {
+      result = toProcessNode2(pSeenNodes, (XmlJoin) node);
+    }
+    pSeenNodes.put(node.getId(), result);
+    return result;
+  }
+
+  private static Join toProcessNode2(Map<String, ProcessNode> pSeenNodes, XmlJoin pNode) {
+    // TODO Auto-generated method stub
+    // return null;
+    throw new UnsupportedOperationException("Not yet implemented");
+    
+  }
+
+  private static Activity toProcessNode2(Map<String, ProcessNode> pSeenNodes, XmlActivity pNode) {
+    ProcessNode predecessor = toProcessNode(pSeenNodes, pNode.getPredecessor());
+    String name = pNode.getName();
+    String condition = pNode.getCondition();
+    List<XmlImportType> imports = pNode.getImports();
+    List<XmlExportType> exports = pNode.getExports();
+    
+    final Activity result = new Activity(predecessor);
+    result.setName(name);
+    result.setCondition(condition);
+    result.setImports(imports);
+    result.setExports(exports);
+    return result;
+  }
+
+  private static Collection<StartNode> reverseGraph(Collection<EndNode> pEndNodes) {
     
     Collection<StartNode> resultList = new ArrayList<StartNode>();
     for (EndNode endNode:pEndNodes) {
@@ -38,7 +99,7 @@ public class ProcessModel implements Serializable{
     return resultList;
   }
 
-  private void reverseGraph(Collection<StartNode> pResultList, ProcessNode pNode) {
+  private static void reverseGraph(Collection<StartNode> pResultList, ProcessNode pNode) {
     Collection<ProcessNode> previous = pNode.getPrevious();
     for(ProcessNode prev: previous) {
       if (prev instanceof StartNode) {
@@ -67,5 +128,16 @@ public class ProcessModel implements Serializable{
     return aEndNodeCount;
   }
   
+  public static void main(String[] pArgs) {
+    if (pArgs.length!=1) {
+      System.out.println("Give process model file as parameter");
+      System.exit(1);
+    }
+    XmlProcessModel pm = JAXB.unmarshal(new File(pArgs[0]), XmlProcessModel.class);
+    CharArrayWriter out = new CharArrayWriter();
+    JAXB.marshal(pm, out);
+    System.out.println(out.toString());
+    System.out.println(pm.toString());
+  }
 
 }
--- ProcessEngine/src/nl/adaptivity/process/engine/processModel/Activity.java
+++ ProcessEngine/src/nl/adaptivity/process/engine/processModel/Activity.java
@@ -1,12 +1,79 @@
 package nl.adaptivity.process.engine.processModel;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
-public abstract class Activity extends ProcessNode{
+import nl.adaptivity.process.engine.ProcessInstance;
+import nl.adaptivity.process.processmodel.jaxb.XmlExportType;
+import nl.adaptivity.process.processmodel.jaxb.XmlImportType;
 
-  protected Activity(ProcessNode pPrevious) {
+
+public class Activity extends ProcessNode{
+
+  private String aName;
+  private String aCondition;
+  private List<XmlImportType> aImports;
+  private List<XmlExportType> aExports;
+
+  
+  public Activity(ProcessNode pPrevious) {
     super(pPrevious);
   }
 
   private static final long serialVersionUID = 282944120294737322L;
+
+  @Override
+  public boolean condition() {
+    // TODO Auto-generated method stub
+    // return false;
+    throw new UnsupportedOperationException("Not yet implemented");
+    
+  }
+
+  @Override
+  public void start(Collection<ProcessNodeInstance> pThreads, ProcessInstance pProcessInstance, ProcessNodeInstance pPredecessor) {
+    // TODO Auto-generated method stub
+    // 
+    throw new UnsupportedOperationException("Not yet implemented");
+    
+  }
+
+  public String getName() {
+    return aName;
+  }
+
+  public void setName(String pName) {
+    aName = pName;
+  }
+
+  public void setCondition(String pCondition) {
+    aCondition = pCondition;
+  }
+
+  public void setImports(List<XmlImportType> pImports) {
+    aImports = new ArrayList<XmlImportType>(pImports.size());
+    aImports.addAll(pImports);
+  }
+
+  public void setExports(List<XmlExportType> pExports) {
+    aExports = new ArrayList<XmlExportType>(pExports.size());
+    aExports.addAll(pExports);
+  }
+
+  
+  public String getCondition() {
+    return aCondition;
+  }
+
+  
+  public List<XmlImportType> getImports() {
+    return aImports;
+  }
+
+  
+  public List<XmlExportType> getExports() {
+    return aExports;
+  }
 
 }
