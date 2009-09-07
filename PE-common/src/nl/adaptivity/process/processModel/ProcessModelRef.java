94c58365ad3e52e123ab4516396c2b2ee80dec18
paul <paul@dbe36f43-c5b9-0310-9b8b-9d51405ec3fa>
2009-09-07 20:49:07 +0000

    Make the retrieving of the process model list work using both firefox, and the client
    git-svn-id: file:///home/svn/programming/PE-common@3043 dbe36f43-c5b9-0310-9b8b-9d51405ec3fa

---
+++ PE-common/src/nl/adaptivity/process/processModel/ProcessModelRef.java
@@ -0,0 +1,37 @@
+package nl.adaptivity.process.processModel;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+
+@XmlAccessorType(XmlAccessType.NONE)
+public class ProcessModelRef {
+
+  private String aName;
+  private long aHandle;
+  
+  public ProcessModelRef() {}
+  
+  public ProcessModelRef(String pName, long pHandle) {
+    aName = pName;
+    aHandle = pHandle;
+  }
+
+  void setName(String name) {
+    aName = name;
+  }
+  
+  @XmlAttribute(required=true)
+  public String getName() {
+    return aName;
+  }
+  
+  void setHandle(long handle) {
+    aHandle = handle;
+  }
+  
+  @XmlAttribute(required=true)
+  public long getHandle() {
+    return aHandle;
+  }
+}
--- PE-common/src/nl/adaptivity/process/util/Constants.java
+++ PE-common/src/nl/adaptivity/process/util/Constants.java
@@ -1,8 +1,15 @@
 package nl.adaptivity.process.util;
 
+import java.net.URI;
+
+import javax.xml.namespace.QName;
+
 
 public final class Constants {
   
+  public static final QName PESERVICE = new QName("http://adaptivity.nl/ProcessEngine/","ProcessEngine");
+  public static final URI WSDL_MEP_IN_OUT = URI.create("http://www.w3.org/2004/08/wsdl/in-out");
+
   private Constants() {
     
   }
