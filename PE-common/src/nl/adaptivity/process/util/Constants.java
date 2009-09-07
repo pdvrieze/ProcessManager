f4517cbeaee052a2c5fb51f183e6997b8b020b42
paul <paul@dbe36f43-c5b9-0310-9b8b-9d51405ec3fa>
2009-09-07 18:32:52 +0000

    Split up test and common library to resolve cycles
    git-svn-id: file:///home/svn/programming/PE-common@3042 dbe36f43-c5b9-0310-9b8b-9d51405ec3fa

---
+++ PE-common/src/nl/adaptivity/process/util/Constants.java
@@ -0,0 +1,12 @@
+package nl.adaptivity.process.util;
+
+
+public final class Constants {
+  
+  private Constants() {
+    
+  }
+  
+  public static final String PROTOCOL_HEADERS = "javax.jbi.messaging.protocol.headers";
+
+}
