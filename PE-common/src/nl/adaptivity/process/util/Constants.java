19cf1932408b56193d105c6906b8d75e50880038
paul <paul@dbe36f43-c5b9-0310-9b8b-9d51405ec3fa>
2009-09-14 14:49:54 +0000

    Add constant for IN_ONLY MEP
    git-svn-id: file:///home/svn/programming/PE-common@3078 dbe36f43-c5b9-0310-9b8b-9d51405ec3fa

--- PE-common/src/nl/adaptivity/process/util/Constants.java
+++ PE-common/src/nl/adaptivity/process/util/Constants.java
@@ -9,11 +9,12 @@ public final class Constants {
   
   public static final QName PESERVICE = new QName("http://adaptivity.nl/ProcessEngine/","ProcessEngine");
   public static final URI WSDL_MEP_IN_OUT = URI.create("http://www.w3.org/2004/08/wsdl/in-out");
+  public static final Object WSDL_MEP_IN_ONLY = URI.create("http://www.w3.org/2004/08/wsdl/in-only");
+  
+  public static final String PROTOCOL_HEADERS = "javax.jbi.messaging.protocol.headers";
 
   private Constants() {
     
   }
-  
-  public static final String PROTOCOL_HEADERS = "javax.jbi.messaging.protocol.headers";
 
 }
