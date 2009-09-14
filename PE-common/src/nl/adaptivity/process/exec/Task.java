5d5f4c9c40092710622349522f6a8b9668a4f195
paul <paul@dbe36f43-c5b9-0310-9b8b-9d51405ec3fa>
2009-09-14 14:48:40 +0000

    Add a generic interface for interaction on process tasks
    git-svn-id: file:///home/svn/programming/PE-common@3076 dbe36f43-c5b9-0310-9b8b-9d51405ec3fa

---
+++ PE-common/src/nl/adaptivity/process/exec/Task.java
@@ -0,0 +1,19 @@
+package nl.adaptivity.process.exec;
+
+import net.devrieze.util.HandleMap.HandleAware;
+
+
+public interface Task extends HandleAware{
+  
+  
+  public enum TaskState {
+    Available,
+    Taken,
+    Complete;
+  }
+
+  public TaskState getState();
+  
+  public void setState(TaskState aNewState);
+
+}
