a518c88a94cab47cbf58135c6686e99f541c167e
paul <paul@dbe36f43-c5b9-0310-9b8b-9d51405ec3fa>
2009-09-16 18:43:05 +0000

    Move REST annotations to common library
    git-svn-id: file:///home/svn/programming/PE-common@3104 dbe36f43-c5b9-0310-9b8b-9d51405ec3fa

---
+++ PE-common/src/nl/adaptivity/rest/annotations/RestMethod.java
@@ -0,0 +1,37 @@
+package nl.adaptivity.rest.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+public @interface RestMethod {
+
+  
+  public enum HttpMethod {
+    GET,
+    POST,
+    PUT,
+    DELETE
+  }
+
+  HttpMethod method();
+
+  String path();
+
+  /**
+   * Expressions that put conditions on post parameters
+   */
+  String[] post() default {};
+  /**
+   * Expressions that put conditions on get parameters
+   */
+  String[] get() default {};
+  /**
+   * Expressions that put conditions on any request parameters (post or get)
+   */
+  String[] query() default {};
+
+}
---
+++ PE-common/src/nl/adaptivity/rest/annotations/RestParam.java
@@ -0,0 +1,27 @@
+package nl.adaptivity.rest.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.PARAMETER, ElementType.LOCAL_VARIABLE})
+public @interface RestParam {
+
+  
+  public enum ParamType {
+    QUERY,
+    POST,
+    GET,
+    XPATH,
+    VAR
+  }
+
+  String name() default "";
+  
+  ParamType type() default ParamType.QUERY;
+  
+  String xpath() default ""; 
+
+}
