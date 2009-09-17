39c285ea168925927bf47a8e58ec1e52b99604d6
paul <paul@dbe36f43-c5b9-0310-9b8b-9d51405ec3fa>
2009-09-17 20:11:16 +0000

    Add a nice Map interface exposing the attachments in a normalized message (without exposing everything else as well)
    git-svn-id: file:///home/svn/programming/PE-common@3115 dbe36f43-c5b9-0310-9b8b-9d51405ec3fa

---
+++ PE-common/src/nl/adaptivity/jbi/util/AttachmentMap.java
@@ -0,0 +1,154 @@
+package nl.adaptivity.jbi.util;
+
+import java.util.*;
+
+import javax.activation.DataHandler;
+import javax.jbi.messaging.MessagingException;
+import javax.jbi.messaging.NormalizedMessage;
+
+
+public class AttachmentMap extends AbstractMap<String, DataHandler> {
+
+  
+  private class Entry implements Map.Entry<String, DataHandler>{
+
+    private final String aKey;
+
+    public Entry(String pKey) {
+      aKey = pKey;
+    }
+
+    @Override
+    public String getKey() {
+      return aKey;
+    }
+
+    @Override
+    public DataHandler getValue() {
+      return aMessage.getAttachment(aKey);
+    }
+
+    @Override
+    public DataHandler setValue(DataHandler pValue) {
+      DataHandler result = aMessage.getAttachment(aKey);
+      try {
+        aMessage.addAttachment(aKey, pValue);
+      } catch (MessagingException e) {
+        throw new UnsupportedOperationException(e);
+      }
+      return result;
+    }
+    
+  }
+  
+  private class EntryIterator implements Iterator<Map.Entry<String, DataHandler>> {
+    
+    private final Iterator<String> aBackingIterator;
+
+    @SuppressWarnings("unchecked")
+    public EntryIterator() {
+      aBackingIterator = aMessage.getAttachmentNames().iterator();
+    }
+    
+    @Override
+    public boolean hasNext() {
+      return aBackingIterator.hasNext();
+    }
+
+    @Override
+    public Entry next() {
+      
+      final String next = aBackingIterator.next();
+      Entry result = new Entry(next);
+      return result ;
+    }
+
+    @Override
+    public void remove() {
+      aBackingIterator.remove();
+    }
+
+  }
+
+  private class EntrySet extends AbstractSet<Map.Entry<String, DataHandler>> {
+
+    @Override
+    public boolean remove(Object pO) {
+      if (pO instanceof Map.Entry) {
+        Map.Entry<?, ?> me = (Map.Entry<?, ?>) pO;
+        return AttachmentMap.this.remove(me.getKey())!=null;
+      }
+      return false;
+    }
+
+    private int aSize = -1;
+
+    @Override
+    public Iterator<Map.Entry<String, DataHandler>> iterator() {
+      return new EntryIterator();
+    }
+
+    @Override
+    public int size() {
+      if (aSize <0) {
+        aSize = aMessage.getAttachmentNames().size(); 
+      }
+      return aSize;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> pC) {
+      boolean result = false;
+      for(Object o:pC) {
+        result |=AttachmentMap.this.remove(o)!=null;
+      }
+      return result;
+    }
+
+  }
+
+  private final NormalizedMessage aMessage;
+
+  public AttachmentMap(NormalizedMessage pMessage) {
+    aMessage = pMessage;
+  }
+
+  @Override
+  public Set<java.util.Map.Entry<String, DataHandler>> entrySet() {
+    return new EntrySet();
+  }
+
+  @Override
+  public boolean containsKey(Object pKey) {
+    return keySet().contains(pKey);
+  }
+
+  @Override
+  public DataHandler get(Object pKey) {
+    if (pKey instanceof String) {
+      return aMessage.getAttachment((String) pKey);
+    }
+    return null;
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public Set<String> keySet() {
+    return aMessage.getAttachmentNames();
+  }
+
+  @Override
+  public DataHandler remove(Object pKey) {
+    if (pKey instanceof String) {
+      DataHandler old = aMessage.getAttachment((String)pKey);
+      try {
+        aMessage.removeAttachment((String) pKey);
+      } catch (MessagingException e) {
+        throw new UnsupportedOperationException(e);
+      }
+      return old;
+    }
+    return null;
+  }
+
+}
