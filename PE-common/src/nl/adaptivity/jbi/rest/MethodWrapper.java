90214d072a1c7df5d2945fb59cd1bdacc9e63566
paul <paul@dbe36f43-c5b9-0310-9b8b-9d51405ec3fa>
2009-09-17 19:19:49 +0000

    Centralize Rest handling. Other components may need it as well.
    git-svn-id: file:///home/svn/programming/PE-common@3112 dbe36f43-c5b9-0310-9b8b-9d51405ec3fa

---
+++ PE-common/src/nl/adaptivity/jbi/rest/MethodWrapper.java
@@ -0,0 +1,302 @@
+package nl.adaptivity.jbi.rest;
+
+import java.io.CharArrayReader;
+import java.io.OutputStreamWriter;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.*;
+import java.util.Collection;
+import java.util.Map;
+
+import javax.activation.DataHandler;
+import javax.jbi.messaging.MessagingException;
+import javax.jbi.messaging.NormalizedMessage;
+import javax.xml.XMLConstants;
+import javax.xml.bind.JAXB;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSeeAlso;
+import javax.xml.bind.util.JAXBSource;
+import javax.xml.namespace.QName;
+import javax.xml.transform.Source;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import net.devrieze.util.*;
+
+import nl.adaptivity.rest.annotations.RestParam;
+import nl.adaptivity.rest.annotations.RestParam.ParamType;
+import nl.adaptivity.util.HttpMessage;
+import nl.adaptivity.util.HttpMessage.Body;
+
+
+public class MethodWrapper {
+
+  private Map<String, String> aPathParams;
+  private final Object aOwner;
+  private final Method aMethod;
+  private Object[] aParams;
+  private Object aResult;
+
+  public MethodWrapper(Object pOwner, Method pMethod) {
+    aOwner = pOwner;
+    aMethod = pMethod;
+  }
+
+  public void setPathParams(Map<String, String> pPathParams) {
+    aPathParams = pPathParams;
+  }
+
+  public void unmarshalParams(HttpMessage pHttpMessage) {
+    if (aParams!=null) {
+      throw new IllegalStateException("Parameters have already been unmarshalled");
+    }
+    Class<?>[] parameterTypes = aMethod.getParameterTypes();
+    Annotation[][] parameterAnnotations = aMethod.getParameterAnnotations();
+    int argCnt = 0;
+    aParams = new Object[parameterTypes.length];
+    
+    for(int i =0; i<parameterTypes.length; ++i) {
+      RestParam annotation = Annotations.getAnnotation(parameterAnnotations[i], RestParam.class);
+      String name;
+      ParamType type;
+      String xpath;
+      if (annotation==null) {
+        name = "arg"+Integer.toString(argCnt);
+        type = ParamType.QUERY;
+        xpath = null;
+      } else {
+        name = annotation.name();
+        type = annotation.type();
+        xpath = annotation.xpath();
+      }
+
+      aParams[i] = getParam(parameterTypes[i], name, type, xpath, pHttpMessage);
+      
+    }
+  }
+
+  private Object getParam(Class<?> pClass, String pName, ParamType pType, String pXpath, HttpMessage pMessage) {
+    Object result = null;
+    switch (pType) {
+      case GET:
+        result = getParamGet(pName, pMessage);
+        break;
+      case POST:
+        result = getParamPost(pName, pMessage);
+        break;
+      case QUERY:
+        result = getParamGet(pName, pMessage);
+        if (result==null) {
+          result = getParamPost(pName, pMessage);
+        }
+        break;
+      case VAR:
+        result = aPathParams.get(pName);
+        break;
+      case XPATH:
+        result = getParamXPath(pClass, pXpath, pMessage.getBody());
+        break;
+    }
+    if (result != null && (! pClass.isInstance(result))) {
+      result = JAXB.unmarshal(new CharArrayReader(result.toString().toCharArray()), pClass);
+    }
+    
+    return result;
+  }
+
+  private Object getParamGet(String pName, HttpMessage pMessage) {
+    return pMessage.getQuery(pName);
+  }
+
+  private Object getParamPost(String pName, HttpMessage pMessage) {
+    return pMessage.getPost(pName);
+  }
+
+  private <T> T getParamXPath(Class<T> pClass, String pXpath, Body pBody) {
+    boolean jaxb;
+    if (CharSequence.class.isAssignableFrom(pClass)) {
+      jaxb = false;
+    } else {
+      jaxb = true;
+    }
+    Node match;
+    for (Node n: pBody.getElements()) {
+      match = xpathMatch(n, pXpath);
+      if (match !=null) {
+        if (jaxb) {
+          return JAXB.unmarshal(new DOMSource(match), pClass);
+        } else {
+          return pClass.cast(nodeToString(match));
+        }
+      }
+    }
+    // TODO Auto-generated method stub
+    // return null;
+    throw new UnsupportedOperationException("Not yet implemented");
+    
+  }
+
+  private String nodeToString(Node pNode) {
+    return pNode.getTextContent();
+  }
+
+  private Node xpathMatch(Node pN, String pXpath) {
+    XPathFactory factory = XPathFactory.newInstance();
+    XPath xpath = factory.newXPath();
+    NodeList result;
+    try {
+      result = (NodeList) xpath.evaluate(pXpath, new DOMSource(pN), XPathConstants.NODESET);
+    } catch (XPathExpressionException e) {
+      return null;
+    }
+    if (result==null || result.getLength()==0) {
+      return null;
+    }
+    return result.item(0);
+  }
+
+  public void exec() throws MessagingException {
+    if (aParams==null) {
+      throw new IllegalArgumentException("Argument unmarshalling has not taken place yet");
+    }
+    try {
+      aResult = aMethod.invoke(aOwner, aParams);
+    } catch (IllegalArgumentException e) {
+      throw new MessagingException(e);
+    } catch (IllegalAccessException e) {
+      throw new MessagingException(e);
+    } catch (InvocationTargetException e) {
+      throw new MessagingException(e);
+    }
+  }
+
+  public void marshalResult(NormalizedMessage pReply) throws MessagingException {
+    XmlRootElement xmlRootElement = aResult==null ? null : aResult.getClass().getAnnotation(XmlRootElement.class);
+    if (xmlRootElement!=null) {
+      try {
+        JAXBContext jaxbContext = JAXBContext.newInstance(aMethod.getReturnType());
+        pReply.setContent(new JAXBSource(jaxbContext, aResult));
+      } catch (JAXBException e) {
+        throw new MessagingException(e);
+      }
+    } else if (aResult instanceof Source) {
+      pReply.setContent((Source) aResult);
+    } else if (aResult instanceof Node){
+      pReply.setContent(new DOMSource((Node) aResult));
+    } else if (aResult instanceof Collection) {
+      XmlElementWrapper annotation = aMethod.getAnnotation(XmlElementWrapper.class);
+      if (annotation!=null) {
+        pReply.setContent(collectionToSource(aMethod.getGenericReturnType(),(Collection<?>) aResult, getQName(annotation)));
+//       
+//        
+//        Collection<?> value = (Collection<?>) aResult;
+//        Collection<JAXBElement<String>> value2 = new ArrayDeque<JAXBElement<String>>();
+//        value2.add(new JAXBElement<String>(new QName("test"), String.class, "value1"));
+//        value2.add(new JAXBElement<String>(new QName("test"), String.class, "value2"));
+//        @SuppressWarnings("unchecked") Class<Collection<?>> declaredType = ((Class) aResult.getClass());
+//        QName name = getQName(annotation);
+//        
+//        
+//        JAXBElement<?> element = new JAXBElement<Collection<?>>(name, declaredType, value2);
+//        
+//        element = (new JAXBCollectionWrapper((Collection<?>) aResult)).getJAXBElement(name);
+//        
+//        try {
+//          JAXBContext jaxbContext = newJAXBContext(JAXBCollectionWrapper.class, aResult.getClass());
+////          jaxbContext.createMarshaller().marshal(element, System.err);
+//          pReply.setContent(new JAXBSource(jaxbContext, element));
+//        } catch (JAXBException e) {
+//          throw new MessagingException(e);
+//        }
+        
+      }
+    } else if (aResult instanceof CharSequence) {
+      pReply.addAttachment("text", new DataHandler(new StringDataSource("text", "text/plain", aResult.toString())));
+    } else {
+      if (aResult !=null) {
+        try {
+          JAXBContext jaxbContext = JAXBContext.newInstance(aMethod.getReturnType());
+          pReply.setContent(new JAXBSource(jaxbContext, aResult));
+        } catch (JAXBException e) {
+          throw new MessagingException(e);
+        }
+      }
+    }
+  }
+
+  private Source collectionToSource(Type pReturnType, Collection<?> pResult, QName pName) throws MessagingException {
+    final Class<?> rawType;
+    if (pReturnType instanceof ParameterizedType) {
+      ParameterizedType returnType = (ParameterizedType) pReturnType;
+      rawType = (Class<?>) returnType.getRawType();
+    } else if (pReturnType instanceof Class<?>) {
+      rawType = (Class<?>) pReturnType;
+    } else if (pReturnType instanceof WildcardType) {
+      final Type[] UpperBounds = ((WildcardType) pReturnType).getUpperBounds();
+      if (UpperBounds.length>0) {
+        rawType = (Class<?>) UpperBounds[0];
+      } else {
+        rawType = Object.class;
+      }
+    } else if (pReturnType instanceof TypeVariable) {
+      final Type[] UpperBounds = ((TypeVariable<?>) pReturnType).getBounds();
+      if (UpperBounds.length>0) {
+        rawType = (Class<?>) UpperBounds[0];
+      } else {
+        rawType = Object.class;
+      }
+    } else {
+      throw new IllegalArgumentException("Unsupported type variable");
+    }
+    Class<?> elementType = null;
+    if (Collection.class.isAssignableFrom(rawType)) {
+      Type[] paramTypes = Types.getTypeParametersFor(Collection.class, pReturnType);
+      elementType = (Class<?>) paramTypes[0];
+      if (elementType.isInterface()) {
+        // interfaces not supported by jaxb
+        elementType = Types.commonAncestor(pResult);
+      }
+    } else {
+      elementType = Types.commonAncestor(pResult);
+    }
+    try {
+      JAXBContext context = newJAXBContext(JAXBCollectionWrapper.class, elementType);
+      return new JAXBSource(context, new JAXBCollectionWrapper(pResult).getJAXBElement(pName));
+    } catch (JAXBException e) {
+      throw new MessagingException(e);
+    }
+  }
+
+  private JAXBContext newJAXBContext(Class<?>...pClasses) throws JAXBException {
+    Class<?>[] classList;
+    Class<?> clazz = aMethod.getDeclaringClass();
+    XmlSeeAlso seeAlso = clazz.getAnnotation(XmlSeeAlso.class);
+    if (seeAlso!=null && seeAlso.value().length>0) {
+      final Class<?>[] seeAlsoClasses = seeAlso.value();
+      classList = new Class<?>[seeAlsoClasses.length+pClasses.length];
+      System.arraycopy(seeAlsoClasses, 0, classList, 0, seeAlsoClasses.length);
+      System.arraycopy(pClasses, 0, classList, seeAlsoClasses.length, pClasses.length);
+    } else {
+      classList = pClasses;
+    }
+    return JAXBContext.newInstance(classList);
+  }
+
+  private static QName getQName(XmlElementWrapper pAnnotation) {
+    String nameSpace = pAnnotation.namespace();
+    if ("##default".equals(nameSpace)) {
+      nameSpace = XMLConstants.NULL_NS_URI;
+    }
+    String localName = pAnnotation.name();
+    return new QName(nameSpace, localName, XMLConstants.DEFAULT_NS_PREFIX);
+  }
+
+}
---
+++ PE-common/src/nl/adaptivity/jbi/rest/RestMessageHandler.java
@@ -0,0 +1,243 @@
+package nl.adaptivity.jbi.rest;
+
+import java.lang.reflect.Method;
+import java.util.*;
+
+import javax.jbi.messaging.MessagingException;
+import javax.jbi.messaging.NormalizedMessage;
+import javax.xml.bind.JAXB;
+
+import net.devrieze.util.PrefixMap;
+
+import nl.adaptivity.rest.annotations.RestMethod;
+import nl.adaptivity.rest.annotations.RestMethod.HttpMethod;
+import nl.adaptivity.util.HttpMessage;
+
+
+public class RestMessageHandler {
+
+  private static RestMessageHandler aInstance;
+  
+  private Map<Class<?>,EnumMap<HttpMethod, PrefixMap<Method>>> cache;
+  
+
+  public static RestMessageHandler newInstance() {
+    if (aInstance == null) {
+      aInstance = new RestMessageHandler();
+    }
+    return aInstance;
+  }
+  
+  private RestMessageHandler() {}
+
+  public boolean processRequest(HttpMethod operation, NormalizedMessage message, NormalizedMessage reply, Object target) throws MessagingException{
+    HttpMessage httpMessage = JAXB.unmarshal(message.getContent(),HttpMessage.class);
+
+    MethodWrapper method = getMethodFor(operation, httpMessage, target);
+    
+    if (method !=null) {
+      method.unmarshalParams(httpMessage);
+      method.exec();
+      if (reply!=null) {
+        method.marshalResult(reply);
+      }
+      return true;
+    }
+    return false;
+  }
+
+  private MethodWrapper getMethodFor(HttpMethod pHttpMethod, HttpMessage httpMessage, Object target) {
+//    final Method[] candidates = target.getClass().getDeclaredMethods();
+    Collection<Method> candidates = getCandidatesFor(target.getClass(), pHttpMethod, httpMessage.getPathInfo());
+    for(Method candidate:candidates) {
+      RestMethod annotation = candidate.getAnnotation(RestMethod.class);
+      Map<String, String> pathParams = new HashMap<String, String>();
+
+      if (annotation !=null && 
+          annotation.method()==pHttpMethod && 
+          pathFits(pathParams, annotation.path(), httpMessage.getPathInfo()) &&
+          conditionsSatisfied(annotation.get(), annotation.post(), annotation.query(), httpMessage)) {
+        MethodWrapper result = new MethodWrapper(target, candidate);
+        result.setPathParams(pathParams);
+        return result;
+      }
+      
+    }
+    return null;
+  }
+
+
+  private Collection<Method> getCandidatesFor(Class<? extends Object> pClass, HttpMethod pHttpMethod, String pPathInfo) {
+    if (cache == null) { cache = new HashMap<Class<?>, EnumMap<HttpMethod,PrefixMap<Method>>>(); }
+    EnumMap<HttpMethod,PrefixMap<Method>> v = cache.get(pClass);
+    if (v==null) {
+      v = createCacheElem(pClass);
+      cache.put(pClass, v);
+    }
+    PrefixMap<Method> w = v.get(pHttpMethod);
+    if (w == null) { return Collections.emptyList(); }
+    
+    return w.getValues(pPathInfo);
+  }
+
+  private EnumMap<HttpMethod, PrefixMap<Method>> createCacheElem(Class<? extends Object> pClass) {
+    EnumMap<HttpMethod, PrefixMap<Method>> result = new EnumMap<HttpMethod, PrefixMap<Method>>(HttpMethod.class);
+    final Method[] methods = pClass.getDeclaredMethods();
+    
+    for(Method m: methods) {
+      RestMethod annotation = m.getAnnotation(RestMethod.class);
+      if (annotation != null) {
+        String prefix = getPrefix(annotation.path());
+        HttpMethod operation = annotation.method();
+        PrefixMap<Method> x = result.get(operation);
+        if (x==null) {
+          x = new PrefixMap<Method>();
+          result.put(operation, x);
+        }
+        x.put(prefix, m);
+      }
+    }
+    return result;
+  }
+
+  private String getPrefix(String pPath) {
+    int i =pPath.indexOf('$');
+    int j = 0;
+    StringBuilder result = null;
+    while (i>=0) {
+      if (i+1<pPath.length() && pPath.charAt(i+1)=='$') {
+        if(result==null) { result = new StringBuilder(); }
+        result.append(pPath.substring(j, i+1));
+        j = i+2;
+        i = pPath.indexOf('$', i+2);
+      } else {
+        return pPath.substring(0, i);
+      }
+    }
+    if (result!=null) {
+      if (j+1<pPath.length()) {
+        result.append(pPath.substring(j));
+      }
+      return result.toString();
+    }
+    return pPath;
+  }
+
+  private static boolean conditionsSatisfied(String[] pGet, String[] pPost, String[] pQuery, HttpMessage pHttpMessage) {
+    for (String condition: pGet) {
+      if (! conditionGetSatisfied(condition, pHttpMessage)) {
+        return false;
+      }
+    }
+    for (String condition: pPost) {
+      if (! conditionPostSatisfied(condition, pHttpMessage)) {
+        return false;
+      }
+    }
+    for (String condition: pQuery) {
+      if (! conditionParamSatisfied(condition, pHttpMessage)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private static boolean conditionGetSatisfied(String pCondition, HttpMessage pHttpMessage) {
+    int i = pCondition.indexOf('=');
+    String param;
+    String value;
+    if (i>0) {
+      param = pCondition.substring(0, i);
+      value = pCondition.substring(i+1);
+    } else {
+      param = pCondition;
+      value=null;
+    }
+    String val = pHttpMessage.getQuery(param);
+    return (val != null) && (value == null || value.equals(val));
+  }
+
+  private static boolean conditionPostSatisfied(String pCondition, HttpMessage pHttpMessage) {
+    int i = pCondition.indexOf('=');
+    String param;
+    String value;
+    if (i>0) {
+      param = pCondition.substring(0, i);
+      value = pCondition.substring(i+1);
+    } else {
+      param = pCondition;
+      value=null;
+    }
+    String val = pHttpMessage.getPost(param);
+    return (val != null) && (value == null || value.equals(val));
+  }
+
+  private static boolean conditionParamSatisfied(String pCondition, HttpMessage pHttpMessage) {
+    int i = pCondition.indexOf('=');
+    String param;
+    String value;
+    if (i>0) {
+      param = pCondition.substring(0, i);
+      value = pCondition.substring(i+1);
+    } else {
+      param = pCondition;
+      value=null;
+    }
+    String val = pHttpMessage.getParam(param);
+    return (val != null) && (value == null || value.equals(val));
+  }
+
+  private static boolean pathFits(Map<String, String> pParamMatch, String pPath, String pPathInfo) {
+    int i=0;
+    int j=0;
+    while (i<pPath.length()) {
+      if (j>=pPathInfo.length()) {
+        return false;
+      }
+      char c = pPath.charAt(i);
+      if (c=='$' && pPath.length()>(i+1) && pPath.charAt(i+1)=='{') {
+        i += 2;
+        int k=i;
+        while (i<pPath.length() && pPath.charAt(i)!='}') {
+          ++i;
+        }
+        if (i>=pPath.length()) {
+          // Not valid parameter, treat like no parameter
+          i-=2;
+          if (c!=pPathInfo.charAt(j)) {
+            return false;
+          }
+        } else {
+          final String paramName = pPath.substring(k, i);
+          final String paramValue;
+          if (pPath.length()>(i+1)) {
+            char delim = pPath.charAt(i+1);
+            int l = j;
+            while (j<pPathInfo.length() && pPathInfo.charAt(j)!=delim) {
+              ++j;
+            }
+            if (j>=pPathInfo.length()) {
+              return false;
+            }
+            paramValue = pPathInfo.substring(l, j);
+          } else {
+            paramValue = pPathInfo.substring(j);
+          }
+          pParamMatch.put(paramName, paramValue);
+        }
+      } else {
+        if (c!=pPathInfo.charAt(j)) {
+          return false;
+        }
+        if (c=='$' && pPath.length()>(i+1) && pPath.charAt(i+1)=='$') {
+          ++i;
+        }
+      }
+      ++i;
+      ++j;
+    }
+    return j>=pPathInfo.length();
+  }
+  
+  
+}
