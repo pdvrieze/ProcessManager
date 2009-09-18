29eff9e6176de3c8a7050351c43a4dd5729b718d
paul <paul@dbe36f43-c5b9-0310-9b8b-9d51405ec3fa>
2009-09-18 20:02:58 +0000

    Make Task.TaskState actually serializable by giving it an @XmlRootElement annotation
    git-svn-id: file:///home/svn/programming/PE-common@3118 dbe36f43-c5b9-0310-9b8b-9d51405ec3fa

--- PE-common/src/nl/adaptivity/process/exec/Task.java
+++ PE-common/src/nl/adaptivity/process/exec/Task.java
@@ -1,10 +1,12 @@
 package nl.adaptivity.process.exec;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
 import net.devrieze.util.HandleMap.HandleAware;
 
 public interface Task extends HandleAware{
   
-  
+  @XmlRootElement(name="taskState", namespace="http:://adaptivity.nl/userMessageHandler")
   public static enum TaskState {
     Available,
     Taken,
