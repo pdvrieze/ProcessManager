31b879c8e8dad152b81dba4d4eea9a80d939799d
Paul de Vrieze <paul.devrieze@gmail.com>
2012-08-28 21:29:11 +0000

    Start simple authenticator for darwin.
    git-svn-id: https://darwin.bournemouth.ac.uk/pdvrieze/svn/darwin/projects/darwinrealm@156 3bae1aa3-1c52-4c45-a669-1fb9b31e7360

--- DarwinRealm/src/uk/ac/bournemouth/darwin/catalina/authenticator/DarwinAuthenticator.java
+++ DarwinRealm/src/uk/ac/bournemouth/darwin/catalina/authenticator/DarwinAuthenticator.java
@@ -1,23 +1,51 @@
 package uk.ac.bournemouth.darwin.catalina.authenticator;
 
 import java.io.IOException;
+import java.security.Principal;
+
+import javax.servlet.http.Cookie;
 
 import org.apache.catalina.authenticator.AuthenticatorBase;
 import org.apache.catalina.connector.Request;
 import org.apache.catalina.connector.Response;
 import org.apache.catalina.deploy.LoginConfig;
 
+import uk.ac.bournemouth.darwin.catalina.realm.DarwinUserPrincipal;
+import uk.ac.bournemouth.darwin.catalina.realm.DarwinUserPrincipalImpl;
+
 
 public class DarwinAuthenticator extends AuthenticatorBase{
+  
+  private static final String AUTHTYPE = "DARWIN";
 
-  
-  
   @Override
   protected boolean authenticate(Request pRequest, Response pResponse, LoginConfig pConfig) throws IOException {
+    DarwinUserPrincipal principal = toDarwinPrincipal(pRequest.getUserPrincipal());
+    if (principal != null) { 
+      pRequest.setAuthType(AUTHTYPE);
+      pRequest.setUserPrincipal(principal);
+      return (true); 
+    }
     
-    // TODO Auto-generated method stub
-    // return false;
-    throw new UnsupportedOperationException("Not yet implemented");
+    for (Cookie cookie: pRequest.getCookies()) {
+      if ("DWNID".equals(cookie.getName())) {
+        // TODO Look up user from database
+        
+        break;
+      }
+    }
+    
+    // Not logged in yet. So go to login page.
+    pResponse.sendRedirect(pConfig.getLoginPage());
+    return false;
+  }
+
+  private DarwinUserPrincipal toDarwinPrincipal(Principal pPrincipal) {
+    if (pPrincipal==null) { return null; }
+    if (pPrincipal instanceof DarwinUserPrincipal) {
+      return (DarwinUserPrincipal) pPrincipal;
+    }
+    return new DarwinUserPrincipalImpl(pPrincipal.getName());
   }
 
   
