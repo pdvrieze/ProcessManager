dc4c2108d5ef934fedb67b119c923b433d699d2b
Paul de Vrieze <paul.devrieze@gmail.com>
2012-08-29 22:25:47 +0000

    Create a stored function to update the lifetime of a token.
    git-svn-id: https://darwin.bournemouth.ac.uk/pdvrieze/svn/darwin/trunk/sql@157 3bae1aa3-1c52-4c45-a669-1fb9b31e7360

---
+++ darwin-sql/webauth/storedProcs.sql
@@ -0,0 +1,26 @@
+DROP FUNCTION refreshToken;
+
+delimiter //
+CREATE DEFINER='webauthadm'@'localhost' FUNCTION refreshToken(tokenparam VARCHAR(45))
+RETURNS INT
+SQL SECURITY DEFINER
+NOT DETERMINISTIC
+MODIFIES SQL DATA
+BEGIN
+  DECLARE mytimestamp INT DEFAULT 0;
+  DECLARE myuser VARCHAR(30) DEFAULT "hello";
+--  SELECT UNIX_TIMESTAMP() INTO @mytimestamp;
+  SET @mytimestamp = UNIX_TIMESTAMP();
+  DELETE FROM tokens WHERE (epoch +1800 < @mytimestamp);
+  BEGIN
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET @myuser = NULL;
+    SELECT user INTO @myuser FROM tokens WHERE token = tokenparam AND (epoch + 1800) > @mytimestamp;
+  END;
+  IF ISNULL(@myuser) THEN
+    RETURN 0;
+  ELSE
+    UPDATE tokens SET epoch=@mytimestamp WHERE token=tokenparam;
+    RETURN @mytimestamp + 1800;
+  END IF;
+END//
+delimiter ;
