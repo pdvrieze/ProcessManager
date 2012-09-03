ed1efb1f728dae7cf48cdc5954c2d262d935d8a0
Paul de Vrieze <paul.devrieze@gmail.com>
2012-09-03 14:54:28 +0000

    Create an exception class that can wrap multiple underlying exceptions.
    git-svn-id: https://darwin.bournemouth.ac.uk/pdvrieze/svn/darwin/projects/DarwinCommon/trunk@191 3bae1aa3-1c52-4c45-a669-1fb9b31e7360

---
+++ DarwinCommon/src/net/devrieze/util/CompoundException.java
@@ -0,0 +1,96 @@
+package net.devrieze.util;
+
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.util.List;
+
+
+public class CompoundException extends RuntimeException {
+
+  private List<? extends Throwable> aCauses;
+  private int replayPos=0;
+
+  public CompoundException(List<? extends Exception> pCauses) {
+    super("Multiple exceptions occurred");
+    aCauses = pCauses;
+  }
+  
+  public <T extends Throwable> void  replayNext(Class<T> pClass) throws T {
+    int pos = replayPos;
+    replayPos++;
+    if (pos<aCauses.size()) {
+      Throwable e = aCauses.get(pos);
+      if (pClass!=null && pClass.isInstance(e)) {
+        throw pClass.cast(e);
+      }
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public String getMessage() {
+    // TODO Auto-generated method stub
+    return super.getMessage();
+  }
+
+  @Override
+  public void printStackTrace() {
+    printStackTrace(System.out);
+  }
+
+  @Override
+  public void printStackTrace(PrintStream s) {
+    synchronized (s) {
+      s.println(this);
+      for(int i=0; i<aCauses.size(); ++i) {
+        if (i>=1) { s.println(); }
+        Throwable cause = aCauses.get(i);
+        s.print("Cause "); s.print(i); s.print(": "); s.println(cause);
+        for(StackTraceElement elem: cause.getStackTrace()) {
+          s.print(i); s.print(":\tat"); s.print(elem);
+        }
+        Throwable elemCause = cause.getCause();
+        if (elemCause!=null) { printStackTraceAsCause(s, i, elemCause); }
+      }
+    }
+  }
+
+  private static void printStackTraceAsCause(PrintStream s, int i, Throwable pCause) {
+    Throwable cause = pCause;
+    s.print("Cause "); s.print(i); s.print(": "); s.println(cause);
+    for(StackTraceElement elem: cause.getStackTrace()) {
+      s.print(i); s.print(":\tat"); s.print(elem);
+    }
+    Throwable elemCause = cause.getCause();
+    if (elemCause!=null) { printStackTraceAsCause(s, i, elemCause); }
+  }
+
+  @Override
+  public void printStackTrace(PrintWriter s) {
+    synchronized (s) {
+      s.println(this);
+      for(int i=0; i<aCauses.size(); ++i) {
+        if (i>=1) { s.println(); }
+        Throwable cause = aCauses.get(i);
+        s.print("Cause "); s.print(i); s.print(": "); s.println(cause);
+        for(StackTraceElement elem: cause.getStackTrace()) {
+          s.print(i); s.print(":\tat"); s.print(elem);
+        }
+        Throwable elemCause = cause.getCause();
+        if (elemCause!=null) { printStackTraceAsCause(s, i, elemCause); }
+      }
+    }
+  }
+
+  private static void printStackTraceAsCause(PrintWriter s, int i, Throwable pCause) {
+    Throwable cause = pCause;
+    s.print("Cause "); s.print(i); s.print(": "); s.println(cause);
+    for(StackTraceElement elem: cause.getStackTrace()) {
+      s.print(i); s.print(":\tat"); s.print(elem);
+    }
+    Throwable elemCause = cause.getCause();
+    if (elemCause!=null) { printStackTraceAsCause(s, i, elemCause); }
+  }
+  
+  
+}
