2a92f0f72c0a24b87555aca3abef08df3456f55c
Paul de Vrieze <paul@devrieze.net>
2012-09-05 22:28:09 +0100

    Work on making it work with normal servlet

--- ProcessEngine/src/nl/adaptivity/process/engine/servlet/ServletProcessEngine.java
+++ ProcessEngine/src/nl/adaptivity/process/engine/servlet/ServletProcessEngine.java
@@ -2,8 +2,10 @@ package nl.adaptivity.process.engine.servlet;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
+import java.net.URI;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.activation.DataHandler;
 import javax.jws.WebMethod;
@@ -12,17 +14,26 @@ import javax.jws.WebParam.Mode;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.xml.bind.JAXB;
 import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.namespace.QName;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.stream.*;
+import javax.xml.stream.events.*;
+import javax.xml.transform.Source;
+import javax.xml.transform.dom.DOMResult;
+import javax.xml.transform.dom.DOMSource;
 
 import org.w3c.dom.Node;
 
 import net.devrieze.util.HandleMap;
 
 import nl.adaptivity.process.IMessageService;
-import nl.adaptivity.process.engine.HProcessInstance;
-import nl.adaptivity.process.engine.ProcessEngine;
-import nl.adaptivity.process.engine.ProcessInstance;
+import nl.adaptivity.process.engine.*;
 import nl.adaptivity.process.engine.ProcessInstance.ProcessInstanceRef;
 import nl.adaptivity.process.engine.processModel.ProcessNodeInstance;
 import nl.adaptivity.process.exec.Task.TaskState;
@@ -34,19 +45,236 @@ import nl.adaptivity.rest.annotations.RestMethod;
 import nl.adaptivity.rest.annotations.RestMethod.HttpMethod;
 import nl.adaptivity.rest.annotations.RestParam;
 import nl.adaptivity.rest.annotations.RestParam.ParamType;
+import nl.adaptivity.ws.rest.RestMessageHandler;
+import nl.adaptivity.ws.soap.SoapMessageHandler;
 
 
 public class ServletProcessEngine extends HttpServlet implements IMessageService<ServletProcessEngine.ServletMessage, ProcessNodeInstance> {
 
+  private static final long serialVersionUID = -6277449163953383974L;
+  
+  public static final String MY_JBI_NS = "http://adaptivity.nl/jbi";
   public static final String PROCESS_ENGINE_NS = "http://adaptivity.nl/ProcessEngine/";
+  public static final URI MODIFY_NS = URI.create("http://adaptivity.nl/ProcessEngine/activity");
+  public static final QName SERVICE_QNAME = new QName(PROCESS_ENGINE_NS,"ProcessEngine");
+
+  private static final String OP_POST_MESSAGE = "postMessage";
+  private static final String OP_START_PROCESS = "startProcess";
 
   static class ServletMessage{
+
+    private final QName aRemoteService;
+    private final String aRemoteEndpoint;
+    private final QName aOperation;
+    private Source aBody;
+
+    public ServletMessage(QName pService, String pEndpoint, QName pOperation, Source pBody) {
+      aRemoteService = pService;
+      aRemoteEndpoint = pEndpoint;
+      aOperation = pOperation;
+      aBody = pBody;
+    }
+
+
+    public QName getService() {
+      return aRemoteService;
+    }
+
+
+    public String getEndpoint() {
+      return aRemoteEndpoint;
+    }
+
+
+    public QName getOperation() {
+      return aOperation;
+    }
+
+
+    public Source getContent() {
+      return aBody;
+    }
+
+
+    public void setHandle(long pHandle) {
+      try {
+        XMLInputFactory xif = XMLInputFactory.newInstance();
+        if (aBody ==null) { throw new NullPointerException(); }
+        
+        XMLEventReader xer = xif.createXMLEventReader(aBody);
+        XMLOutputFactory xof = XMLOutputFactory.newInstance();
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        dbf.setNamespaceAware(true);
+        DocumentBuilder db;
+        try {
+          db = dbf.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+          throw new MyMessagingException(e);
+        }
+        DOMResult result = new DOMResult(db.newDocument());
+        XMLEventWriter xew = xof.createXMLEventWriter(result);
+
+        while (xer.hasNext()) {
+          XMLEvent event = xer.nextEvent();
+          if (event.isStartElement()) {
+            StartElement se = event.asStartElement();
+            final QName eName = se.getName();
+            if (MODIFY_NS.toString().equals(eName.getNamespaceURI())) {
+              @SuppressWarnings("unchecked") Iterator<Attribute> attributes = se.getAttributes();
+              if (eName.getLocalPart().equals("attribute")) {
+                writeAttribute(xer, attributes, xew, pHandle);
+              } else if (eName.getLocalPart().equals("element")) {
+                writeElement(xer, attributes, xew, pHandle);
+              } else {
+                throw new MyMessagingException("Unsupported activity modifier");
+              }
+            } else {
+              xew.add(se);
+            }
+          } else {
+            if (event.isCharacters()) {
+              Characters c = event.asCharacters();
+              String charData = c.getData();
+              int i;
+              for(i =0; i<charData.length(); ++i) {
+                if (! Character.isWhitespace(charData.charAt(i))) {
+                  break;
+                }
+              }
+              if (i==charData.length()) {
+                continue; // ignore it, and go to next event
+              }
+            }
+
+            if (event instanceof Namespace) {
+
+              Namespace ns = (Namespace) event;
+              if (! ns.getNamespaceURI().equals(MODIFY_NS)) {
+                xew.add(event);
+              }
+            } else {
+              xew.add(event);
+            }
+          }
+        }
+        aBody = new DOMSource(result.getNode());
+
+      } catch (FactoryConfigurationError e) {
+        throw new MyMessagingException(e);
+      } catch (XMLStreamException e) {
+        throw new MyMessagingException(e);
+      }
+    }
+
+    private void writeElement(XMLEventReader in, Iterator<Attribute> pAttributes, XMLEventWriter out, long pHandle) throws XMLStreamException {
+      String valueName = null;
+      {
+        while(pAttributes.hasNext()) {
+          Attribute attr = pAttributes.next();
+          String attrName = attr.getName().getLocalPart();
+          if ("value".equals(attrName)) {
+            valueName = attr.getValue();
+          }
+        }
+      }
+      {
+        XMLEvent ev = in.nextEvent();
+
+        while (! ev.isEndElement()) {
+          if (ev.isStartElement()) { throw new MyMessagingException("Violation of schema"); }
+          if (ev.isAttribute()) {
+            Attribute attr = (Attribute) ev;
+            String attrName = attr.getName().getLocalPart();
+            if ("value".equals(attrName)) {
+              valueName = attr.getValue();
+            }
+          }
+        }
+      }
+      if (valueName!=null) {
+        XMLEventFactory xef = XMLEventFactory.newInstance();
+
+        if ("handle".equals(valueName)) {
+          out.add(xef.createCharacters(Long.toString(pHandle)));
+        } else if ("endpoint".equals(valueName)) {
+          // TODO Write our own location.
+          QName qname1 = new QName(MY_JBI_NS, "endpointDescriptor", "");
+          List<Namespace> namespaces = Collections.singletonList(xef.createNamespace("", MY_JBI_NS));
+          out.add(xef.createStartElement(qname1, null, namespaces.iterator()));
+
+//          {
+//            out.add(xef.createAttribute("serviceNS", aEndPoint.getServiceName().getNamespaceURI()));
+//            out.add(xef.createAttribute("serviceLocalName", aEndPoint.getServiceName().getLocalPart()));
+//            out.add(xef.createAttribute("endpointName", aEndPoint.getEndpointName()));
+//          }
+
+          out.add(xef.createEndElement(qname1, namespaces.iterator()));
+        }
+      } else {
+        throw new MyMessagingException("Missing parameter name");
+      }
+
+    }
+
+    private void writeAttribute(XMLEventReader in, Iterator<Attribute> pAttributes, XMLEventWriter out, long pHandle) throws XMLStreamException {
+      String valueName = null;
+      String paramName = null;
+      {
+        while(pAttributes.hasNext()) {
+          Attribute attr = pAttributes.next();
+          String attrName = attr.getName().getLocalPart();
+          if ("value".equals(attrName)) {
+            valueName = attr.getValue();
+          } else if ("name".equals(attrName)) {
+            paramName = attr.getValue();
+          }
+        }
+      }
+      {
+        XMLEvent ev = in.nextEvent();
+
+        while (! ev.isEndElement()) {
+          if (ev.isStartElement()) { throw new MyMessagingException("Violation of schema"); }
+          if (ev.isAttribute()) {
+            Attribute attr = (Attribute) ev;
+            String attrName = attr.getName().getLocalPart();
+            if ("value".equals(attrName)) {
+              valueName = attr.getValue();
+            } else if ("name".equals(attrName)) {
+              paramName = attr.getValue();
+            }
+          }
+        }
+      }
+      if (valueName!=null) {
+
+
+        XMLEventFactory xef = XMLEventFactory.newInstance();
+
+        if ("handle".equals(valueName)) {
+          Attribute attr;
+          if (paramName !=null) {
+            attr = xef.createAttribute(paramName, Long.toString(pHandle));
+          } else {
+            attr = xef.createAttribute("handle", Long.toString(pHandle));
+          }
+          out.add(attr);
+        }
+
+
+      } else {
+        throw new MyMessagingException("Missing parameter name");
+      }
+
+    }
     
   }
-  
-  private ProcessEngine aProcessEngine;
-  private boolean aKeepRunning = true;
   private Thread aThread;
+  private boolean aKeepRunning = true;
+  private Logger aLogger;
+  private ProcessEngine aProcessEngine;
+  private RestMessageHandler aRestMessageHandler;
+  private SoapMessageHandler aSoapMessageHandler;
   
   /*
    * Servlet methods 
@@ -70,15 +298,49 @@ public class ServletProcessEngine extends HttpServlet implements IMessageService
   }
   
   @Override
-  public void init(ServletConfig pArg0) throws ServletException {
+  public void init(ServletConfig pConfig) throws ServletException {
+    super.init(pConfig);
     aProcessEngine = new ProcessEngine(this);
-    // TODO Auto-generated method stub
-
   }
 
   /*
    * IMessageService methods 
    */
+
+  @Override
+  protected void doDelete(HttpServletRequest pReq, HttpServletResponse pResp) throws ServletException, IOException {
+    // TODO Auto-generated method stub
+    // super.doDelete(pReq, pResp);
+    throw new UnsupportedOperationException("Not yet implemented");
+  }
+
+  @Override
+  protected void doGet(HttpServletRequest pReq, HttpServletResponse pResp) throws ServletException, IOException {
+    // TODO Auto-generated method stub
+    // super.doGet(pReq, pResp);
+    throw new UnsupportedOperationException("Not yet implemented");
+  }
+
+  @Override
+  protected void doHead(HttpServletRequest pReq, HttpServletResponse pResp) throws ServletException, IOException {
+    // TODO Auto-generated method stub
+    // super.doHead(pReq, pResp);
+    throw new UnsupportedOperationException("Not yet implemented");
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest pReq, HttpServletResponse pResp) throws ServletException, IOException {
+    // TODO Auto-generated method stub
+    // super.doPost(pReq, pResp);
+    throw new UnsupportedOperationException("Not yet implemented");
+  }
+
+  @Override
+  protected void doPut(HttpServletRequest pReq, HttpServletResponse pResp) throws ServletException, IOException {
+    // TODO Auto-generated method stub
+    // super.doPut(pReq, pResp);
+    throw new UnsupportedOperationException("Not yet implemented");
+  }
 
   @Override
   public ServletMessage createMessage(XmlMessage pMessage) {
@@ -93,7 +355,81 @@ public class ServletProcessEngine extends HttpServlet implements IMessageService
     // return false;
     throw new UnsupportedOperationException("Not yet implemented");
   }
+
+  private RestMessageHandler getRestMessageHandler() {
+    if (aRestMessageHandler == null) {
+      aRestMessageHandler = RestMessageHandler.newInstance(this);
+    }
+    return aRestMessageHandler;
+  }
+
+  private SoapMessageHandler getSoapMessageHandler() {
+    if (aSoapMessageHandler == null) {
+      aSoapMessageHandler = SoapMessageHandler.newInstance(this);
+    }
+    return aSoapMessageHandler;
+  }
   
+  /*
+   * Methods inherited from JBIProcessEngine
+   */
+
+  Logger getLogger() {
+    if (aLogger != null) {
+      return aLogger;
+    }
+    aLogger = Logger.getLogger(getClass().getName());
+    aLogger.setLevel(Level.ALL);
+    return aLogger;
+  }
+
+  private void processRestSoap(HttpMethod pMethod, HttpServletRequest pRequest, HttpServletResponse pResponse) {
+    RestMessageHandler restHandler = getRestMessageHandler();
+    try {
+      if (restHandler.isRestRequest(pRequest)) {
+        if (!getRestMessageHandler().processRequest(pMethod, pRequest, pResponse)) {
+          getLogger().warning("Error processing rest request");
+        }
+      } else {
+        if (!getSoapMessageHandler().processRequest(pRequest, pResponse)) {
+          getLogger().warning("Error processing soap request");
+        }
+        
+      }
+    } catch (IOException e) {
+      try {
+        pResponse.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+      } catch (IOException e1) {
+        getLogger().log(Level.WARNING, "Failure to notify client of error", e);
+      }
+    }
+    /*
+    NormalizedMessage inMessage = pEx.getMessage("in");
+    NormalizedMessage reply = pEx.createMessage();
+    if (pEx.getOperation().getNamespaceURI().equals(Constants.WEBMETHOD_NS.toString())) {
+      HttpMethod operation = HttpMethod.valueOf(pEx.getOperation().getLocalPart());
+      if (getRestMessageHandler().processRequest(operation, inMessage, reply, this)) {
+        pEx.setMessage(reply, "out");
+        pDeliveryChannel.send(pEx);
+      } else {
+        pEx.setError(new FileNotFoundException());
+        pDeliveryChannel.send(pEx);
+      }
+    } else {
+      if (getSoapMessageHandler().processRequest(pEx.getOperation(), inMessage, reply, this)) {
+        if (pEx.getPattern().equals(Constants.WSDL_MEP_IN_ONLY)|| pEx.getPattern().equals(Constants.WSDL_MEP_ROBUST_IN_ONLY)) {
+          pEx.setStatus(ExchangeStatus.DONE);
+        } else {
+          pEx.setMessage(reply, "out");
+        }
+      } else {
+        pEx.setError(new FileNotFoundException());
+      }
+      pDeliveryChannel.send(pEx);
+    }
+    */
+  }
+
   /*
    * Web interface for this servlet 
    */
