463d10a4fc9d2468e926544dca93dea25b3fac94
Paul de Vrieze <paul@devrieze.net>
2012-09-05 15:11:04 +0100

    Make project a web app

---
+++ ProcessEngine/src/nl/adaptivity/process/engine/servlet/ServletProcessEngine.java
@@ -0,0 +1,44 @@
+package nl.adaptivity.process.engine.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+
+public class ServletProcessEngine implements Servlet {
+
+  @Override
+  public void destroy() {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public ServletConfig getServletConfig() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public String getServletInfo() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void init(ServletConfig pArg0) throws ServletException {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void service(ServletRequest pArg0, ServletResponse pArg1) throws ServletException, IOException {
+    // TODO Auto-generated method stub
+
+  }
+
+}
