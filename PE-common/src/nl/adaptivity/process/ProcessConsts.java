fc51315276975b22565545c30f031b1749c1a2e2
Paul de Vrieze <paul@devrieze.net>
2012-09-16 18:49:18 +0100

    Create a class for holding the various global constants.

---
+++ PE-common/src/nl/adaptivity/process/ProcessConsts.java
@@ -0,0 +1,11 @@
+package nl.adaptivity.process;
+
+
+public class ProcessConsts {
+  
+  public static class Engine {
+    public static final String NAMESPACE = "http://adaptivity.nl/ProcessEngine/";
+    public static final String NSPREFIX = "pe";
+  }
+
+}
