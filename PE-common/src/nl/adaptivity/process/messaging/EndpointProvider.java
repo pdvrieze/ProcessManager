cbc737a47bd2b88b0d6707165734925a6982e601
Paul de Vrieze <paul@devrieze.net>
2012-09-16 20:03:55 +0100

    Document, mark deprecated.

--- PE-common/src/nl/adaptivity/process/messaging/EndpointProvider.java
+++ PE-common/src/nl/adaptivity/process/messaging/EndpointProvider.java
@@ -2,11 +2,24 @@ package nl.adaptivity.process.messaging;
 
 import java.util.Collection;
 
-
+/**
+ * Marker interface for classes that can provide a set of endpoints.
+ * @author Paul de Vrieze
+ * @deprecated Does not work currently in servlet context as they are declarative.
+ */
+@Deprecated
 public interface EndpointProvider {
 
+  /**
+   * Get the endpoints provided.
+   * @return The collection
+   */
   Collection<GenericEndpoint> getEndpoints();
 
-  void setContext(AsyncMessenger pContext);
+  /**
+   * Set the messenger to use for outwards communication. 
+   * @param pMessenger The messenger.
+   */
+  void setContext(AsyncMessenger pMessenger);
 
 }
