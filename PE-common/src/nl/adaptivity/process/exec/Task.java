8cafcbe4e68703edf6faca0b9e35325a3a10abc4
Paul de Vrieze <paul@devrieze.net>
2012-09-16 18:24:52 +0100

    Document and add #cancelTask method as we have that as a terminal as well.

--- PE-common/src/nl/adaptivity/process/exec/Task.java
+++ PE-common/src/nl/adaptivity/process/exec/Task.java
@@ -8,31 +8,100 @@ import net.devrieze.util.HandleMap.HandleAware;
 
 import nl.adaptivity.process.IMessageService;
 
+/**
+ * Class representing the instantiation of an executable process node.
+ * @author Paul de Vrieze
+ *
+ * @param <V> The actual type of the implementing class.
+ */
 public interface Task<V extends Task<V>> extends HandleAware<V>{
 
+  /**
+   * Enumeration representing the various states a task can be in.
+   * @author Paul de Vrieze
+   *
+   */
   @XmlRootElement(name="taskState", namespace="http://adaptivity.nl/userMessageHandler")
   public static enum TaskState {
+    /**
+     * Initial task state. Indicates that the task has been communicated to a handler.
+     */
     Sent,
+    /**
+     * State acknowledging reception of the task. Note that this is generally only used by
+     * process aware services. It signifies that a task has been received, but processing
+     * has not started yet.
+     */
     Acknowledged,
+    /**
+     * Some tasks allow for alternatives (different users). Taken signifies that the task has
+     * been claimed and others can not claim it anymore (unless released again).
+     */
     Taken,
+    /**
+     * Signifies that work on the task has actually started.
+     */
     Started,
+    /**
+     * Signifies that the task is complete. This generally is the end state of a task.
+     */
     Complete,
+    /**
+     * Signifies that the task has failed for some reason.
+     */
     Failed, 
+    /**
+     * Signifies that the task has been cancelled (but not through a failure).
+     */
     Cancelled;
   }
 
+  /**
+   * Get the state of the task.
+   * @return the state.
+   */
   public TaskState getState();
 
+  /**
+   * Set the state of the task.
+   * @param aNewState The new state of the task.
+   */
   public void setState(TaskState aNewState);
 
+  /**
+   * Called by the processEngine so indicate starting of the task.
+   * @param pMessageService Service to use for communication of change of state.
+   * @return <code>true</code> if this stage is complete and the engine should progress to {@link #takeTask(IMessageService)
+   */
   public <T> boolean provideTask(IMessageService<T, V> pMessageService);
 
+  /**
+   * Called by the processEngine to let the task be taken. 
+   * @param pMessageService Service to use for communication of change of state.
+   * @return <code>true</code> if this stage has completed and the task should be {@link #startTask(IMessageService) started}.
+   */
   public <T> boolean takeTask(IMessageService<T, V> pMessageService);
 
+  /**
+   * Called by the processEngine to let the system start the task.
+   * @param pMessageService Service to use for communication of change of state.
+   * @return <code>true</code> if the task has completed and {@link #finishTask(Node)} should be called.
+   */
   public <T> boolean startTask(IMessageService<T, V> pMessageService);
 
+  /**
+   * Called by the processEngine to signify to the task that it is finished (with the given payload).
+   * @param pPayload The payload which is the result of the processing.
+   */
   public void finishTask(Node pPayload);
 
+  /**
+   * Called to signify that this task has failed.
+   */
   public void failTask();
 
+  /**
+   * Called to signify that this task has been cancelled.
+   */
+  public void cancelTask();
 }
