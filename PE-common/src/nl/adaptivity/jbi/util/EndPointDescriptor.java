232a2160a5fe06f8269a082f8e400903ad7bf4c4
Paul de Vrieze <paul@devrieze.net>
2012-09-16 22:49:58 +0100

    Record an actual url for the endpoint.

--- PE-common/src/nl/adaptivity/jbi/util/EndPointDescriptor.java
+++ PE-common/src/nl/adaptivity/jbi/util/EndPointDescriptor.java
@@ -1,9 +1,10 @@
 package nl.adaptivity.jbi.util;
 
+import java.net.URI;
+
 import javax.xml.bind.annotation.*;
 import javax.xml.namespace.QName;
 
-@Deprecated
 @XmlRootElement(name="endpointDescriptor", namespace=EndPointDescriptor.MY_JBI_NS)
 @XmlAccessorType(XmlAccessType.NONE)
 @XmlType(propOrder={"serviceNamespace", "serviceLocalName", "endpointName"})
@@ -13,13 +14,15 @@ public class EndPointDescriptor {
   private String aServiceLocalName;
   private String aServiceNamespace;
   private String aEndpointName;
+  private URI aEndpointLocation;
 
   public EndPointDescriptor() {}
 
-  public EndPointDescriptor(QName pServiceName, String pEndpointName) {
+  public EndPointDescriptor(QName pServiceName, String pEndpointName, URI pEndpointLocation) {
     aServiceLocalName = pServiceName.getLocalPart();
     aServiceNamespace = pServiceName.getNamespaceURI();
     aEndpointName = pEndpointName;
+    aEndpointLocation = pEndpointLocation;
   }
 
   @XmlAttribute(name="endpointName")
@@ -31,6 +34,23 @@ public class EndPointDescriptor {
     aEndpointName = endpointName;
   }
 
+  @XmlAttribute(name="endpointLocation")
+  public String getEndpointLocationString() {
+    return aEndpointLocation.toString();
+  }
+  
+  public void setEndpointLocationString(String pLocation) {
+    aEndpointLocation = URI.create(pLocation);
+  }
+
+  public URI getEnpointLocation() {
+    return aEndpointLocation;
+  }
+  
+  public void setEndpointLocation(URI pLocation) {
+    aEndpointLocation = pLocation;
+  }
+  
   @XmlAttribute(name="serviceLocalName")
   public String getServiceLocalName() {
     return aServiceLocalName;
