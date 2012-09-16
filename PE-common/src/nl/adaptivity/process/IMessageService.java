c34bd62340bfa6b59afb2c476d5742a030d39c69
Paul de Vrieze <paul@devrieze.net>
2012-09-16 18:11:17 +0100

    Comment

--- PE-common/src/nl/adaptivity/process/IMessageService.java
+++ PE-common/src/nl/adaptivity/process/IMessageService.java
@@ -4,9 +4,27 @@ import nl.adaptivity.process.exec.Task;
 import nl.adaptivity.process.processModel.XmlMessage;
 
 
+/**
+ * Interface signifying that the object can be used to send messages.
+ * @author Paul de Vrieze
+ *
+ * @param <T> The type signifying a message that can then be sent.
+ * @param <U> The task that the message corresponds to. This allows for messages to be linked to tasks.
+ */
 public interface IMessageService<T,U extends Task<U>> {
 
+  /**
+   * Create a message.
+   * @param pMessage The message to create (for later sending)
+   * @return The sendable message that can be sent.
+   */
   T createMessage(XmlMessage pMessage);
 
+  /**
+   * Send a message.
+   * @param pMessage The message to send. (Created by {@link #createMessage(XmlMessage)}).
+   * @param pInstance The task instance to link the sending to.
+   * @return <code>true</code> on success, <code>false</code> on failure.
+   */
   boolean sendMessage(T pMessage, U pInstance);
 }
