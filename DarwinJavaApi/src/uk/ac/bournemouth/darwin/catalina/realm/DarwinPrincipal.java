44a275c11347bfd05534a066cba25ebbdf96663c
Paul de Vrieze <paul.devrieze@gmail.com>
2012-09-23 14:01:19 +0000

    Extend the API provided through DarwinPrincipal
    git-svn-id: https://darwin.bournemouth.ac.uk/pdvrieze/svn/darwin/projects/DarwinJavaApi/trunk@225 3bae1aa3-1c52-4c45-a669-1fb9b31e7360

--- DarwinJavaApi/src/uk/ac/bournemouth/darwin/catalina/realm/DarwinPrincipal.java
+++ DarwinJavaApi/src/uk/ac/bournemouth/darwin/catalina/realm/DarwinPrincipal.java
@@ -1,8 +1,42 @@
 package uk.ac.bournemouth.darwin.catalina.realm;
 
 import java.security.Principal;
+import java.util.Set;
 
 
 public interface DarwinPrincipal extends Principal {
 
+  /**
+   * Determine whether the user has overall admin rights.
+   * 
+   * @return <code>true</code> if the user is admin, <code>false</code> if not.
+   */
+  boolean isAdmin();
+
+  /**
+   * Determine whether the user has the given role. Note that depending on the
+   * implementation some caching can occur and therefore the answer might not
+   * reflect very recent changes in the database.
+   * 
+   * @param pRoleName The role to verify.
+   * @return <code>true</code> if the user has the role, <code>false</code> if
+   *         not.
+   */
+  boolean hasRole(String pRoleName);
+  
+  /**
+   * Get the email address of the user.
+   * @return The email address, or <code>null</code> if not known.
+   */
+  CharSequence getEmail();
+
+
+  /**
+   * Get a set of the roles of the user. Note that changes to this will not be allowed,
+   * and in any case would not be reflected in the database.
+   * @return The set of roles.
+   */
+  public Set<? extends CharSequence> getRolesSet();
+  
+  
 }
