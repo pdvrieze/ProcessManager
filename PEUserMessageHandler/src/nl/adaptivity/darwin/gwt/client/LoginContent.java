a4f3602535d76504ee69f6d3673c9a6b03850021
Paul de Vrieze <paul@devrieze.net>
2012-09-24 12:32:05 +0100

    Add redirect field (for when submitting through form submission).

--- PEUserMessageHandler/src/nl/adaptivity/darwin/gwt/client/LoginContent.java
+++ PEUserMessageHandler/src/nl/adaptivity/darwin/gwt/client/LoginContent.java
@@ -1,6 +1,9 @@
 package nl.adaptivity.darwin.gwt.client;
 
+import nl.adaptivity.gwt.ext.client.XMLUtil;
+
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Document;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.InputElement;
 import com.google.gwt.uibinder.client.UiBinder;
@@ -26,9 +29,26 @@ public class LoginContent extends Widget {
   @UiField
   InputElement cancel;
 
-  public LoginContent() {
+  @UiField
+  InputElement redirect;
 
-    setElement(uiBinder.createAndBindUi(this));
+  public LoginContent() {
+    Document document = Document.get();
+    Element parentForm = document.getElementById("loginform");
+    if (parentForm!=null) {
+      parentForm = parentForm.cloneNode(true).cast();
+//      parentForm.removeFromParent();
+      parentForm.removeAttribute("style"); // Remove the display:hidden
+      parentForm.removeAttribute("action");
+      username = XMLUtil.descendentWithAttribute(parentForm, "name", "username").cast();
+      password = XMLUtil.descendentWithAttribute(parentForm, "name", "password").cast();
+      login = XMLUtil.descendentWithAttribute(parentForm, "name", "login").cast();
+      login.setAttribute("type", "button");
+      cancel = XMLUtil.descendentWithAttribute(parentForm, "name", "cancel").cast();
+      setElement(parentForm);
+    } else {
+      setElement(uiBinder.createAndBindUi(this));
+    }
 
   }
 
