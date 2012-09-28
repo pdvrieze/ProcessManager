906c368a77ad367cff95b2425d98e1cbd3e430bf
Paul de Vrieze <paul@devrieze.net>
2012-09-28 23:33:26 +0100

    Move UserTask to this project, but remove the whole asyncmessenger bit.

---
+++ PE-common/src/nl/adaptivity/process/userMessageHandler/server/UserTask.java
@@ -0,0 +1,21 @@
+package nl.adaptivity.process.userMessageHandler.server;
+
+import java.security.Principal;
+
+import net.devrieze.util.HandleMap.HandleAware;
+
+import nl.adaptivity.messaging.EndPointDescriptor;
+import nl.adaptivity.process.exec.Task.TaskState;
+
+
+public interface UserTask <T extends UserTask<T>> extends HandleAware<T>{
+
+  public TaskState getState();
+
+  public void setState(TaskState aNewState, Principal pUser);
+
+  public void setEndpoint(EndPointDescriptor pEndPoint);
+
+  public Principal getOwner();
+
+}
