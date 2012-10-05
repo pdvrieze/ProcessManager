cc261827a4207496c31fb8f1f29da72fc2f18d80
Paul de Vrieze <paul@devrieze.net>
2012-10-05 21:24:12 +0100

    Clean up code.

--- PE-common/src/org/w3/soapEnvelope/Body.java
+++ PE-common/src/org/w3/soapEnvelope/Body.java
@@ -20,10 +20,12 @@ import org.w3c.dom.Element;
 
 
 /**
- * <p>Java class for Body complex type.
- *
- * <p>The following schema fragment specifies the expected content contained within this class.
- *
+ * <p>
+ * Java class for Body complex type.
+ * <p>
+ * The following schema fragment specifies the expected content contained within
+ * this class.
+ * 
  * <pre>
  * &lt;complexType name="Body">
  *   &lt;complexContent>
@@ -36,68 +38,56 @@ import org.w3c.dom.Element;
  *   &lt;/complexContent>
  * &lt;/complexType>
  * </pre>
- *
- *
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = Body.ELEMENTNAME, propOrder = {
-    "any"
-})
+@XmlType(name = Body.ELEMENTNAME, propOrder = { "any" })
 public class Body {
 
-    public static final String ELEMENTNAME="Body";
-  
-    @XmlAnyElement(lax = false)
-    protected List<Object> any;
-    @XmlAnyAttribute
-    private Map<QName, String> otherAttributes = new HashMap<QName, String>();
+  public static final String ELEMENTNAME = "Body";
 
-    /**
-     * Gets the value of the any property.
-     *
-     * <p>
-     * This accessor method returns a reference to the live list,
-     * not a snapshot. Therefore any modification you make to the
-     * returned list will be present inside the JAXB object.
-     * This is why there is not a <CODE>set</CODE> method for the any property.
-     *
-     * <p>
-     * For example, to add a new item, do as follows:
-     * <pre>
-     *    getAny().add(newItem);
-     * </pre>
-     *
-     *
-     * <p>
-     * Objects of the following type(s) are allowed in the list
-     * {@link Object }
-     * {@link Element }
-     *
-     *
-     */
-    public List<Object> getAny() {
-        if (any == null) {
-            any = new ArrayList<Object>();
-        }
-        return this.any;
-    }
+  @XmlAnyElement(lax = false)
+  protected List<Object> any;
 
-    /**
-     * Gets a map that contains attributes that aren't bound to any typed property on this class.
-     *
-     * <p>
-     * the map is keyed by the name of the attribute and
-     * the value is the string value of the attribute.
-     *
-     * the map returned by this method is live, and you can add new attribute
-     * by updating the map directly. Because of this design, there's no setter.
-     *
-     *
-     * @return
-     *     always non-null
-     */
-    public Map<QName, String> getOtherAttributes() {
-        return otherAttributes;
+  @XmlAnyAttribute
+  private final Map<QName, String> otherAttributes = new HashMap<QName, String>();
+
+  /**
+   * Gets the value of the any property.
+   * <p>
+   * This accessor method returns a reference to the live list, not a snapshot.
+   * Therefore any modification you make to the returned list will be present
+   * inside the JAXB object. This is why there is not a <CODE>set</CODE> method
+   * for the any property.
+   * <p>
+   * For example, to add a new item, do as follows:
+   * 
+   * <pre>
+   * getAny().add(newItem);
+   * </pre>
+   * <p>
+   * Objects of the following type(s) are allowed in the list {@link Object }
+   * {@link Element }
+   */
+  public List<Object> getAny() {
+    if (any == null) {
+      any = new ArrayList<Object>();
     }
+    return this.any;
+  }
+
+  /**
+   * Gets a map that contains attributes that aren't bound to any typed property
+   * on this class.
+   * <p>
+   * the map is keyed by the name of the attribute and the value is the string
+   * value of the attribute. the map returned by this method is live, and you
+   * can add new attribute by updating the map directly. Because of this design,
+   * there's no setter.
+   * 
+   * @return always non-null
+   */
+  public Map<QName, String> getOtherAttributes() {
+    return otherAttributes;
+  }
 
 }
--- PE-common/src/org/w3/soapEnvelope/UpgradeType.java
+++ PE-common/src/org/w3/soapEnvelope/UpgradeType.java
@@ -10,6 +10,7 @@ package org.w3.soapEnvelope;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
@@ -17,9 +18,11 @@ import javax.xml.bind.annotation.XmlType;
 
 
 /**
- * <p>Java class for UpgradeType complex type.
- * 
- * <p>The following schema fragment specifies the expected content contained within this class.
+ * <p>
+ * Java class for UpgradeType complex type.
+ * <p>
+ * The following schema fragment specifies the expected content contained within
+ * this class.
  * 
  * <pre>
  * &lt;complexType name="UpgradeType">
@@ -32,45 +35,36 @@ import javax.xml.bind.annotation.XmlType;
  *   &lt;/complexContent>
  * &lt;/complexType>
  * </pre>
- * 
- * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "UpgradeType", propOrder = {
-    "supportedEnvelope"
-})
+@XmlType(name = "UpgradeType", propOrder = { "supportedEnvelope" })
 public class UpgradeType {
 
-    @XmlElement(name = "SupportedEnvelope", required = true)
-    protected List<SupportedEnvType> supportedEnvelope;
+  @XmlElement(name = "SupportedEnvelope", required = true)
+  protected List<SupportedEnvType> supportedEnvelope;
 
-    /**
-     * Gets the value of the supportedEnvelope property.
-     * 
-     * <p>
-     * This accessor method returns a reference to the live list,
-     * not a snapshot. Therefore any modification you make to the
-     * returned list will be present inside the JAXB object.
-     * This is why there is not a <CODE>set</CODE> method for the supportedEnvelope property.
-     * 
-     * <p>
-     * For example, to add a new item, do as follows:
-     * <pre>
-     *    getSupportedEnvelope().add(newItem);
-     * </pre>
-     * 
-     * 
-     * <p>
-     * Objects of the following type(s) are allowed in the list
-     * {@link SupportedEnvType }
-     * 
-     * 
-     */
-    public List<SupportedEnvType> getSupportedEnvelope() {
-        if (supportedEnvelope == null) {
-            supportedEnvelope = new ArrayList<SupportedEnvType>();
-        }
-        return this.supportedEnvelope;
+  /**
+   * Gets the value of the supportedEnvelope property.
+   * <p>
+   * This accessor method returns a reference to the live list, not a snapshot.
+   * Therefore any modification you make to the returned list will be present
+   * inside the JAXB object. This is why there is not a <CODE>set</CODE> method
+   * for the supportedEnvelope property.
+   * <p>
+   * For example, to add a new item, do as follows:
+   * 
+   * <pre>
+   * getSupportedEnvelope().add(newItem);
+   * </pre>
+   * <p>
+   * Objects of the following type(s) are allowed in the list
+   * {@link SupportedEnvType }
+   */
+  public List<SupportedEnvType> getSupportedEnvelope() {
+    if (supportedEnvelope == null) {
+      supportedEnvelope = new ArrayList<SupportedEnvType>();
     }
+    return this.supportedEnvelope;
+  }
 
 }
