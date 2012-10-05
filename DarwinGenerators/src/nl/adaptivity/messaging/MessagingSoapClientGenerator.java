d08ab8a35ff2059ea36d19303ad8a561741d9bb2
Paul de Vrieze <paul.devrieze@gmail.com>
2012-10-05 20:16:31 +0000

    Clean up code. (Based on eclipse cleanup tool.
    git-svn-id: https://darwin.bournemouth.ac.uk/pdvrieze/svn/darwin/projects/DarwinGenerators/trunk@284 3bae1aa3-1c52-4c45-a669-1fb9b31e7360

--- DarwinGenerators/src/nl/adaptivity/messaging/MessagingSoapClientGenerator.java
+++ DarwinGenerators/src/nl/adaptivity/messaging/MessagingSoapClientGenerator.java
@@ -30,9 +30,10 @@ public class MessagingSoapClientGenerator {
   private static final class ParamInfo {
 
     public final String name;
+
     public final Class<?> type;
 
-    public ParamInfo(Class<?> pType, String pName) {
+    public ParamInfo(final Class<?> pType, final String pName) {
       type = pType;
       name = pName;
     }
@@ -42,36 +43,48 @@ public class MessagingSoapClientGenerator {
   /**
    * @param args
    */
-  public static void main(String[] args) {
-    String pkg=null;
+  public static void main(final String[] args) {
+    String pkg = null;
     String outClass = null;
-    List<String> inClasses = new ArrayList<>();
+    final List<String> inClasses = new ArrayList<>();
     String cp = ".";
     String dstdir = ".";
-    for(int i = 0; i<args.length; ++i) {
+    for (int i = 0; i < args.length; ++i) {
       switch (args[i]) {
         case "-package": {
-          if (pkg!=null) { showHelp(); return; }
+          if (pkg != null) {
+            showHelp();
+            return;
+          }
           pkg = args[++i];
           break;
         }
         case "-out": {
-          if (outClass!=null) { showHelp(); return; }
+          if (outClass != null) {
+            showHelp();
+            return;
+          }
           outClass = args[++i];
-          int j = outClass.lastIndexOf('.');
-          if (j>=0 && pkg==null) {
+          final int j = outClass.lastIndexOf('.');
+          if ((j >= 0) && (pkg == null)) {
             pkg = outClass.substring(0, j);
-            outClass = outClass.substring(j+1);
+            outClass = outClass.substring(j + 1);
           }
           break;
         }
         case "-cp": {
-          if ("."!=cp) { showHelp(); return; }
+          if ("." != cp) {
+            showHelp();
+            return;
+          }
           cp = args[++i];
           break;
         }
         case "-dstdir": {
-          if ("."!=dstdir) { showHelp(); return; }
+          if ("." != dstdir) {
+            showHelp();
+            return;
+          }
           dstdir = args[++i];
           break;
         }
@@ -80,8 +93,8 @@ public class MessagingSoapClientGenerator {
           return;
         }
         default: {
-          if (args[i].charAt(0)=='-') {
-            System.err.println("Unsupported arguments: "+args[i]);
+          if (args[i].charAt(0) == '-') {
+            System.err.println("Unsupported arguments: " + args[i]);
             showHelp();
             return;
           } else {
@@ -90,20 +103,20 @@ public class MessagingSoapClientGenerator {
         }
       }
     }
-    if (inClasses.size()==0 || (inClasses.size()==1 && outClass==null) || pkg==null) {
+    if ((inClasses.size() == 0) || ((inClasses.size() == 1) && (outClass == null)) || (pkg == null)) {
       System.err.println("Not all three of inclass, outclass and package have been provided");
       showHelp();
       return;
     }
-    FileSystem fs = FileSystems.getDefault();
+    final FileSystem fs = FileSystems.getDefault();
 
-    String pkgdir = pkg.replaceAll("\\.", fs.getSeparator());
-    String classfilename = outClass+".java";
-    Path outfile = fs.getPath(dstdir, pkgdir, classfilename);
+    final String pkgdir = pkg.replaceAll("\\.", fs.getSeparator());
+    final String classfilename = outClass + ".java";
+    final Path outfile = fs.getPath(dstdir, pkgdir, classfilename);
     // Ensure the parent directory of the outfile exists.
     if (!outfile.getParent().toFile().mkdirs()) {
       if (!outfile.getParent().toFile().exists()) {
-        System.err.println("Could not create the directory "+outfile.getParent());
+        System.err.println("Could not create the directory " + outfile.getParent());
         System.exit(2);
       }
     }
@@ -111,50 +124,50 @@ public class MessagingSoapClientGenerator {
     URLClassLoader urlclassloader;
     urlclassloader = URLClassLoader.newInstance(getUrls(cp));
 
-    if (inClasses.size()==1) {
+    if (inClasses.size() == 1) {
       writeOutFile(inClasses.get(0), pkg, outClass, fs, outfile, urlclassloader);
     } else {
-      for(String inClass: inClasses) {
-        String newOutClass = inClass.substring(inClass.lastIndexOf('.')+1)+"Client";
-        Path newOutFile=outfile.getParent().resolve(newOutClass+".java");
+      for (final String inClass : inClasses) {
+        final String newOutClass = inClass.substring(inClass.lastIndexOf('.') + 1) + "Client";
+        final Path newOutFile = outfile.getParent().resolve(newOutClass + ".java");
         writeOutFile(inClass, pkg, newOutClass, fs, newOutFile, urlclassloader);
       }
     }
 
   }
 
-  private static void writeOutFile(String inClass, String pkg, String outClass, FileSystem fs, Path outfile, URLClassLoader classloader) {
+  private static void writeOutFile(final String inClass, final String pkg, final String outClass, final FileSystem fs, final Path outfile, final URLClassLoader classloader) {
     Class<?> endpointClass;
     try {
       endpointClass = classloader.loadClass(inClass);
-    } catch (ClassNotFoundException e) {
+    } catch (final ClassNotFoundException e) {
       e.printStackTrace();
       return;
     }
-    String pkgname = pkg.replaceAll(fs.getSeparator(), ".");
+    final String pkgname = pkg.replaceAll(fs.getSeparator(), ".");
     try {
       generateJava(outfile, endpointClass, pkgname, outClass);
-    } catch (IOException e) {
+    } catch (final IOException e) {
       e.printStackTrace();
       return;
     }
   }
 
-  private static URL[] getUrls(String pClassPath) {
-    FileSystem fs = FileSystems.getDefault();
-    List<URL> result = new ArrayList<>();
+  private static URL[] getUrls(final String pClassPath) {
+    final FileSystem fs = FileSystems.getDefault();
+    final List<URL> result = new ArrayList<>();
 
     try {
-      for(String element:pClassPath.split(":")) {
+      for (final String element : pClassPath.split(":")) {
         try {
-          URI uri = URI.create(element);
+          final URI uri = URI.create(element);
           result.add(uri.toURL());
-        } catch (IllegalArgumentException e) {
-          Path file = fs.getPath(element);
+        } catch (final IllegalArgumentException e) {
+          final Path file = fs.getPath(element);
           result.add(file.normalize().toUri().toURL());
         }
       }
-    } catch (MalformedURLException e) {
+    } catch (final MalformedURLException e) {
       System.err.println("Invalid classpath element");
       e.printStackTrace();
     }
@@ -171,8 +184,8 @@ public class MessagingSoapClientGenerator {
     System.out.println("  <inputclass>       : The Endpoint that needs a client");
   }
 
-  private static void generateJava(Path pOutfile, Class<?> pEndpointClass, String pPkgname, String pOutClass) throws IOException {
-    Writer out = new BufferedWriter(new FileWriter(pOutfile.toFile()));
+  private static void generateJava(final Path pOutfile, final Class<?> pEndpointClass, final String pPkgname, final String pOutClass) throws IOException {
+    final Writer out = new BufferedWriter(new FileWriter(pOutfile.toFile()));
     try {
       generateJava(out, pEndpointClass, pPkgname, pOutClass);
     } finally {
@@ -180,48 +193,57 @@ public class MessagingSoapClientGenerator {
     }
   }
 
-  private static void generateJava(Writer pOut, Class<?> pEndpointClass, String pPkgname, String pOutClass) throws IOException {
+  private static void generateJava(final Writer pOut, final Class<?> pEndpointClass, final String pPkgname, final String pOutClass) throws IOException {
     pOut.write("/*\n");
     pOut.write(" * Generated by MessagingSoapClientGenerator.\n");
     pOut.write(" */\n\n");
 
-    pOut.write("package "); pOut.write(pPkgname); pOut.write(";\n\n");
+    pOut.write("package ");
+    pOut.write(pPkgname);
+    pOut.write(";\n\n");
 
-    pOut.write("import java.net.URI;\n" +
-               "import java.util.concurrent.Future;\n\n" +
+    pOut.write("import java.net.URI;\n" + "import java.util.concurrent.Future;\n\n" +
 
-               "import javax.xml.bind.JAXBElement;\n" +
-               "import javax.xml.bind.JAXBException;\n"+
-               "import javax.xml.namespace.QName;\n" +
-               "import javax.xml.transform.Source;\n\n" +
+    "import javax.xml.bind.JAXBElement;\n" + "import javax.xml.bind.JAXBException;\n" + "import javax.xml.namespace.QName;\n"
+        + "import javax.xml.transform.Source;\n\n" +
 
-               "import net.devrieze.util.Tripple;\n\n" +
+        "import net.devrieze.util.Tripple;\n\n" +
 
-               "import nl.adaptivity.messaging.CompletionListener;\n" +
-               "import nl.adaptivity.messaging.Endpoint;\n" +
-               "import nl.adaptivity.messaging.EndPointDescriptor;\n" +
-               "import nl.adaptivity.messaging.MessagingRegistry;\n" +
-               "import nl.adaptivity.messaging.SendableSoapSource;\n" +
-               "import nl.adaptivity.ws.soap.SoapHelper;\n\n");
+        "import nl.adaptivity.messaging.CompletionListener;\n" + "import nl.adaptivity.messaging.Endpoint;\n"
+        + "import nl.adaptivity.messaging.EndPointDescriptor;\n" + "import nl.adaptivity.messaging.MessagingRegistry;\n"
+        + "import nl.adaptivity.messaging.SendableSoapSource;\n" + "import nl.adaptivity.ws.soap.SoapHelper;\n\n");
 
-    pOut.write("public class "); pOut.write(pOutClass); pOut.write(" {\n\n");
+    pOut.write("public class ");
+    pOut.write(pOutClass);
+    pOut.write(" {\n\n");
 
     // Write service location constants / variables.
     boolean finalService = false;
     try {
-      Endpoint instance = (Endpoint) pEndpointClass.newInstance();
+      final Endpoint instance = (Endpoint) pEndpointClass.newInstance();
 
-      pOut.write("  private static final QName SERVICE = "+qnamestring(instance.getServiceName())+";\n");
-      pOut.write(appendString(new StringBuilder("  private static final String ENDPOINT = "),instance.getEndpointName()).append(";\n").toString());
-      if (instance.getEndpointLocation()!=null) {
-        pOut.write(appendString(new StringBuilder("  private static final URI LOCATION = URI.create("),instance.getEndpointLocation().toString()).append(");\n\n").toString());
+      pOut.write("  private static final QName SERVICE = " + qnamestring(instance.getServiceName()) + ";\n");
+      pOut.write(appendString(new StringBuilder("  private static final String ENDPOINT = "), instance.getEndpointName()).append(";\n").toString());
+      if (instance.getEndpointLocation() != null) {
+        pOut.write(appendString(new StringBuilder("  private static final URI LOCATION = URI.create("), instance.getEndpointLocation().toString()).append(");\n\n").toString());
       } else {
         pOut.write("  private static final URI LOCATION = null;\n\n");
       }
       finalService = true;
-    } catch (ClassCastException e) { /* Ignore failure to instantiate. We just generate different code. */
-    } catch (InstantiationException e) { /* Ignore failure to instantiate. We just generate different code. */
-    } catch (IllegalAccessException e) { /* Ignore failure to instantiate. We just generate different code. */
+    } catch (final ClassCastException e) { /*
+                                            * Ignore failure to instantiate. We
+                                            * just generate different code.
+                                            */
+    } catch (final InstantiationException e) { /*
+                                                * Ignore failure to instantiate.
+                                                * We just generate different
+                                                * code.
+                                                */
+    } catch (final IllegalAccessException e) { /*
+                                                * Ignore failure to instantiate.
+                                                * We just generate different
+                                                * code.
+                                                */
       e.printStackTrace();
       System.exit(1);
     }
@@ -232,7 +254,9 @@ public class MessagingSoapClientGenerator {
     }
 
     // Constructor
-    pOut.write("  private "); pOut.write(pOutClass); pOut.write("() { }\n\n");
+    pOut.write("  private ");
+    pOut.write(pOutClass);
+    pOut.write("() { }\n\n");
 
     writeMethods(pOut, pEndpointClass);
 
@@ -249,55 +273,67 @@ public class MessagingSoapClientGenerator {
     pOut.write("}\n");
   }
 
-  private static void writeMethods(Writer pOut, Class<?> pEndpointClass) throws IOException {
-    for(Method method:pEndpointClass.getMethods()) {
-      WebMethod annotation = method.getAnnotation(WebMethod.class);
-      if (annotation!=null) {
+  private static void writeMethods(final Writer pOut, final Class<?> pEndpointClass) throws IOException {
+    for (final Method method : pEndpointClass.getMethods()) {
+      final WebMethod annotation = method.getAnnotation(WebMethod.class);
+      if (annotation != null) {
         writeMethod(pOut, method, annotation);
       }
     }
   }
 
-  private static void writeMethod(Writer pOut, Method pMethod, WebMethod pAnnotation) throws IOException {
+  private static void writeMethod(final Writer pOut, final Method pMethod, final WebMethod pAnnotation) throws IOException {
     String methodName = pAnnotation.operationName();
     String principalName = null;
-    if (methodName==null || methodName.length()==0) { methodName = pMethod.getName(); }
+    if ((methodName == null) || (methodName.length() == 0)) {
+      methodName = pMethod.getName();
+    }
     pOut.write("  public static Future<");
     pOut.write(pMethod.getReturnType().getCanonicalName());
-    pOut.write("> "); pOut.write(methodName); pOut.write("(");
-    boolean firstParam  = true;
-    List<ParamInfo> params = new ArrayList<>();
+    pOut.write("> ");
+    pOut.write(methodName);
+    pOut.write("(");
+    boolean firstParam = true;
+    final List<ParamInfo> params = new ArrayList<>();
     {
       int paramNo = 0;
-      for(Class<?> paramType: pMethod.getParameterTypes()) {
+      for (final Class<?> paramType : pMethod.getParameterTypes()) {
         String name = null;
         boolean isPrincipal = false;
-        for(Annotation annotation: pMethod.getParameterAnnotations()[paramNo]) {
+        for (final Annotation annotation : pMethod.getParameterAnnotations()[paramNo]) {
           if (annotation instanceof WebParam) {
-            WebParam webparam = (WebParam) annotation;
-            if (webparam.name()!=null) {
+            final WebParam webparam = (WebParam) annotation;
+            if (webparam.name() != null) {
               name = webparam.name();
             }
           }
           if (annotation instanceof RestParam) {
-            RestParam restParam = (RestParam) annotation;
-            if(restParam.type()==ParamType.PRINCIPAL) {
+            final RestParam restParam = (RestParam) annotation;
+            if (restParam.type() == ParamType.PRINCIPAL) {
               // We nead a principal header
               isPrincipal = true;
-              if (name==null || name.length()==0) { name=restParam.name(); }
-              if (name==null || name.length()==0) { name="principal"; }
+              if ((name == null) || (name.length() == 0)) {
+                name = restParam.name();
+              }
+              if ((name == null) || (name.length() == 0)) {
+                name = "principal";
+              }
             }
           }
         }
-        if (name==null||name.length()==0) {
-          name = "param"+paramNo;
+        if ((name == null) || (name.length() == 0)) {
+          name = "param" + paramNo;
         }
         if (isPrincipal) {
           principalName = name;
         } else {
           params.add(new ParamInfo(paramType, name));
         }
-        if (firstParam) { firstParam=false; } else { pOut.write(", "); }
+        if (firstParam) {
+          firstParam = false;
+        } else {
+          pOut.write(", ");
+        }
 
         pOut.write(paramType.getCanonicalName());
         pOut.write(' ');
@@ -308,9 +344,9 @@ public class MessagingSoapClientGenerator {
     }
     pOut.write(", CompletionListener completionListener) throws JAXBException {\n");
     {
-      int paramNo=0;
-      for(ParamInfo param:params) {
-        pOut.write("    final Tripple<String, Class<?>, Object> param"+paramNo +" = Tripple.<String, Class<?>, Object>tripple(");
+      int paramNo = 0;
+      for (final ParamInfo param : params) {
+        pOut.write("    final Tripple<String, Class<?>, Object> param" + paramNo + " = Tripple.<String, Class<?>, Object>tripple(");
         pOut.write(appendString(new StringBuilder(), param.name).append(", ").toString());
         if (param.type.isArray()) {
           pOut.write("Array.class, ");
@@ -331,19 +367,20 @@ public class MessagingSoapClientGenerator {
     pOut.write("\n");
     pOut.write("    @SuppressWarnings(\"unchecked\")\n");
     pOut.write("    Source message = SoapHelper.createMessage(new QName(");
-    if (pAnnotation.operationName()!=null) {
+    if (pAnnotation.operationName() != null) {
       pOut.write(appendString(new StringBuilder(), pAnnotation.operationName()).append("), ").toString());
     } else {
       pOut.write(appendString(new StringBuilder(), pMethod.getName()).append("), ").toString());
     }
-    if (principalName!=null) {
-      pOut.write("java.util.Arrays.asList(new JAXBElement<String>(new QName(\"http://adaptivity.nl/ProcessEngine/\",\"principal\"), String.class, "+principalName+".getName())), ");
+    if (principalName != null) {
+      pOut.write("java.util.Arrays.asList(new JAXBElement<String>(new QName(\"http://adaptivity.nl/ProcessEngine/\",\"principal\"), String.class, "
+          + principalName + ".getName())), ");
     }
-    for(int i=0; i<params.size(); ++i) {
-      if (i>0) {
+    for (int i = 0; i < params.size(); ++i) {
+      if (i > 0) {
         pOut.write(", ");
       }
-      pOut.write("param"+i);
+      pOut.write("param" + i);
     }
     pOut.write(");\n\n");
 
@@ -358,26 +395,26 @@ public class MessagingSoapClientGenerator {
 
   }
 
-  private static String qnamestring(QName pQName) {
-    StringBuilder result = new StringBuilder();
+  private static String qnamestring(final QName pQName) {
+    final StringBuilder result = new StringBuilder();
 
     result.append("new QName(");
     appendString(result, pQName.getNamespaceURI()).append(", ");
     appendString(result, pQName.getLocalPart());
-    if (pQName.getPrefix()!=null) {
-      appendString(result.append(", "),pQName.getPrefix());
+    if (pQName.getPrefix() != null) {
+      appendString(result.append(", "), pQName.getPrefix());
     }
     result.append(')');
     return result.toString();
   }
 
-  private static StringBuilder appendString(StringBuilder pResult, String pString) {
-    if (pString==null) {
+  private static StringBuilder appendString(final StringBuilder pResult, final String pString) {
+    if (pString == null) {
       pResult.append("null");
     } else {
       pResult.append('"');
-      for(int i=0; i<pString.length(); ++i) {
-        char c = pString.charAt(i);
+      for (int i = 0; i < pString.length(); ++i) {
+        final char c = pString.charAt(i);
         switch (c) {
           case '\\':
             pResult.append("\\\\");
