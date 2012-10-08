973f0bccb2d98382698b6475cd6b0c365a378164
Paul de Vrieze <paul.devrieze@gmail.com>
2012-10-08 12:49:15 +0000

    Create the tables needed to store process instance information.
    git-svn-id: https://darwin.bournemouth.ac.uk/pdvrieze/svn/darwin/trunk/sql@294 3bae1aa3-1c52-4c45-a669-1fb9b31e7360

---
+++ darwin-sql/processengine/processinstances.sql
@@ -0,0 +1,30 @@
+DROP TABLE `processinstances`;
+DROP TABLE `processnodeinstances`;
+DROP TABLE `nodedata`;
+
+CREATE TABLE `processinstances` (
+  `pihandle` BIGINT NOT NULL AUTO_INCREMENT,
+  `owner` varchar(30) NOT NULL,
+  `pmhandle` INT NOT NULL,
+  INDEX ( `owner` ),
+  PRIMARY KEY ( `pihandle` ),
+  FOREIGN KEY ( `pmhandle` ) REFERENCES `processmodels` ( `pmhandle` )
+) ENGINE=InnoDB CHARSET=utf8;
+
+CREATE TABLE `processnodeinstances` (
+  `pnihandle` BIGINT NOT NULL AUTO_INCREMENT,
+  `pihandle` BIGINT NOT NULL,
+  `nodeid` VARCHAR(30) NOT NULL,
+  `predecessor` BIGINT,
+  PRIMARY KEY ( `pnihandle` ),
+  FOREIGN KEY ( `predecessor` ) REFERENCES `processnodeinstances` ( `pnihandle` )
+) ENGINE=InnoDB CHARSET=utf8;
+
+CREATE TABLE `nodedata` (
+  `name` VARCHAR(30) NOT NULL,
+  `pnihandle` BIGINT NOT NULL,
+  `data` TEXT NOT NULL,
+  PRIMARY KEY ( `name`, `pnihandle` ),
+  FOREIGN KEY ( `pnihandle` ) REFERENCES `processnodeinstances` ( `pnihandle` )
+  
+) ENGINE=InnoDB CHARSET=utf8;
