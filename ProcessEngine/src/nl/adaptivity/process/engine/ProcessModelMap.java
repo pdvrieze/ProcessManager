e0f004c596a4e8062d2d71a41d0dadc1d7a7f42c
Paul de Vrieze <paul@devrieze.net>
2012-10-10 21:21:58 +0100

    Adapt to work with the new DbSet setup for DbHandleMap

--- ProcessEngine/src/nl/adaptivity/process/engine/ProcessModelMap.java
+++ ProcessEngine/src/nl/adaptivity/process/engine/ProcessModelMap.java
@@ -8,7 +8,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.util.Arrays;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -16,28 +15,147 @@ import javax.sql.DataSource;
 import javax.xml.bind.JAXB;
 
 import net.devrieze.util.CachingDBHandleMap;
-import net.devrieze.util.DBHandleMap.ElementFactory;
 import net.devrieze.util.StringCache;
+import net.devrieze.util.db.AbstractElementFactory;
 import net.devrieze.util.security.SimplePrincipal;
 
 import nl.adaptivity.process.processModel.ProcessModel;
 import nl.adaptivity.process.processModel.XmlProcessModel;
 
 
-public class ProcessModelMap extends CachingDBHandleMap<ProcessModel> implements ElementFactory<ProcessModel> {
+public class ProcessModelMap extends CachingDBHandleMap<ProcessModel> {
+
 
   private static final String TABLE = "processmodels";
+
   private static final String COL_HANDLE = "pmhandle";
+
   private static final String COL_OWNER = "owner";
+
   private static final String COL_MODEL = "model";
-  private int aColNoOwner;
-  private int aColNoModel;
-  private int aColNoHandle;
-  private final StringCache aStringCache;
+
+  static class ProcessModelFactory extends AbstractElementFactory<ProcessModel> {
+
+    private int aColNoOwner;
+    private int aColNoModel;
+    private int aColNoHandle;
+    private final StringCache aStringCache;
+
+    ProcessModelFactory(StringCache pStringCache) {
+      aStringCache = pStringCache;
+    }
+
+    @Override
+    public CharSequence getTableName() {
+      return TABLE;
+    }
+
+    @Override
+    public String getFilterExpression() {
+      // TODO implement more carefully
+      return null;
+//      if (aFilterUser==null) {
+//        return null;
+//      } else {
+//
+//      }
+    }
+
+    @Override
+    public int setFilterParams(PreparedStatement pStatement, int pOffset) {
+      throw new UnsupportedOperationException(); // this should not be called
+    }
+
+    @Override
+    public void initResultSet(ResultSetMetaData pMetaData) {
+      try {
+        final int columnCount = pMetaData.getColumnCount();
+        for (int i=1; i<=columnCount;++i) {
+          String colName = pMetaData.getColumnName(i);
+          if (COL_HANDLE.equals(colName)) {
+            aColNoHandle = i;
+          } else if (COL_OWNER.equals(colName)) {
+            aColNoOwner = i;
+          } else if (COL_MODEL.equals(colName)) {
+            aColNoModel = i;
+          } // ignore other columns
+        }
+      } catch (SQLException e) {
+        throw new RuntimeException(e);
+      }
+    }
+
+    @Override
+    public ProcessModel create(DataSource pConnectionProvider, ResultSet pRow) throws SQLException {
+      Principal owner = new SimplePrincipal(aStringCache.lookup(pRow.getString(aColNoOwner)));
+      Reader modelReader = pRow.getCharacterStream(aColNoModel);
+      long handle = pRow.getLong(aColNoHandle);
+
+      XmlProcessModel xmlModel = JAXB.unmarshal(modelReader, XmlProcessModel.class);
+      ProcessModel result = xmlModel.toProcessModel();
+
+      result.setHandle(handle);
+      result.cacheStrings(aStringCache);
+      result.setOwner(owner);
+      return result;
+    }
+
+    @Override
+    public CharSequence getPrimaryKeyCondition(ProcessModel pObject) {
+      return getHandleCondition(pObject.getHandle());
+    }
+
+    @Override
+    public int setPrimaryKeyParams(PreparedStatement pStatement, ProcessModel pElement, int pOffset) throws SQLException {
+      return setHandleParams(pStatement, pElement.getHandle(), pOffset);
+    }
+
+    @Override
+    public ProcessModel asInstance(Object pObject) {
+      if (pObject instanceof ProcessModel) {
+        return (ProcessModel) pObject;
+      } else {
+        return null;
+      }
+    }
+
+    @Override
+    public CharSequence getCreateColumns() {
+      return COL_OWNER + ", " + COL_MODEL;
+    }
+
+    @Override
+    public CharSequence getCreateParamHolders() {
+      return "?, ?";
+    }
+
+    @Override
+    public int setStoreParams(PreparedStatement pStatement, ProcessModel pElement, int pOffset) throws SQLException {
+      pStatement.setString(pOffset, pElement.getOwner().getName());
+
+      // TODO see if this can be done in a streaming way.
+      XmlProcessModel xmlModel = new XmlProcessModel(pElement);
+      CharArrayWriter out = new CharArrayWriter();
+      JAXB.marshal(xmlModel, out);
+      pStatement.setCharacterStream(pOffset+1, new CharArrayReader(out.toCharArray()));
+      return 2;
+    }
+
+    @Override
+    public CharSequence getHandleCondition(long pHandle) {
+      return aColNoHandle + " = ?";
+    }
+
+    @Override
+    public int setHandleParams(PreparedStatement pStatement, long pHandle, int pOffset) throws SQLException {
+      pStatement.setLong(pOffset, pHandle);
+      return 1;
+    }
+
+  }
 
   public ProcessModelMap(String pResourceName, StringCache pStringCache) {
-    super(resourceNameToDataSource(pResourceName), null, TABLE, COL_HANDLE);
-    aStringCache = pStringCache;
+    super(resourceNameToDataSource(pResourceName), new ProcessModelFactory(pStringCache));
   }
 
   private static DataSource resourceNameToDataSource(String pResourceName) {
@@ -46,61 +164,6 @@ public class ProcessModelMap extends CachingDBHandleMap<ProcessModel> implements
     } catch (NamingException e) {
       throw new RuntimeException(e);
     }
-  }
-
-  @Override
-  public ProcessModel create(ResultSet pRow) throws SQLException {
-    Principal owner = new SimplePrincipal(aStringCache.lookup(pRow.getString(aColNoOwner)));
-    Reader modelReader = pRow.getCharacterStream(aColNoModel);
-    long handle = pRow.getLong(aColNoHandle);
-
-    XmlProcessModel xmlModel = JAXB.unmarshal(modelReader, XmlProcessModel.class);
-    ProcessModel result = xmlModel.toProcessModel();
-
-    result.setHandle(handle);
-    result.cacheStrings(aStringCache);
-    result.setOwner(owner);
-    return result;
-  }
-
-  @Override
-  public void init(ResultSetMetaData pMetaData) {
-    try {
-      final int columnCount = pMetaData.getColumnCount();
-      for (int i=1; i<=columnCount;++i) {
-        String colName = pMetaData.getColumnName(i);
-        if (COL_HANDLE.equals(colName)) {
-          aColNoHandle = i;
-        } else if (COL_OWNER.equals(colName)) {
-          aColNoOwner = i;
-        } else if (COL_MODEL.equals(colName)) {
-          aColNoModel = i;
-        } // ignore other columns
-      }
-    } catch (SQLException e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  @Override
-  public boolean isInstance(Object pObject) {
-    return pObject instanceof ProcessInstance;
-  }
-
-  @Override
-  public Iterable<String> getInsertColumns() {
-    return Arrays.asList(COL_OWNER, COL_MODEL);
-  }
-
-  @Override
-  public void insertColumnValues(PreparedStatement pStatement, ProcessModel pElement) throws SQLException {
-    pStatement.setString(1, pElement.getOwner().getName());
-
-    // TODO see if this can be done in a streaming way.
-    XmlProcessModel xmlModel = new XmlProcessModel(pElement);
-    CharArrayWriter out = new CharArrayWriter();
-    JAXB.marshal(xmlModel, out);
-    pStatement.setCharacterStream(2, new CharArrayReader(out.toCharArray()));
   }
 
 }
