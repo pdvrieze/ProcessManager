8c283188362af916292a36bb0d0b9a4f486ac820
Paul de Vrieze <paul.devrieze@gmail.com>
2012-10-13 19:37:01 +0000

    User groups are in the user_role table. Don't use them here.
    git-svn-id: https://darwin.bournemouth.ac.uk/pdvrieze/svn/darwin/trunk/sql@328 3bae1aa3-1c52-4c45-a669-1fb9b31e7360

--- darwin-sql/webauth/users.sql
+++ darwin-sql/webauth/users.sql
@@ -2,6 +2,5 @@ DROP TABLE `users`;
 CREATE TABLE `users` (
   `user` varchar(30) NOT NULL,
   `password` varchar(40) NOT NULL,
-  `user_group` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`user`) USING HASH
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
