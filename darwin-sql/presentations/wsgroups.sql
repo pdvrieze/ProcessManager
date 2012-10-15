5b39aadc9392e5fa8cfbf13219410ad7a9a47d74
Paul de Vrieze <paul.devrieze@gmail.com>
2012-10-15 19:33:48 +0000

    Add sql for the web ystems unit
    git-svn-id: https://darwin.bournemouth.ac.uk/pdvrieze/svn/darwin/trunk/sql@351 3bae1aa3-1c52-4c45-a669-1fb9b31e7360

---
+++ darwin-sql/presentations/wsgroups.sql
@@ -0,0 +1,25 @@
+DROP TABLE wsgroups;
+DROP TABLE slots;
+DROP TABLE members;
+
+CREATE TABLE `slots` (
+  `slot` INT NOT NULL AUTO_INCREMENT,
+  `description` VARCHAR(80),
+  `date` DATE,
+  PRIMARY KEY ( `slot` )
+) ENGINE=InnoDB CHARSET=utf8;
+
+CREATE TABLE `wsgroups` (
+  `hgroup` INT NOT NULL AUTO_INCREMENT,
+  `topic` MEDIUMTEXT,
+  `slot` INT,
+  PRIMARY KEY ( `hgroup` ),
+  FOREIGN KEY ( `slot` ) REFERENCES `slots` (`slot`)
+) ENGINE=InnoDB CHARSET=utf8;
+
+CREATE TABLE `members` (
+  `user` VARCHAR(30) NOT NULL,
+  `hgroup` INT NOT NULL,
+  PRIMARY KEY( `user` ),
+  FOREIGN KEY( `hgroup` ) REFERENCES `wsgroups` ( `hgroup` )
+) ENGINE=InnoDB CHARSET=utf8;
