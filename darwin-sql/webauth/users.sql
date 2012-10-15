58a2578bbb6daa3e31963a2a5a6a175730a5dfe8
Paul de Vrieze <paul.devrieze@gmail.com>
2012-10-15 16:14:06 +0000

    Add field for full user names.
    git-svn-id: https://darwin.bournemouth.ac.uk/pdvrieze/svn/darwin/trunk/sql@349 3bae1aa3-1c52-4c45-a669-1fb9b31e7360

--- darwin-sql/webauth/users.sql
+++ darwin-sql/webauth/users.sql
@@ -1,6 +1,7 @@
 DROP TABLE `users`;
 CREATE TABLE `users` (
   `user` varchar(30) NOT NULL,
+  `fullname` varchar(80),
   `password` varchar(40) NOT NULL,
   PRIMARY KEY (`user`) USING HASH
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
