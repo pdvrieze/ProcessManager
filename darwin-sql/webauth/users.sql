5c44681d01d809e231099da9e7e07c2a7a2eb468
Paul de Vrieze <paul.devrieze@gmail.com>
2013-09-17 21:47:20 +0000

    Work on a password reset function.
    git-svn-id: https://darwin.bournemouth.ac.uk/pdvrieze/svn/darwin/trunk/sql@718 3bae1aa3-1c52-4c45-a669-1fb9b31e7360

--- darwin-sql/webauth/users.sql
+++ darwin-sql/webauth/users.sql
@@ -3,5 +3,7 @@ CREATE TABLE `users` (
   `user` varchar(30) NOT NULL,
   `fullname` varchar(80),
   `password` varchar(40) NOT NULL,
+  `resettoken` VARCHAR(20),
+  `resettime` DATETIME,
   PRIMARY KEY (`user`) USING HASH
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
