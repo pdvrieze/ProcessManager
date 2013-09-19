5ed0d5708b15709203b5dda0166b25a42d31a101
Paul de Vrieze <paul.devrieze@gmail.com>
2013-09-19 16:20:42 +0000

    Make the annotations applicable to methods (applies to return value).
    git-svn-id: https://darwin.bournemouth.ac.uk/pdvrieze/svn/darwin/projects/JavaCommonApi/trunk@738 3bae1aa3-1c52-4c45-a669-1fb9b31e7360

--- JavaCommonApi/src/net/devrieze/annotations/NotNull.java
+++ JavaCommonApi/src/net/devrieze/annotations/NotNull.java
@@ -1,12 +1,13 @@
 package net.devrieze.annotations;
 
-import static java.lang.annotation.ElementType.*;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import static java.lang.annotation.ElementType.*;
+
 @Retention(RetentionPolicy.CLASS)
-@Target({PARAMETER, FIELD, LOCAL_VARIABLE})
+@Target({PARAMETER, FIELD, LOCAL_VARIABLE, METHOD})
 public @interface NotNull {
   /* Just a marker interface */
 }
