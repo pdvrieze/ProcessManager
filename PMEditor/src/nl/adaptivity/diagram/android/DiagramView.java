d36808639e887245f21ed6afff4247eab5cdaf99
Paul de Vrieze <paul.devrieze@gmail.com>
2013-09-28 21:22:00 +0000

    Display the build time slightly neater.
    git-svn-id: https://darwin.bournemouth.ac.uk/pdvrieze/svn/darwin/projects/PMEditor/trunk@844 3bae1aa3-1c52-4c45-a669-1fb9b31e7360

--- PMEditor/src/nl/adaptivity/diagram/android/DiagramView.java
+++ PMEditor/src/nl/adaptivity/diagram/android/DiagramView.java
@@ -6,6 +6,7 @@ import java.util.Properties;
 
 import nl.adaptivity.diagram.Diagram;
 import nl.adaptivity.diagram.Rectangle;
+import nl.adaptivity.process.editor.android.BuildConfig;
 import nl.adaptivity.process.editor.android.R;
 import android.content.Context;
 import android.graphics.Canvas;
@@ -20,6 +21,7 @@ public class DiagramView extends View {
 
   private Diagram aDiagram;
   private Paint aRed;
+  private Paint aTimePen;
   private Rect aBounds = new Rect();
 
   public DiagramView(Context pContext, AttributeSet pAttrs, int pDefStyle) {
@@ -46,40 +48,51 @@ public class DiagramView extends View {
   public void draw(Canvas pCanvas) {
     super.draw(pCanvas);
 //    pCanvas.drawLine(200, 0, 0, 200, aRed);
-    if (aRed==null) {
-      aRed = new Paint(Paint.ANTI_ALIAS_FLAG|Paint.SUBPIXEL_TEXT_FLAG);
-      aRed.setARGB(255, 255, 0, 0);
-      aRed.setTypeface(Typeface.DEFAULT);
-      aRed.setTextSize(50f);
-    }
     if (aDiagram!=null) {
       final Rectangle clipBounds = new Rectangle(0d, 0d, getHeight(), getWidth());
       final AndroidCanvas canvas = new AndroidCanvas(pCanvas);
       aDiagram.draw(canvas, clipBounds);
     } else {
+      if (aRed==null) {
+        aRed = new Paint(Paint.ANTI_ALIAS_FLAG|Paint.SUBPIXEL_TEXT_FLAG);
+        aRed.setARGB(255, 255, 0, 0);
+        aRed.setTypeface(Typeface.DEFAULT);
+        aRed.setTextSize(50f);
+      }
       final String text = getContext().getResources().getString(R.string.missing_diagram);
       aRed.getTextBounds(text, 0, text.length(), aBounds);
       pCanvas.drawText(text, (getWidth()-aBounds.width())/2, (getHeight()-aBounds.height())/2, aRed);
       pCanvas.drawCircle(100, 100, 75, aRed);
     }
-    InputStream stream = getClass().getClassLoader().getResourceAsStream("nl/adaptivity/process/diagram/version.properties");
-    if (stream!=null) {
-      try {
-        Properties props = new Properties();
+    if (BuildConfig.DEBUG) {
+      InputStream stream = getClass().getClassLoader().getResourceAsStream("nl/adaptivity/process/diagram/version.properties");
+      if (stream!=null) {
         try {
-          props.load(stream);
-        } catch (IOException e) {
-          e.printStackTrace();
-          return;
-        }
-        String buildtime = props.getProperty("buildtime");
-        aRed.getTextBounds(buildtime, 0, buildtime.length(), aBounds);
-        pCanvas.drawText(buildtime, getWidth() - aBounds.width()-20, getHeight()-aBounds.height()-20, aRed);
-      } finally {
-        try {
-          stream.close();
-        } catch (IOException e) {
-          e.printStackTrace();
+          Properties props = new Properties();
+          try {
+            props.load(stream);
+          } catch (IOException e) {
+            e.printStackTrace();
+            return;
+          }
+          String buildtime = props.getProperty("buildtime");
+          if (buildtime!=null) {
+            if (aTimePen==null) {
+              aTimePen = new Paint(Paint.ANTI_ALIAS_FLAG|Paint.SUBPIXEL_TEXT_FLAG);
+              aTimePen.setARGB(255, 0, 0, 31);
+              aTimePen.setTypeface(Typeface.DEFAULT);
+              aTimePen.setTextSize(25f);
+            }
+
+            aTimePen.getTextBounds(buildtime, 0, buildtime.length(), aBounds);
+            pCanvas.drawText(buildtime, getWidth() - aBounds.width()-20, getHeight()-20, aTimePen);
+          }
+        } finally {
+          try {
+            stream.close();
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
         }
       }
     }
