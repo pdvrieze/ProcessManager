cb080615160d6fb443fd0cf7300be63184469e4c
Paul de Vrieze <paul.devrieze@gmail.com>
2013-09-28 13:14:25 +0000

    Use the new algorithm classs to draw nodes.
    git-svn-id: https://darwin.bournemouth.ac.uk/pdvrieze/svn/darwin/projects/PE-diagram/trunk@829 3bae1aa3-1c52-4c45-a669-1fb9b31e7360

--- PE-diagram/src/nl/adaptivity/process/clientProcessModel/ClientProcessModel.java
+++ PE-diagram/src/nl/adaptivity/process/clientProcessModel/ClientProcessModel.java
@@ -3,13 +3,15 @@ package nl.adaptivity.process.clientProcessModel;
 import java.security.Principal;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 
 import net.devrieze.util.CollectionUtil;
 
 import nl.adaptivity.diagram.Bounded;
-import nl.adaptivity.diagram.Point;
+import nl.adaptivity.diagram.Rectangle;
+import nl.adaptivity.process.diagram.DiagramNode;
 import nl.adaptivity.process.diagram.LayoutAlgorithm;
 import nl.adaptivity.process.processModel.EndNode;
 import nl.adaptivity.process.processModel.ProcessModel;
@@ -239,360 +241,51 @@ public class ClientProcessModel<T extends IClientProcessNode<T>> implements Proc
   }
 
   protected void layout() {
-    boolean changed = Double.isNaN(aInnerHeight)|| Double.isNaN(aInnerWidth);
-    for (final T node : aNodes) {
-      if (Double.isNaN(node.getX()) || Double.isNaN(node.getY())) {
-        changed = true;
-        layoutNode(node, true, true); // always force as that should be slightly more efficient
+    aLayoutAlgorithm.layout(toDiagramNodes(getModelNodes()));
+  }
+
+
+  private List<DiagramNode> toDiagramNodes(Collection<? extends T> pModelNodes) {
+    HashMap<T,DiagramNode> map = new HashMap<T, DiagramNode>();
+    List<DiagramNode> result = new ArrayList<DiagramNode>();
+    for(T node:pModelNodes) {
+      final double leftExtend;
+      final double rightExtend;
+      final double topExtend;
+      final double bottomExtend;
+      if (node instanceof Bounded) {
+        Rectangle bounds = ((Bounded)node).getBounds();
+        leftExtend = node.getX()-bounds.left;
+        rightExtend = bounds.right()-node.getX();
+        topExtend = node.getY()-bounds.top;
+        bottomExtend = bounds.bottom()-node.getY();
+      } else {
+        leftExtend = rightExtend = aLayoutAlgorithm.getDefaultNodeWidth()/2;
+        topExtend = bottomExtend = aLayoutAlgorithm.getDefaultNodeHeight()/2;
       }
+      DiagramNode dn = new DiagramNode(node, leftExtend, rightExtend, topExtend, bottomExtend);
+      if (node.getId()!=null) {
+        map.put(node, dn);
+      }
+      result.add(dn);
     }
-    if (changed) {
-      double minX = Double.MAX_VALUE;
-      double minY = Double.MAX_VALUE;
-      double maxX = Double.MIN_VALUE;
-      double maxY = Double.MIN_VALUE;
-      for (final T node : aNodes) {
-        minX = Math.min(node.getX()-leftDistance(node), minX);
-        minY = Math.min(node.getY()-topDistance(node), minY);
-        maxX = Math.max(node.getX()+rightDistance(node), maxX);
-        maxY = Math.max(node.getY()+bottomDistance(node), maxY);
-      }
-      final double offsetX = aLeftPadding - minX;
-      final double offsetY = aTopPadding - minY;
 
-      aInnerWidth = maxX - minX;
-      aInnerHeight = maxY - minY;
-
-      if (Math.abs(offsetX)>TOLERANCE || Math.abs(offsetY)>TOLERANCE) {
-        for (final T node : aNodes) {
-          node.setX(node.getX()+offsetX);
-          node.setY(node.getY()+offsetY);
+    int i=0;
+    for(T mn:pModelNodes) {
+      DiagramNode dn = result.get(i);
+      for(T successor:mn.getSuccessors()) {
+        DiagramNode rightdn = map.get(successor);
+        if (rightdn!=null) {
+          dn.getRightNodes().add(rightdn);
         }
       }
-    }
-  }
-
-
-  private void layoutNode(T pNode, boolean forceX, boolean forceY) {
-    List<Point> leftPoints = getLeftPoints(pNode);
-    List<Point> abovePoints = getAbovePoints(pNode);
-    List<Point> rightPoints = getRightPoints(pNode);
-    List<Point> belowPoints = getBelowPoints(pNode);
-
-    double minY = maxY(abovePoints)+getVertSeparation() + topDistance(pNode);
-    double maxY = minY(belowPoints)-getVertSeparation() - bottomDistance(pNode);
-    double minX = maxX(leftPoints)+getHorizSeparation() + leftDistance(pNode);
-    double maxX = minX(rightPoints)-getHorizSeparation() - rightDistance(pNode);
-
-    double x = pNode.getX();
-    double y = pNode.getY();
-
-    if (leftPoints.isEmpty()) {
-      if (rightPoints.isEmpty()) {
-        if (forceX || Double.isNaN(x)|| x<minX || x>maxX) {
-          x = averageX(abovePoints, belowPoints, 0d);
-        }
-        if (forceY || Double.isNaN(y) || y<minY || y>maxY) {
-          if (abovePoints.isEmpty()) {
-            if (! belowPoints.isEmpty()) {
-              y = maxY;
-            } // otherwise keep it where it is
-          } else { // abovePoints not empty
-            if (belowPoints.isEmpty()) {
-              y = minY;
-            } else {
-              y = Math.max(minY, (minY+maxY)/2);
-            }
-          }
-        }
-
-      } else { // leftPoints empty, rightPoints not empty
-        if (forceY || Double.isNaN(y)|| y<minY || y>maxY) {
-          y = Math.max(minY, averageY(rightPoints));
-        }
-        if (forceX || Double.isNaN(x)|| x<minX || x>maxX) {
-          x = Math.min(averageX(abovePoints, belowPoints,Double.POSITIVE_INFINITY),maxX);
+      for(T predecessor:mn.getPredecessors()) {
+        DiagramNode leftdn = map.get(predecessor);
+        if (leftdn!=null) {
+          dn.getLeftNodes().add(leftdn);
         }
       }
-    } else { // leftPoints not empty
-      if (forceY || Double.isNaN(y) || y<minY || y>maxY) {
-        if (leftPoints.size()==1) {
-          y = Math.max(minY, averageY(leftPoints));
-        } else {
-          if (rightPoints.size()==1) {
-            y = Math.max(minY, averageY(rightPoints));
-          } else {
-            y = Math.max(minY, averageY(leftPoints, rightPoints, 0));
-          }
-        }
-      }
-      if (forceX || Double.isNaN(x)|| x<minX || x>maxX) {
-        if (rightPoints.isEmpty()) {
-          x = Math.max(averageX(abovePoints, belowPoints,Double.NEGATIVE_INFINITY),minX);
-        } else {
-          x = Math.max(minX, (minX+maxX)/2);
-        }
-      }
-    }
-    if (Double.isNaN(x)) { x = 0d; }
-    if (Double.isNaN(y)) { y = 0d; }
-    boolean xChanged = changed(x, pNode.getX(), TOLERANCE);
-    boolean yChanged = changed(y, pNode.getY(), TOLERANCE);
-    if (yChanged || xChanged) {
-      System.err.println("Moving node "+pNode+ "to ("+x+", "+y+')');
-      pNode.setX(x);
-      pNode.setY(y);
-      for(T n:getPrecedingSiblings(pNode)) {
-        layoutNode(n, xChanged, yChanged && y<minY);
-      }
-      for(T n:getFollowingSiblings(pNode)) {
-        layoutNode(n, xChanged, yChanged && y>maxY);
-      }
-      for(T n:pNode.getPredecessors()) {
-        layoutNode(n, xChanged && x<minX, yChanged);
-      }
-      for(T n:pNode.getSuccessors()) {
-        layoutNode(n, xChanged && x>maxX, yChanged);
-      }
-    }
-  }
-
-  private static boolean changed(double pA, double pB, double pTolerance) {
-    if (Double.isNaN(pA)) { return !Double.isNaN(pB); }
-    if (Double.isNaN(pB)) { return true; }
-    return Math.abs(pA-pB)>pTolerance;
-  }
-
-  private double topDistance(T pNode) {
-    if ((!Double.isNaN(pNode.getY())) && pNode instanceof Bounded) {
-      return pNode.getY()-((Bounded) pNode).getBounds().top;
-    }
-    return aLayoutAlgorithm.getDefaultNodeHeight()/2;
-  }
-
-  private double bottomDistance(T pNode) {
-    if ((!Double.isNaN(pNode.getY()))&& pNode instanceof Bounded) {
-      return ((Bounded) pNode).getBounds().bottom()-pNode.getY();
-    }
-    return aLayoutAlgorithm.getDefaultNodeHeight()/2;
-  }
-
-  private double leftDistance(T pNode) {
-    if ((!Double.isNaN(pNode.getX()))&& pNode instanceof Bounded) {
-      return pNode.getX()-((Bounded) pNode).getBounds().left;
-    }
-    return aLayoutAlgorithm.getDefaultNodeWidth()/2;
-  }
-
-  private double rightDistance(T pNode) {
-    if ((!Double.isNaN(pNode.getX()))&& pNode instanceof Bounded) {
-      return ((Bounded) pNode).getBounds().right()-pNode.getX();
-    }
-    return aLayoutAlgorithm.getDefaultNodeWidth()/2;
-  }
-
-  private static double averageY(List<Point> pPoints) {
-    if (pPoints.isEmpty()) {
-      return Double.NaN;
-    } else {
-      double total = 0;
-      for(Point p: pPoints) { total+=p.y; }
-      return total/pPoints.size();
-    }
-  }
-
-  private static double averageY(List<Point> pPoints1, List<Point> pPoints2, double fallback) {
-    if (pPoints1.isEmpty() && pPoints2.isEmpty()) {
-      return fallback;
-    } else {
-      double total = 0;
-      for(Point p: pPoints1) { total+=p.y; }
-      for(Point p: pPoints2) { total+=p.y; }
-      return total/(pPoints1.size()+pPoints2.size());
-    }
-  }
-
-  private static double averageX(List<Point> pPoints) {
-    if (pPoints.isEmpty()) {
-      return Double.NaN;
-    } else {
-      double total = 0;
-      for(Point p: pPoints) { total+=p.x; }
-      return total/pPoints.size();
-    }
-  }
-
-  private static double averageX(List<Point> pPoints1, List<Point> pPoints2, double fallback) {
-    if (pPoints1.isEmpty() && pPoints2.isEmpty()) {
-      return fallback;
-    } else {
-      double total = 0;
-      for(Point p: pPoints1) { total+=p.x; }
-      for(Point p: pPoints2) { total+=p.x; }
-      return total/(pPoints1.size()+pPoints2.size());
-    }
-  }
-
-  private static double maxY(List<Point> pPoints) {
-    double result = Double.NEGATIVE_INFINITY;
-    for(Point p: pPoints) {
-      if (p.y>result) {
-        result = p.y;
-      }
-    }
-    return result;
-  }
-
-  private static double minY(List<Point> pPoints) {
-    double result = Double.POSITIVE_INFINITY;
-    for(Point p: pPoints) {
-      if (p.y<result) {
-        result = p.y;
-      }
-    }
-    return result;
-  }
-
-  private static double maxX(List<Point> pPoints) {
-    double result = Double.NEGATIVE_INFINITY;
-    for(Point p: pPoints) {
-      if (p.x>result) {
-        result = p.x;
-      }
-    }
-    return result;
-  }
-
-  private static double minX(List<Point> pPoints) {
-    double result = Double.POSITIVE_INFINITY;
-    for(Point p: pPoints) {
-      if (p.x<result) {
-        result = p.x;
-      }
-    }
-    return result;
-  }
-
-  private List<Point> getLeftPoints(T pNode) {
-    List<Point> result = new ArrayList<Point>();
-    for(T n:pNode.getPredecessors()) {
-      if (!(Double.isNaN(n.getX()) || Double.isNaN(n.getY()))) {
-        double x;
-        double y = n.getY();
-        if (n instanceof Bounded) {
-          x = ((Bounded) n).getBounds().right();
-        } else {
-          x = n.getX()+(aLayoutAlgorithm.getDefaultNodeWidth()/2);
-        }
-        result.add(new Point(x,y));
-      }
-    }
-    return result;
-  }
-
-  private List<Point> getRightPoints(T pNode) {
-    List<Point> result = new ArrayList<Point>();
-    for(T n:pNode.getSuccessors()) {
-      if (!(Double.isNaN(n.getX()) || Double.isNaN(n.getY()))) {
-        double x;
-        double y = n.getY();
-        if (n instanceof Bounded) {
-          x = ((Bounded) n).getBounds().left;
-        } else {
-          x = n.getX()-(aLayoutAlgorithm.getDefaultNodeWidth()/2);
-        }
-        result.add(new Point(x,y));
-      }
-    }
-    return result;
-  }
-
-  private List<Point> getAbovePoints(T pNode) {
-    List<Point> result = new ArrayList<Point>();
-    for(T n:getPrecedingSiblings(pNode)) {
-      if (!(Double.isNaN(n.getX()) || Double.isNaN(n.getY()))) {
-        double x = n.getX();
-        double y;
-        if (n instanceof Bounded) {
-          y = ((Bounded) n).getBounds().bottom();
-        } else {
-          y = n.getY()+(aLayoutAlgorithm.getDefaultNodeHeight()/2);
-        }
-        result.add(new Point(x,y));
-      }
-    }
-    return result;
-  }
-
-  private List<Point> getBelowPoints(T pNode) {
-    List<Point> result = new ArrayList<Point>();
-    for(T n:getFollowingSiblings(pNode)) {
-      if (!(Double.isNaN(n.getX()) || Double.isNaN(n.getY()))) {
-        double x = n.getX();
-        double y;
-        if (n instanceof Bounded) {
-          y = ((Bounded) n).getBounds().top;
-        } else {
-          y = n.getY()-(aLayoutAlgorithm.getDefaultNodeHeight()/2);
-        }
-        result.add(new Point(x,y));
-      }
-    }
-    return result;
-  }
-
-  private List<T> getPrecedingSiblings(T pNode) {
-    List<T> result = new ArrayList<T>();
-    for(T pred:pNode.getPredecessors()) {
-      if (pred.getSuccessors().contains(pNode)) {
-        for(T sibling: pred.getSuccessors()) {
-          if (sibling==pNode) {
-            break;
-          } else {
-            result.add(sibling);
-          }
-        }
-      }
-    }
-    for(T pred:pNode.getSuccessors()) {
-      if (pred.getPredecessors().contains(pNode)) {
-        for(T sibling: pred.getPredecessors()) {
-          if (sibling==pNode) {
-            break;
-          } else {
-            result.add(sibling);
-          }
-        }
-      }
-    }
-    return result;
-  }
-
-  private List<T> getFollowingSiblings(T pNode) {
-    List<T> result = new ArrayList<T>();
-    for(T successor:pNode.getPredecessors()) {
-      if (successor.getSuccessors().contains(pNode)) {
-        boolean following = false;
-        for(T sibling: successor.getSuccessors()) {
-          if (sibling==pNode) {
-            following = true;
-          } else if (following){
-            result.add(sibling);
-          }
-        }
-      }
-    }
-    for(T successor:pNode.getSuccessors()) {
-      if (successor.getPredecessors().contains(pNode)) {
-        boolean following = false;
-        for(T sibling: successor.getPredecessors()) {
-          if (sibling==pNode) {
-            following = true;
-          } else if (following){
-            result.add(sibling);
-          }
-        }
-      }
+      ++i;
     }
     return result;
   }
