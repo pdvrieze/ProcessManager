8abdab6e8b6dd97212c41102c815018ff82db1e7
Paul de Vrieze <paul.devrieze@gmail.com>
2013-09-28 21:16:03 +0000

    Add infrastructure to display the build time in the app
    git-svn-id: https://darwin.bournemouth.ac.uk/pdvrieze/svn/darwin/projects/PMEditor/trunk@843 3bae1aa3-1c52-4c45-a669-1fb9b31e7360

--- PMEditor/src/nl/adaptivity/diagram/android/DiagramView.java
+++ PMEditor/src/nl/adaptivity/diagram/android/DiagramView.java
@@ -1,11 +1,16 @@
 package nl.adaptivity.diagram.android;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import nl.adaptivity.diagram.Diagram;
 import nl.adaptivity.diagram.Rectangle;
 import nl.adaptivity.process.editor.android.R;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.view.View;
@@ -15,6 +20,7 @@ public class DiagramView extends View {
 
   private Diagram aDiagram;
   private Paint aRed;
+  private Rect aBounds = new Rect();
 
   public DiagramView(Context pContext, AttributeSet pAttrs, int pDefStyle) {
     super(pContext, pAttrs, pDefStyle);
@@ -40,20 +46,42 @@ public class DiagramView extends View {
   public void draw(Canvas pCanvas) {
     super.draw(pCanvas);
 //    pCanvas.drawLine(200, 0, 0, 200, aRed);
-
+    if (aRed==null) {
+      aRed = new Paint(Paint.ANTI_ALIAS_FLAG|Paint.SUBPIXEL_TEXT_FLAG);
+      aRed.setARGB(255, 255, 0, 0);
+      aRed.setTypeface(Typeface.DEFAULT);
+      aRed.setTextSize(50f);
+    }
     if (aDiagram!=null) {
       final Rectangle clipBounds = new Rectangle(0d, 0d, getHeight(), getWidth());
       final AndroidCanvas canvas = new AndroidCanvas(pCanvas);
       aDiagram.draw(canvas, clipBounds);
     } else {
-      if (aRed==null) {
-        aRed = new Paint(Paint.ANTI_ALIAS_FLAG|Paint.SUBPIXEL_TEXT_FLAG);
-        aRed.setARGB(255, 255, 0, 0);
-        aRed.setTypeface(Typeface.DEFAULT);
-        aRed.setTextSize(50f);
-      }
-      pCanvas.drawText(getContext().getResources().getString(R.string.missing_diagram), getWidth()/2, getHeight()/2, aRed);
+      final String text = getContext().getResources().getString(R.string.missing_diagram);
+      aRed.getTextBounds(text, 0, text.length(), aBounds);
+      pCanvas.drawText(text, (getWidth()-aBounds.width())/2, (getHeight()-aBounds.height())/2, aRed);
       pCanvas.drawCircle(100, 100, 75, aRed);
+    }
+    InputStream stream = getClass().getClassLoader().getResourceAsStream("nl/adaptivity/process/diagram/version.properties");
+    if (stream!=null) {
+      try {
+        Properties props = new Properties();
+        try {
+          props.load(stream);
+        } catch (IOException e) {
+          e.printStackTrace();
+          return;
+        }
+        String buildtime = props.getProperty("buildtime");
+        aRed.getTextBounds(buildtime, 0, buildtime.length(), aBounds);
+        pCanvas.drawText(buildtime, getWidth() - aBounds.width()-20, getHeight()-aBounds.height()-20, aRed);
+      } finally {
+        try {
+          stream.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
     }
   }
 
