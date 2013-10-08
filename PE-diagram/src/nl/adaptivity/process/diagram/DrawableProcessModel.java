18eebdd8c527853a932b68afd4f391c142ff0d09
Paul de Vrieze <paul.devrieze@gmail.com>
2013-10-08 11:54:12 +0000

    Less Noise, better stroke width for connectors.
    git-svn-id: https://darwin.bournemouth.ac.uk/pdvrieze/svn/darwin/projects/PE-diagram/trunk@878 3bae1aa3-1c52-4c45-a669-1fb9b31e7360

--- PE-diagram/src/nl/adaptivity/process/diagram/DrawableProcessModel.java
+++ PE-diagram/src/nl/adaptivity/process/diagram/DrawableProcessModel.java
@@ -150,14 +150,14 @@ public class DrawableProcessModel extends ClientProcessModel<DrawableProcessNode
   public void draw(Canvas pCanvas, Rectangle pClipBounds) {
     Canvas canvas = pCanvas.childCanvas(getBounds(), aScale);
     Pen red = canvas.newColor(255, 0, 0, 255);
-    Pen arc = canvas.newColor(0, 0, 0, 255).setStrokeWidth(1d);
+    Pen arc = canvas.newColor(0, 0, 0, 255).setStrokeWidth(aScale);
     for(DrawableProcessNode start:getModelNodes()) {
       for (DrawableProcessNode end: start.getSuccessors()) {
         canvas.drawPath(new double[]{start.getBounds().right()-STROKEWIDTH, start.getY(), end.getBounds().left+STROKEWIDTH, end.getY()}, arc);
       }
     }
     for(DrawableProcessNode node:getModelNodes()) {
-      System.err.println("Drawing "+ node.getClass().getSimpleName()+" "+node.getId()+ "("+node.getX()+", "+node.getY()+")");
+//      System.err.println("Drawing "+ node.getClass().getSimpleName()+" "+node.getId()+ "("+node.getX()+", "+node.getY()+")");
       // TODO actually support clipbounds
 
       node.draw(canvas.childCanvas(node.getBounds(), 1 ), null);
