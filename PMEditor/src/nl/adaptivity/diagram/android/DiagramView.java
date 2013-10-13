89c91960feaae6f834965338279e953821bcbb5e
Paul de Vrieze <paul.devrieze@gmail.com>
2013-10-13 14:15:18 +0000

    Use a temporary bitmap to speed up scaling / panning.
    git-svn-id: https://darwin.bournemouth.ac.uk/pdvrieze/svn/darwin/projects/PMEditor/trunk@887 3bae1aa3-1c52-4c45-a669-1fb9b31e7360

--- PMEditor/src/nl/adaptivity/diagram/android/DiagramView.java
+++ PMEditor/src/nl/adaptivity/diagram/android/DiagramView.java
@@ -3,6 +3,7 @@ package nl.adaptivity.diagram.android;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+
 import nl.adaptivity.android.compat.Compat;
 import nl.adaptivity.diagram.Diagram;
 import nl.adaptivity.diagram.Rectangle;
@@ -11,6 +12,7 @@ import nl.adaptivity.process.editor.android.R;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
@@ -81,9 +83,10 @@ public class DiagramView extends View implements OnZoomListener{
 
     @Override
     public boolean onScaleBegin(ScaleGestureDetector pDetector) {
-      double newScale = getScale()*(double) pDetector.getScaleFactor();
+      double newScale = getScale()*pDetector.getScaleFactor();
       aPreviousDiagramX = getOffsetX()+ pDetector.getFocusX()/newScale;
       aPreviousDiagramY = getOffsetY()+ pDetector.getFocusY()/newScale;
+
       return super.onScaleBegin(pDetector);
     }
 
@@ -107,6 +110,14 @@ public class DiagramView extends View implements OnZoomListener{
     }
 
   };
+  private boolean aTouchActionOptimize = false;
+  private double aCachedScale;
+  private Paint aCachePaint;
+  private double aCachedOffsetY;
+  private double aCachedOffsetX;
+  private Bitmap aCacheBitmap;
+  private Canvas aCacheCanvas;
+  private Rectangle aCacheRect = new Rectangle(0d,0d,0d,0d);
 
   public DiagramView(Context pContext, AttributeSet pAttrs, int pDefStyle) {
     super(pContext, pAttrs, pDefStyle);
@@ -181,18 +192,49 @@ public class DiagramView extends View implements OnZoomListener{
 
   @Override
   public void onDraw(Canvas pCanvas) {
-    super.onDraw(pCanvas);
-    int canvasSave = pCanvas.save();
+    Canvas canvas;
+    if (aCacheCanvas ==null) {
+      if (aTouchActionOptimize) {
+        updateCacheRect(aCacheRect); //cacherect is in screen scale
+        aCacheBitmap = Bitmap.createBitmap((int) Math.ceil(aCacheRect.width*aScale), (int) Math.ceil(aCacheRect.height*aScale), Bitmap.Config.ARGB_8888);
+        aCacheCanvas = new Canvas(aCacheBitmap);
+        aCacheBitmap.eraseColor(0x00000000);
+
+        aCacheCanvas.translate((float)((aOffsetX - aCacheRect.left)*aScale), (float) ((aOffsetY - aCacheRect.top)*aScale));
+        canvas = aCacheCanvas;
+      } else {
+        canvas = pCanvas;
+      }
+    } else {
+      if (aTouchActionOptimize) {
+        RectF target = new RectF();
+        target.left = (float) ((aCacheRect.left-aOffsetX)*aScale);
+        target.top = (float) ((aCacheRect.top-aOffsetY)*aScale);
+        target.right = (float) (target.left+aCacheRect.width*aScale);
+        target.bottom = (float) (target.top+aCacheRect.height*aScale);
+
+//        pCanvas.drawBitmap(aCacheBitmap, target.left, target.top, null);
+        pCanvas.drawBitmap(aCacheBitmap, null, target, null);
+        // invalidate if needed, otherwise just blit bitmap.
+  //      canvas = aCacheCanvas;
+  //      aCacheBitmap.eraseColor(0x20ff0000);
+        return;
+      } else {
+        canvas = pCanvas;
+      }
+    }
+
+    int canvasSave = canvas.save();
 //    pCanvas.drawLine(200, 0, 0, 200, aRed);
     if (aDiagram!=null) {
 
       @SuppressLint("DrawAllocation")
       final Rectangle clipBounds = new Rectangle(-(aOffsetX/aScale), -(aOffsetY/aScale), getHeight(), getWidth());
       @SuppressLint("DrawAllocation")
-      final AndroidCanvas canvas = new AndroidCanvas(pCanvas);
-      aDiagram.draw(canvas.childCanvas(clipBounds, aScale), clipBounds);
+      final AndroidCanvas androidcanvas = new AndroidCanvas(canvas);
+      aDiagram.draw(androidcanvas.childCanvas(clipBounds, aScale), clipBounds);
     } else {
-      pCanvas.scale((float) aScale, (float) aScale);
+      canvas.scale((float) aScale, (float) aScale);
       if (aRed==null) {
         aRed = new Paint(Paint.ANTI_ALIAS_FLAG|Paint.SUBPIXEL_TEXT_FLAG);
         aRed.setARGB(255, 255, 0, 0);
@@ -201,20 +243,20 @@ public class DiagramView extends View implements OnZoomListener{
       }
       final String text = getContext().getResources().getString(R.string.missing_diagram);
       aRed.getTextBounds(text, 0, text.length(), aBounds);
-      pCanvas.drawText(text, (getWidth()-aBounds.width())/2, (getHeight()-aBounds.height())/2, aRed);
-      pCanvas.drawCircle(100, 100, 75, aRed);
+      canvas.drawText(text, (getWidth()-aBounds.width())/2, (getHeight()-aBounds.height())/2, aRed);
+      canvas.drawCircle(100, 100, 75, aRed);
     }
-    pCanvas.restoreToCount(canvasSave);
-    canvasSave = pCanvas.save();
+    canvas.restoreToCount(canvasSave);
+    canvasSave = canvas.save();
     if (aOverlay!=null) {
-      pCanvas.scale((float) aScale, (float) aScale);
+      canvas.scale((float) aScale, (float) aScale);
       if (aOverlay instanceof DiagramDrawable) {
-        ((DiagramDrawable) aOverlay).draw(pCanvas, aScale);
+        ((DiagramDrawable) aOverlay).draw(canvas, aScale);
       }
-      aOverlay.draw(pCanvas);
+      aOverlay.draw(canvas);
     }
 
-    pCanvas.restoreToCount(canvasSave);
+    canvas.restoreToCount(canvasSave);
     if (BuildConfig.DEBUG) {
       InputStream stream = getClass().getClassLoader().getResourceAsStream("nl/adaptivity/process/diagram/version.properties");
       if (stream!=null) {
@@ -236,7 +278,7 @@ public class DiagramView extends View implements OnZoomListener{
             }
 
             aTimePen.getTextBounds(buildtime, 0, buildtime.length(), aBounds);
-            pCanvas.drawText(buildtime, getWidth() - aBounds.width()-20, getHeight()-20, aTimePen);
+            canvas.drawText(buildtime, getWidth() - aBounds.width()-20, getHeight()-20, aTimePen);
           }
         } finally {
           try {
@@ -247,6 +289,25 @@ public class DiagramView extends View implements OnZoomListener{
         }
       }
     }
+    if (canvas == aCacheCanvas) {
+      RectF target = new RectF();
+      target.left = (float) ((aCacheRect.left-aOffsetX)*aScale);
+      target.top = (float) ((aCacheRect.top-aOffsetY)*aScale);
+      target.right = target.left+((float) (aCacheRect.width*aScale));
+      target.bottom = target.top+((float) (aCacheRect.height*aScale));
+
+//      pCanvas.drawBitmap(aCacheBitmap, target.left, target.top, null);
+      pCanvas.drawBitmap(aCacheBitmap, null, target, null);
+    }
+  }
+
+  private void updateCacheRect(Rectangle pCacheRect) {
+    Rectangle diagrambounds = aDiagram.getBounds();
+    double diagLeft = Math.max(diagrambounds.left-1, aOffsetX-30);
+    double diagWidth = Math.min(diagrambounds.right()-diagLeft+6, (getWidth()/aScale) + 60);
+    double diagTop = Math.max(diagrambounds.top-1, aOffsetY-30);
+    double diagHeight = Math.min(diagrambounds.bottom()-diagTop+6, (getHeight()/aScale) + 60);
+    pCacheRect.set(diagLeft, diagTop, diagWidth, diagHeight);
   }
 
   public void setOverlay(Drawable pOverlay) {
@@ -261,6 +322,19 @@ public class DiagramView extends View implements OnZoomListener{
 
   @Override
   public boolean onTouchEvent(MotionEvent pEvent) {
+    int action = pEvent.getActionMasked();
+    if (action==MotionEvent.ACTION_DOWN) {
+      aTouchActionOptimize  = true;
+//      buildDrawingCache(true);
+//      setDrawingCacheEnabled(true);
+    } else if (action==MotionEvent.ACTION_UP|| action==MotionEvent.ACTION_CANCEL) {
+      aTouchActionOptimize  = false;
+//      setDrawingCacheEnabled(false);
+//      destroyDrawingCache();
+//      Compat.postInvalidateOnAnimation(this);
+      aCacheBitmap = null; aCacheCanvas = null;
+      Compat.postInvalidateOnAnimation(this);
+    }
     boolean retVal = aScaleGestureDetector.onTouchEvent(pEvent);
     retVal = aGestureDetector.onTouchEvent(pEvent) || retVal;
     return retVal || super.onTouchEvent(pEvent);
