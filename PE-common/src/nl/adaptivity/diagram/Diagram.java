5c1adb89fe09c6be07977a607707704de292e404
Paul de Vrieze <paul@devrieze.net>
2013-10-22 22:28:02 +0100

    Remove unneeded elements.

--- PE-common/src/nl/adaptivity/diagram/Diagram.java
+++ PE-common/src/nl/adaptivity/diagram/Diagram.java
@@ -1,13 +1,6 @@
 package nl.adaptivity.diagram;
 
-import java.util.Collection;
 
 
 public interface Diagram extends GroupDrawable {
-  /**
-   * Set the given items to be highlighted. This will unHighlight all other items.
-   */
-  void setHighlighted(Collection<Drawable> pItems);
-
-  Collection<? extends Drawable> getHighlighted();
 }
