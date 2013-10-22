2d4e21112c87cbd7e5082d00f2b8f365d74b85c0
Paul de Vrieze <paul@devrieze.net>
2013-10-22 21:57:45 +0100

    Some changes to make theming work.

--- PE-common/src/nl/adaptivity/diagram/ThemeItem.java
+++ PE-common/src/nl/adaptivity/diagram/ThemeItem.java
@@ -2,5 +2,20 @@ package nl.adaptivity.diagram;
 
 
 public interface ThemeItem {
+  /**
+   * Get the number for this theme item. Multiple items should never return the same
+   * number. Conciseness will help though.
+   * @return The item ordinal.
+   */
+  public int getItemNo();
 
+  /**
+   * Get the state that needs to be used for drawing the item at the given state. This allows
+   * for optimization in caching.
+   * @param The state needed.
+   * @return The effective state.
+   */
+  public int getEffectiveState(int pState);
+
+  public <PEN_T extends Pen<PEN_T>> PEN_T createPen(DrawingStrategy<?,PEN_T,?> pStrategy, int pState);
 }
