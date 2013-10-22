30806a699540846c6123270b13ab9cf2c6a657ec
Paul de Vrieze <paul@devrieze.net>
2013-10-22 20:55:04 +0100

    Add more state contstants.

--- PE-common/src/nl/adaptivity/diagram/Drawable.java
+++ PE-common/src/nl/adaptivity/diagram/Drawable.java
@@ -1,15 +1,18 @@
 package nl.adaptivity.diagram;
 
 public interface Drawable extends Bounded {
-  
-  static final int STATE_DEFAULT=0;
-  static final int STATE_TOUCHED=1;
-  static final int STATE_SELECTED=2;
-  static final int STATE_FOCUSSED=4;
-  static final int STATE_CUSTOM1=8;
-  static final int STATE_CUSTOM2=16;
-  static final int STATE_CUSTOM3=32;
-  
+
+  static final int STATE_DEFAULT=0x0;
+  static final int STATE_TOUCHED=0x1;
+  static final int STATE_SELECTED=0x2;
+  static final int STATE_FOCUSSED=0x4;
+  static final int STATE_DISABLED=0x8;
+  static final int STATE_CUSTOM1=0x10;
+  static final int STATE_CUSTOM2=0x20;
+  static final int STATE_CUSTOM3=0x40;
+  static final int STATE_CUSTOM4=0x80;
+  static final int STATE_MASK=0xffff;
+
 
   /**
    * Draw the drawable to the given canvas. The drawing will use a top left of (0,0).
@@ -21,14 +24,14 @@ public interface Drawable extends Bounded {
 
   @Override
   public Drawable getItemAt(double pX, double pY);
-  
+
   /**
    * Get the current state of the drawable. Individual implementations should specify what each state value means.
    * The <code>0</code> value however means the default.
-   * @return The current state of the drawable. 
+   * @return The current state of the drawable.
    */
   public int getState();
-  
+
   /**
    * Set the current state of the drawable.
    * @param pState
