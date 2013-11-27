5ee4d69bedc6fea221c6decfe68ec80a91d85653
Paul de Vrieze <paul.devrieze@gmail.com>
2013-11-27 09:49:10 +0000

    Move the NOPcache to the StringCache interface. It's lightweight so can be in the API and allows a default instantiation.
    git-svn-id: https://darwin.bournemouth.ac.uk/pdvrieze/svn/darwin/projects/JavaCommonApi/trunk@952 3bae1aa3-1c52-4c45-a669-1fb9b31e7360

--- JavaCommonApi/src/net/devrieze/util/StringCache.java
+++ JavaCommonApi/src/net/devrieze/util/StringCache.java
@@ -1,10 +1,24 @@
 package net.devrieze.util;
 
+import net.devrieze.annotations.NotNull;
 import net.devrieze.annotations.Nullable;
 
 
 
 public interface StringCache {
+
+  public static enum UniqueCaches implements StringCache {
+    NOP;
+
+    @Override
+    public String lookup(String pString) {
+      return pString;
+    }
+
+  }
+
+  @NotNull
+  public static final StringCache NOPCACHE=UniqueCaches.NOP;
 
   /**
    * Look up a string in the cache for string reuse.
