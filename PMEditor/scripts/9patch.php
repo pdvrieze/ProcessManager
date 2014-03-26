5bb55470094762517aef229d3d50ee3072cba0b9
Paul de Vrieze <paul.devrieze@gmail.com>
2014-03-26 22:42:07 +0000

    Add icons, and the makefile to generate them.
    git-svn-id: https://darwin.bournemouth.ac.uk/pdvrieze/svn/darwin/projects/PMEditor/trunk@1172 3bae1aa3-1c52-4c45-a669-1fb9b31e7360

---
+++ PMEditor/scripts/9patch.php
@@ -0,0 +1,72 @@
+#!/usr/bin/php
+<?php
+  
+  if ($argc != 14) {
+    print("usage: $argv[0] <hsizeindp> <vsizeindp> <sourcesvg> <sourceid> <destfile> <offx1> <lenx1> <offy1> <leny1> <offx2> <lenx2> <offy2> <leny2>\n");
+    exit(1);
+  }
+
+  function filedpi($destfile) {
+    if (strpos($destfile,'drawable-ldpi')!==FALSE) {
+      return 120;
+    } elseif (strpos($destfile,'drawable-tvdpi')!==FALSE) {
+      return 213;
+    } elseif (strpos($destfile,'drawable-hdpi')!==FALSE) {
+      return 240;
+    } elseif (strpos($destfile,'drawable-xhdpi')!==FALSE) {
+      return 320;
+    } else {
+      return 160;
+    }
+  }
+
+  function dp2px($size, $dpi) {
+    return round(($dpi/160)*$size);
+  }
+
+
+  $hsizedp=$argv[1];
+  $vsizedp=$argv[2];
+  $source=$argv[3];
+  $imgid=$argv[4];
+  $dest=$argv[5];
+  $dpi=filedpi($dest);
+  print("Using dpi: ".$dpi."\n");
+
+  $offx1=dp2px($argv[6],$dpi); $lenx1=dp2px($argv[7],$dpi);
+  $offy1=dp2px($argv[8],$dpi); $leny1=dp2px($argv[9],$dpi);
+
+  $offx2=dp2px($argv[10],$dpi); $lenx2=dp2px($argv[11],$dpi);
+  $offy2=dp2px($argv[12],$dpi); $leny2=dp2px($argv[13],$dpi);
+
+  $hsize=dp2px($hsizedp,$dpi);
+  $vsize=dp2px($vsizedp,$dpi);
+
+  $tmpframe=tempnam(NULL, "9patch-");
+  $inkscapecmd="inkscape -w $hsize -h $vsize -j -i $imgid -e $tmpframe \"$source\"";
+  exec($inkscapecmd);
+
+  $srcimg=imagecreatefrompng($tmpframe);
+
+  $dstimg=imagecreatetruecolor($hsize+2, $vsize+2);
+  imagealphablending($dstimg, FALSE);
+  imagesavealpha($dstimg, TRUE);
+
+  $clr_transparent=imagecolorallocatealpha($dstimg,255,255,255,127);
+  imagefilledrectangle($dstimg,0,0, $hsize+2, $vsize+2,$clr_transparent);
+
+  $clr_black=imagecolorallocate($dstimg, 0,0,0);
+
+  imagecopy($dstimg, $srcimg, 1, 1, 0, 0, $hsize, $vsize);
+
+  imageline($dstimg, $offx1, 0, $offx1+$lenx1,0, $clr_black);
+  imageline($dstimg, 0, $offy1, 0, $offy1+$leny1, $clr_black);
+
+  imageline($dstimg, $offx2, $vsize+1, $offx2+$lenx2,$vsize+1, $clr_black);
+  imageline($dstimg, $hsize+1, $offy2, $hsize+1, $offy2+$leny2, $clr_black);
+
+  imagepng($dstimg, $dest);
+  unlink($tmpframe);
+
+
+?>
---
+++ PMEditor/scripts/iconize.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+SRC=$3
+DST=$4
+if [ $# -gt 4 ]; then
+  ID="-j -i $5"
+else
+  ID=""
+fi
+
+CORRECT=0
+QUARTW=$(( $1 / 4 ))
+QUARTH=$(( $2 / 4 ))
+if [ "${DST/drawable*-nodpi}" != "${DST}" ]; then
+	((WIDTH = $(basename "$DST"|sed -e "s,.*_\([0-9][0-9]*\)\.[^\.]*$,\1,") ))
+	((HEIGHT = ( $2 * $WIDTH ) / $1 ))
+elif [ "${DST/drawable*-ldpi}" != "${DST}" ]; then
+	CORRECT=-1
+	((WIDTH = ( $CORRECT * $QUARTW ) + $1 ))
+	(( HEIGHT= ( $CORRECT * $QUARTH ) + $2 ))
+elif [ "${DST/drawable*-hdpi}" != "${DST}" ]; then
+	CORRECT=2
+	((WIDTH = ( $CORRECT * $QUARTW ) + $1 ))
+	(( HEIGHT= ( $CORRECT * $QUARTH ) + $2 ))
+elif [ "${DST/drawable*-tvdpi}" != "${DST}" ]; then
+	CORRECT=213
+	((WIDTH = ( $CORRECT * $1 +80 ) /160 ))
+	(( HEIGHT= ( $CORRECT * $2 +80 ) /160 ))
+elif [ "${DST/drawable*-xhdpi}" != "${DST}" ]; then
+	CORRECT=4
+	((WIDTH = ( $CORRECT * $QUARTW ) + $1 ))
+	(( HEIGHT= ( $CORRECT * $QUARTH ) + $2 ))
+else
+	WIDTH=$1
+	HEIGHT=$2
+fi
+
+echo "Creating icon ${DST}"
+if [ "${DST/.xml}" != "${DST}" ]; then
+  mkdir -p "$(dirname "${DST}")"
+  cat <<EOF > "${DST}"
+<?xml version="1.0" encoding="utf-8"?>
+<bitmap xmlns:android="http://schemas.android.com/apk/res/android" android:src="@drawable/$(basename ${DST/.xml/_${WIDTH}}|sed -e "s,^btn_,peg_,")">
+</bitmap>
+EOF
+else
+  inkscape -w $WIDTH -h $HEIGHT -e $DST $ID $SRC
+  exec optipng "${DST}"
+fi
+
