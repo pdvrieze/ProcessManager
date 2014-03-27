430ef155466075bcba8b07506db7821d781d5259
Paul de Vrieze <paul.devrieze@gmail.com>
2014-03-27 14:40:54 +0000

    Document a bit better.
    git-svn-id: https://darwin.bournemouth.ac.uk/pdvrieze/svn/darwin/projects/PE-diagram/trunk@1174 3bae1aa3-1c52-4c45-a669-1fb9b31e7360

--- PE-diagram/src/nl/adaptivity/process/clientProcessModel/IClientProcessNode.java
+++ PE-diagram/src/nl/adaptivity/process/clientProcessModel/IClientProcessNode.java
@@ -6,8 +6,20 @@ import nl.adaptivity.process.processModel.ProcessNodeSet;
 
 public interface IClientProcessNode<T extends IClientProcessNode<T>> extends ProcessNode<T> {
 
+  /**
+   * Set the X coordinate of the reference point of the element. This is
+   * normally the center.
+   * 
+   * @param pX The x coordinate
+   */
   void setX(double pX);
 
+  /**
+   * Set the Y coordinate of the reference point of the element. This is
+   * normally the center of the symbol (excluding text).
+   * 
+   * @param pY
+   */
   void setY(double pY);
 
   @Override
