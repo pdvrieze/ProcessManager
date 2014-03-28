4a677366fd487e40de058944e841095733e1c7d8
Paul de Vrieze <paul.devrieze@gmail.com>
2014-03-28 20:52:01 +0000

    Fix the 9patch script to use use offsets relative to the image start. Also use proper length.
    git-svn-id: https://darwin.bournemouth.ac.uk/pdvrieze/svn/darwin/projects/PMEditor/trunk@1180 3bae1aa3-1c52-4c45-a669-1fb9b31e7360

--- PMEditor/scripts/9patch.php
+++ PMEditor/scripts/9patch.php
@@ -33,11 +33,11 @@
   $dpi=filedpi($dest);
   print("Using dpi: ".$dpi."\n");
 
-  $offx1=dp2px($argv[6],$dpi); $lenx1=dp2px($argv[7],$dpi);
-  $offy1=dp2px($argv[8],$dpi); $leny1=dp2px($argv[9],$dpi);
+  $offx1=dp2px($argv[6],$dpi)+1; $lenx1=dp2px($argv[7],$dpi);
+  $offy1=dp2px($argv[8],$dpi)+1; $leny1=dp2px($argv[9],$dpi);
 
-  $offx2=dp2px($argv[10],$dpi); $lenx2=dp2px($argv[11],$dpi);
-  $offy2=dp2px($argv[12],$dpi); $leny2=dp2px($argv[13],$dpi);
+  $offx2=dp2px($argv[10],$dpi)+1; $lenx2=dp2px($argv[11],$dpi);
+  $offy2=dp2px($argv[12],$dpi)+1; $leny2=dp2px($argv[13],$dpi);
 
   $hsize=dp2px($hsizedp,$dpi);
   $vsize=dp2px($vsizedp,$dpi);
@@ -59,11 +59,11 @@
 
   imagecopy($dstimg, $srcimg, 1, 1, 0, 0, $hsize, $vsize);
 
-  imageline($dstimg, $offx1, 0, $offx1+$lenx1,0, $clr_black);
-  imageline($dstimg, 0, $offy1, 0, $offy1+$leny1, $clr_black);
+  imageline($dstimg, $offx1, 0, $offx1+$lenx1-1,0, $clr_black);
+  imageline($dstimg, 0, $offy1, 0, $offy1+$leny1-1, $clr_black);
 
-  imageline($dstimg, $offx2, $vsize+1, $offx2+$lenx2,$vsize+1, $clr_black);
-  imageline($dstimg, $hsize+1, $offy2, $hsize+1, $offy2+$leny2, $clr_black);
+  imageline($dstimg, $offx2, $vsize+1, $offx2+$lenx2-1,$vsize+1, $clr_black);
+  imageline($dstimg, $hsize+1, $offy2, $hsize+1, $offy2+$leny2-1, $clr_black);
 
   imagepng($dstimg, $dest);
   unlink($tmpframe);
