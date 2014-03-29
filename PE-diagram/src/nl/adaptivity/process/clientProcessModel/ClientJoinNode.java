901766312fbceeb12f291040104521092b2ae880
Paul de Vrieze <paul.devrieze@gmail.com>
2014-03-29 21:59:45 +0000

    Extract out a common baseclass for splits and joins
    git-svn-id: https://darwin.bournemouth.ac.uk/pdvrieze/svn/darwin/projects/PE-diagram/trunk@1213 3bae1aa3-1c52-4c45-a669-1fb9b31e7360

--- PE-diagram/src/nl/adaptivity/process/clientProcessModel/ClientJoinNode.java
+++ PE-diagram/src/nl/adaptivity/process/clientProcessModel/ClientJoinNode.java
@@ -3,11 +3,7 @@ package nl.adaptivity.process.clientProcessModel;
 import nl.adaptivity.process.processModel.Join;
 
 
-public class ClientJoinNode<T extends IClientProcessNode<T>> extends ClientProcessNode<T> implements Join<T> {
-
-  private int aMin;
-
-  private int aMax;
+public class ClientJoinNode<T extends IClientProcessNode<T>> extends ClientJoinSplit<T> implements Join<T> {
 
   public ClientJoinNode(ClientProcessModel<T> pOwner) {
     super(pOwner);
@@ -24,26 +20,6 @@ public class ClientJoinNode<T extends IClientProcessNode<T>> extends ClientProce
   @Override
   public int getMaxPredecessorCount() {
     return Integer.MAX_VALUE;
-  }
-
-  @Override
-  public void setMax(int pMax) {
-    aMax = pMax;
-  }
-
-  @Override
-  public int getMax() {
-    return aMax;
-  }
-
-  @Override
-  public void setMin(int pMin) {
-    aMin = pMin;
-  }
-
-  @Override
-  public int getMin() {
-    return aMin;
   }
 
 }
---
+++ PE-diagram/src/nl/adaptivity/process/clientProcessModel/ClientJoinSplit.java
@@ -0,0 +1,46 @@
+package nl.adaptivity.process.clientProcessModel;
+
+
+import nl.adaptivity.process.processModel.JoinSplit;
+
+
+public abstract class ClientJoinSplit<T extends IClientProcessNode<T>> extends ClientProcessNode<T> implements JoinSplit<T>{
+
+  private int aMin;
+  private int aMax;
+
+  public ClientJoinSplit(ClientProcessModel<T> pOwner) {
+    super(pOwner);
+  }
+
+  public ClientJoinSplit(String pId, ClientProcessModel<T> pOwner) {
+    super(pId, pOwner);
+  }
+
+  public ClientJoinSplit(ClientJoinSplit<T> pOrig) {
+    super(pOrig);
+    aMin = pOrig.aMin;
+    aMax = pOrig.aMax;
+  }
+
+  @Override
+  public void setMax(int pMax) {
+    aMax = pMax;
+  }
+
+  @Override
+  public int getMax() {
+    return aMax;
+  }
+
+  @Override
+  public void setMin(int pMin) {
+    aMin = pMin;
+  }
+
+  @Override
+  public int getMin() {
+    return aMin;
+  }
+
+}
