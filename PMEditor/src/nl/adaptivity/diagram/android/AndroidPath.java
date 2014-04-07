5d1bc66dadcbc89dbf7ba79c0e0e01f1f0c2ad34
Paul de Vrieze <paul.devrieze@gmail.com>
2014-04-07 09:11:04 +0000

    Update to support the new Path interface.
    git-svn-id: https://darwin.bournemouth.ac.uk/pdvrieze/svn/darwin/projects/PMEditor/trunk@1275 3bae1aa3-1c52-4c45-a669-1fb9b31e7360

--- PMEditor/src/nl/adaptivity/diagram/android/AndroidPath.java
+++ PMEditor/src/nl/adaptivity/diagram/android/AndroidPath.java
@@ -21,8 +21,15 @@ public final class AndroidPath implements DiagramPath<AndroidPath> {
   }
 
   @Override
-  public void close() {
+  public AndroidPath cubicTo(double x1, double y1, double x2, double y2, double x3, double y3) {
+    aPath.cubicTo((float)x1, (float)y1, (float)x2, (float)y2, (float)x3, (float)y3);
+    return this;
+  }
+
+  @Override
+  public AndroidPath close() {
     aPath.close();
+    return this;
   }
 
   public Path getPath() {
