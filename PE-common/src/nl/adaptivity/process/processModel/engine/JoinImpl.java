a2682e034cfffd53c69f12e04161f860f5dc545c
Paul de Vrieze <paul@devrieze.net>
2014-06-01 15:08:48 +0100

    A ProcessNodeSet is a list, so just return that. A direct link to the underlying list is needed as it may be updated by jaxb.

--- PE-common/src/nl/adaptivity/process/processModel/engine/JoinImpl.java
+++ PE-common/src/nl/adaptivity/process/processModel/engine/JoinImpl.java
@@ -1,10 +1,8 @@
 package nl.adaptivity.process.processModel.engine;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
-import java.util.Set;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
@@ -12,15 +10,12 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlIDREF;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
-import javax.xml.bind.annotation.adapters.XmlAdapter;
-import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 import nl.adaptivity.process.IMessageService;
 import nl.adaptivity.process.exec.IProcessNodeInstance;
 import nl.adaptivity.process.processModel.IllegalProcessModelException;
 import nl.adaptivity.process.processModel.Join;
 import nl.adaptivity.process.processModel.ProcessNodeSet;
-import nl.adaptivity.util.ListFilter;
 
 
 @XmlRootElement(name = JoinImpl.ELEMENTNAME)
@@ -32,8 +27,6 @@ JoinSplitImpl implements Join<ProcessNodeImpl> {
   private static final long serialVersionUID = -8598245023280025173L;
 
   public static final String ELEMENTNAME = "join";
-
-  private Set<ProcessNodeImpl> aPred;
 
   public JoinImpl(final Collection<? extends ProcessNodeImpl> pPredecessors, final int pMin, final int pMax) {
     super(pPredecessors, pMin, pMax);
@@ -63,26 +56,9 @@ JoinSplitImpl implements Join<ProcessNodeImpl> {
     //    throw new UnsupportedOperationException("Not yet correct");
   }
 
-  // TODO see whether this is still needed
-  @Deprecated
-  public Set<ProcessNodeImpl> getPred() {
-    if (aPred == null) {
-      aPred = ProcessNodeSet.processNodeSet();
-    }
-    return aPred;
-  }
-
-  @Override
-  public Set<? extends ProcessNodeImpl> getPredecessors() {
-    if (aPred != null) {
-      setPredecessors(aPred);
-      aPred = null;
-    }
-    return super.getPredecessors();
-  }
-
   List<? extends ProcessNodeImpl> getXmlPrececessors() {
-    return new ArrayList<>(getPredecessors());
+    if (getPredecessors()==null) { return null; }
+    return (ProcessNodeSet<? extends ProcessNodeImpl>) getPredecessors();
   }
 
   @XmlElement(name = "predecessor")
