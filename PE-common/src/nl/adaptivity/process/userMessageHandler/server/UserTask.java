5b93e4a861acfdcfcb4894573a48e864ef480a55
Paul de Vrieze <paul@devrieze.net>
2014-07-15 21:19:26 +0100

    Add items to a task

--- PE-common/src/nl/adaptivity/process/userMessageHandler/server/UserTask.java
+++ PE-common/src/nl/adaptivity/process/userMessageHandler/server/UserTask.java
@@ -1,14 +1,27 @@
 package nl.adaptivity.process.userMessageHandler.server;
 
 import java.security.Principal;
+import java.util.List;
 
 import net.devrieze.util.HandleMap.HandleAware;
-
 import nl.adaptivity.messaging.EndpointDescriptorImpl;
 import nl.adaptivity.process.exec.IProcessNodeInstance.TaskState;
 
 
 public interface UserTask<T extends UserTask<T>> extends HandleAware<T> {
+
+
+  public interface TaskItem {
+
+    public abstract List<String> getOptions();
+
+    public abstract String getValue();
+
+    public abstract String getType();
+
+    public abstract String getName();
+
+  }
 
   public TaskState getState();
 
@@ -17,5 +30,9 @@ public interface UserTask<T extends UserTask<T>> extends HandleAware<T> {
   public void setEndpoint(EndpointDescriptorImpl pEndPoint);
 
   public Principal getOwner();
+
+  public List<? extends TaskItem> getItems();
+
+  public void setItems(List<? extends TaskItem> pItems);
 
 }
