6673d8acbe746784eeaacae19a0f4e2e774e3c58
Paul de Vrieze <paul.devrieze@gmail.com>
2014-07-19 19:38:57 +0000

    Add a checker parameter and callback interface that allows to check whether the name is not yet in use
    git-svn-id: https://darwin.bournemouth.ac.uk/pdvrieze/svn/darwin/projects/PMEditor/trunk@1419 3bae1aa3-1c52-4c45-a669-1fb9b31e7360

--- PMEditor/src/nl/adaptivity/util/Util.java
+++ PMEditor/src/nl/adaptivity/util/Util.java
@@ -2,19 +2,41 @@ package nl.adaptivity.util;
 
 
 public final class Util {
+
+  public interface NameChecker {
+
+    boolean isAvailable(String pString);
+
+  }
+
   private Util() { /* xx */ }
 
   public static String suggestNewName(CharSequence previousName) {
+    return suggestNewName(previousName, null);
+  }
+
+  public static String suggestNewName(CharSequence previousName, NameChecker nameChecker) {
     int i=previousName.length()-1;
     while (Character.isDigit(previousName.charAt(i))) {
       --i;
     }
     String suggestedNewName;
+    int suffix;
+    CharSequence base;
     if ((i+1)<previousName.length()) {
       int prevNo = Integer.parseInt(previousName.subSequence(i+1, previousName.length()).toString());
-      suggestedNewName = previousName.subSequence(0, i+1)+Integer.toString(prevNo+1);
+      base = previousName.subSequence(0, i+1);
+      suffix = prevNo+1;
     } else {
-      suggestedNewName = previousName + " 2";
+      base = previousName+" ";
+      suffix = 2;
+    }
+    if (nameChecker!=null) {
+      while (! nameChecker.isAvailable(suggestedNewName=base+Integer.toString(suffix))) {
+        suffix++;
+      }
+    } else {
+      suggestedNewName = base+Integer.toString(suffix);
     }
     return suggestedNewName;
   }
