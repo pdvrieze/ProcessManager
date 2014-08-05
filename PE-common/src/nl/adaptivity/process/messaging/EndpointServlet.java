966e056358f0335f01a8c5560da0f5ed623867b9
Paul de Vrieze <paul@devrieze.net>
2014-08-05 20:47:34 +0100

    Log to the console before attempting to send an error. This prevents log messages being missed when the error can not be properly sent.

--- PE-common/src/nl/adaptivity/process/messaging/EndpointServlet.java
+++ PE-common/src/nl/adaptivity/process/messaging/EndpointServlet.java
@@ -143,9 +143,10 @@ public class EndpointServlet extends HttpServlet {
       }
     } catch (final Exception e) {
       try {
-        pResponse.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
         getLogger().log(Level.WARNING, "Error when processing REST/SOAP", e);
+        pResponse.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
       } catch (final IOException e1) {
+        e1.addSuppressed(e);
         getLogger().log(Level.WARNING, "Failure to notify client of error", e);
       }
     }
