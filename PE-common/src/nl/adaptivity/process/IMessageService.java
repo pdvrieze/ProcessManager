c96266f667e5ff30ec4225a0add1623a3af05fb4
Paul de Vrieze <paul@devrieze.net>
2014-08-06 22:51:51 +0100

    Add a method to retrieve the endpoint directly (without going through the other methods)

--- PE-common/src/nl/adaptivity/process/IMessageService.java
+++ PE-common/src/nl/adaptivity/process/IMessageService.java
@@ -1,12 +1,13 @@
 package nl.adaptivity.process;
 
+import nl.adaptivity.messaging.EndpointDescriptor;
 import nl.adaptivity.process.exec.IProcessNodeInstance;
 import nl.adaptivity.process.processModel.IXmlMessage;
 
 
 /**
  * Interface signifying that the object can be used to send messages.
- * 
+ *
  * @author Paul de Vrieze
  * @param <T> The type signifying a message that can then be sent.
  * @param <U> The task that the message corresponds to. This allows for messages
@@ -16,7 +17,7 @@ public interface IMessageService<T, U extends IProcessNodeInstance<U>> {
 
   /**
    * Create a message.
-   * 
+   *
    * @param pMessage The message to create (for later sending)
    * @return The sendable message that can be sent.
    */
@@ -24,11 +25,17 @@ public interface IMessageService<T, U extends IProcessNodeInstance<U>> {
 
   /**
    * Send a message.
-   * 
+   *
    * @param pMessage The message to send. (Created by
    *          {@link #createMessage(IXmlMessage)}).
    * @param pInstance The task instance to link the sending to.
    * @return <code>true</code> on success, <code>false</code> on failure.
    */
   boolean sendMessage(T pMessage, U pInstance);
+
+  /**
+   * Get the endpoint belonging to the messenger. (Where can replies go)
+   * @return The descriptor of the local endpoint.
+   */
+  EndpointDescriptor getLocalEndpoint();
 }
