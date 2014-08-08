9039f239ecf5a1aba56de55c447daca248a61c80
Paul de Vrieze <paul@devrieze.net>
2014-08-08 16:48:32 +0100

    JAXBSources are defective

--- ProcessEngine/src/nl/adaptivity/process/engine/ProcessModelMap.java
+++ ProcessEngine/src/nl/adaptivity/process/engine/ProcessModelMap.java
@@ -23,6 +23,7 @@ import net.devrieze.util.db.DBTransaction;
 import net.devrieze.util.security.SimplePrincipal;
 import nl.adaptivity.process.processModel.XmlProcessModel;
 import nl.adaptivity.process.processModel.engine.ProcessModelImpl;
+import nl.adaptivity.util.activation.Sources;
 
 
 public class ProcessModelMap extends CachingDBHandleMap<ProcessModelImpl> {
@@ -150,7 +151,7 @@ public class ProcessModelMap extends CachingDBHandleMap<ProcessModelImpl> {
       } catch (JAXBException e) {
         throw new RuntimeException(e);
       }
-      pStatement.setCharacterStream(pOffset+1, JAXBSource.sourceToInputSource(jbs).getCharacterStream());
+      pStatement.setCharacterStream(pOffset+1, Sources.toReader(jbs));
       return 2;
     }
 
