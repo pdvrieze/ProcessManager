0d206092330872bb5c188d9a61177876386acb42
Paul de Vrieze <paul@devrieze.net>
2014-08-14 12:59:00 +0100

    Make the eventReader create namespace events even when there are no namespace attributes in the dom nodes.

--- PE-common/src/nl/adaptivity/util/xml/NodeEventReader.java
+++ PE-common/src/nl/adaptivity/util/xml/NodeEventReader.java
@@ -1,10 +1,15 @@
 package nl.adaptivity.util.xml;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.Iterator;
 import java.util.List;
 
+import javax.xml.XMLConstants;
 import javax.xml.stream.XMLEventFactory;
 import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.Namespace;
 import javax.xml.stream.events.XMLEvent;
 
 import org.w3c.dom.*;
@@ -17,6 +22,7 @@ public class NodeEventReader extends AbstractBufferedEventReader {
   private Node mCurrent;
   private XMLEventFactory mXef = XMLEventFactory.newInstance();
   private Node mGuardParent;
+  private Deque<List<Namespace>> mNamespaceContext = new ArrayDeque<>();
 
   public NodeEventReader(NodeList pNodeList) {
     mNodes = pNodeList;
@@ -49,6 +55,9 @@ public class NodeEventReader extends AbstractBufferedEventReader {
         if (mCurrent.getNodeType()==Node.ELEMENT_NODE || mCurrent.getNodeType()==Node.DOCUMENT_NODE || mCurrent.getNodeType()==Node.DOCUMENT_FRAGMENT_NODE) {
           // node without content
           add(createEndEvent(mCurrent));
+          if (mCurrent.getNodeType()==Node.ELEMENT_NODE) {
+            mNamespaceContext.pop();
+          }
         }
         Node parent = mCurrent.getParentNode();
         while (parent!=mGuardParent && parent!=null && node==null) {
@@ -66,6 +75,7 @@ public class NodeEventReader extends AbstractBufferedEventReader {
         mCurrent = null;
       }
       ++mNodesPos;
+      mNamespaceContext.clear();
     }
     return null; // end of stream
   }
@@ -73,7 +83,7 @@ public class NodeEventReader extends AbstractBufferedEventReader {
   private XMLEvent createEndEvent(Node pNode) {
     switch (pNode.getNodeType()) {
       case Node.ELEMENT_NODE:
-        return mXef.createEndElement(pNode.getPrefix(), pNode.getNamespaceURI(), pNode.getLocalName());
+        return mXef.createEndElement(getPrefix(pNode), pNode.getNamespaceURI(), pNode.getLocalName());
       case Node.DOCUMENT_NODE:
         return mXef.createEndDocument();
     }
@@ -114,31 +124,67 @@ public class NodeEventReader extends AbstractBufferedEventReader {
   }
 
   private void createElement(Element pNode) {
-    add(mXef.createStartElement(pNode.getPrefix(), pNode.getNamespaceURI(), pNode.getLocalName()));
+    final ArrayList<Namespace> namespaces = new ArrayList<>();
+    mNamespaceContext.push(namespaces);
+    final String prefix = getPrefix(pNode);
+    add(mXef.createStartElement(prefix, pNode.getNamespaceURI(), pNode.getLocalName()));
     NamedNodeMap attrs1 = pNode.getAttributes();
     List<Attr> attrs2 = new ArrayList<>();
     for(int i=0; i<attrs1.getLength(); ++i) {
       Attr attr = (Attr) attrs1.item(i);
+      final Namespace ns;
       if ("xmlns".equals(attr.getPrefix())) {
-        add(mXef.createNamespace(attr.getLocalName(), attr.getValue()));
+        ns = mXef.createNamespace(attr.getLocalName(), attr.getValue());
       } else if ("".equals(attr.getPrefix())&& "xmlns".equals(attr.getLocalName())) {
-        add(mXef.createNamespace(attr.getValue()));
+        ns = mXef.createNamespace(attr.getValue());
       } else {
         attrs2.add(attr);
+        continue;
       }
+      namespaces.add(ns);
+      add(ns);
+    }
+    if (! isKnownNs(prefix, pNode.getNamespaceURI())) {
+      Namespace ns = mXef.createNamespace(prefix, pNode.getNamespaceURI());
+      namespaces.add(ns);
+      add(ns);
     }
     for(Attr attr: attrs2) {
       createAttribute(attr);
     }
   }
 
+  private boolean isKnownNs(String pPrefix, String pNamespaceURI) {
+    for(Iterator<List<Namespace>> it = mNamespaceContext.descendingIterator(); it.hasNext();) {
+      final List<Namespace> namespaces = it.next();
+      for(Namespace namespace:namespaces) {
+        if (pPrefix.equals(namespace.getPrefix())) {
+          // If we have the prefix, we can't go look back
+          return pNamespaceURI.equals(namespace.getNamespaceURI());
+        }
+      }
+    }
+    return false;
+  }
+
   private void createAttribute(Attr attr) {
-    add(mXef.createAttribute(attr.getPrefix(), attr.getNamespaceURI(), attr.getLocalName(), attr.getValue()));
+    final String prefix = getPrefix(attr);
+    if (! isKnownNs(prefix, attr.getNamespaceURI())) {
+      Namespace ns = mXef.createNamespace(prefix, attr.getNamespaceURI());
+      add(ns);
+      mNamespaceContext.peekLast().add(ns);
+    }
+    add(mXef.createAttribute(prefix, attr.getNamespaceURI(), attr.getLocalName(), attr.getValue()));
   }
 
   @Override
   public Object getProperty(String pName) throws IllegalArgumentException {
     throw new IllegalArgumentException("Not supported");
+  }
+
+  private static String getPrefix(Node pNode) {
+    final String prefix = pNode.getPrefix();
+    return prefix==null ? XMLConstants.DEFAULT_NS_PREFIX: prefix;
   }
 
 }
