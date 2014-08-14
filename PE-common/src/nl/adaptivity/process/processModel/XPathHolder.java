3b4e6c0c26a84af23692294bc742667460c9ba05
Paul de Vrieze <paul@devrieze.net>
2014-08-14 16:41:40 +0100

    Have a self node path when no path is specified.

--- PE-common/src/nl/adaptivity/process/processModel/XPathHolder.java
+++ PE-common/src/nl/adaptivity/process/processModel/XPathHolder.java
@@ -10,7 +10,16 @@ import javax.xml.xpath.XPathFactory;
 @XmlAccessorType(XmlAccessType.NONE)
 public class XPathHolder {
 
-  protected XPathExpression path;
+  private static final XPathExpression SELF_PATH;
+  private XPathExpression path;
+
+  static {
+    try {
+      SELF_PATH = XPathFactory.newInstance().newXPath().compile(".");
+    } catch (XPathExpressionException e) {
+      throw new RuntimeException(e);
+    }
+  }
 
   public XPathHolder() {
     super();
@@ -34,7 +43,9 @@ public class XPathHolder {
     }
   }
 
-  public XPathExpression getXPath() { return path; }
+  public XPathExpression getXPath() {
+    return path !=null ? path : SELF_PATH;
+  }
 
   public void setXPath(XPathExpression xpath) { path = xpath; }
 
