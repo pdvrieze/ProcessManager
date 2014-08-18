b913fcb4a34c50f5db2a7d04e64f6fcc1496c89a
Paul de Vrieze <paul@devrieze.net>
2014-08-18 15:59:49 +0100

    Remove non-filter based transform methods. They were now unused.

--- PE-common/src/nl/adaptivity/process/engine/PETransformer.java
+++ PE-common/src/nl/adaptivity/process/engine/PETransformer.java
@@ -5,10 +5,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-import javax.servlet.http.HttpServletResponse;
 import javax.xml.bind.JAXB;
 import javax.xml.bind.JAXBElement;
 import javax.xml.namespace.QName;
@@ -30,7 +27,6 @@ import javax.xml.transform.Source;
 import javax.xml.transform.dom.DOMResult;
 import javax.xml.transform.dom.DOMSource;
 
-import nl.adaptivity.messaging.HttpResponseException;
 import nl.adaptivity.process.util.Constants;
 import nl.adaptivity.util.activation.Sources;
 import nl.adaptivity.util.xml.AbstractBufferedEventReader;
@@ -378,161 +374,6 @@ public class PETransformer {
 
   private XMLEventReader createFilter(XMLEventReader pInput) {
     return new MyFilter(aContext, pInput);
-  }
-
-  public void transform2(Source source, final Result result) throws XMLStreamException {
-    final XMLInputFactory xif = XMLInputFactory.newInstance();
-    final XMLOutputFactory xof = XMLOutputFactory.newInstance();
-    // Use a reader as a DOMSource is not directly supported by stax for some stupid reason.
-    final XMLEventReader xer = xif.createXMLEventReader(Sources.toReader(source));
-    final XMLEventWriter xew = xof.createXMLEventWriter(result);
-
-    final XMLEventFactory xef = XMLEventFactory.newInstance();
-
-    while (xer.hasNext()) {
-      final XMLEvent event = xer.nextEvent();
-      if (event.isStartElement()) {
-        final StartElement se = event.asStartElement();
-        final QName eName = se.getName();
-        if (Constants.MODIFY_NS.toString().equals(eName.getNamespaceURI())) {
-          @SuppressWarnings("unchecked")
-          final Iterator<Attribute> attributes = se.getAttributes();
-          if (eName.getLocalPart().equals("attribute")) {
-            writeAttribute(aContext, xef, xer, attributes, xew);
-          } else if (eName.getLocalPart().equals("element") ||
-              eName.getLocalPart().equals("value")) {
-            writeElement(aContext, xef, xer, attributes, xew);
-          } else {
-            throw new HttpResponseException(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Unsupported activity modifier ");
-          }
-        } else {
-          xew.add(se);
-        }
-      } else {
-        if (event.isCharacters()) {
-          final Characters c = event.asCharacters();
-          final String charData = c.getData();
-          int i;
-          for (i = 0; i < charData.length(); ++i) {
-            if (!Character.isWhitespace(charData.charAt(i))) {
-              break;
-            }
-          }
-          if (i == charData.length()) {
-            continue; // ignore it, and go to next event
-          }
-        }
-
-        if (event instanceof Namespace) {
-
-          final Namespace ns = (Namespace) event;
-          if (!ns.getNamespaceURI().equals(Constants.MODIFY_NS)) {
-            xew.add(event);
-          }
-        } else {
-          try {
-            xew.add(event);
-          } catch (final IllegalStateException e) {
-            final StringBuilder errorMessage = new StringBuilder("Error adding event: ");
-            errorMessage.append(event.toString()).append(' ');
-            if (event.isStartElement()) {
-              errorMessage.append('<').append(event.asStartElement().getName()).append('>');
-            } else if (event.isEndElement()) {
-              errorMessage.append("</").append(event.asEndElement().getName()).append('>');
-            }
-            getLogger().log(Level.WARNING, errorMessage.toString(), e);
-
-            throw e;
-          }
-        }
-      }
-    }
-  }
-
-  private static Logger getLogger() {
-    Logger logger = Logger.getLogger(PETransformer.class.getName());
-    return logger;
-  }
-
-  private static void writeElement(PETransformerContext pContext, XMLEventFactory pXef, final XMLEventReader in, final Iterator<Attribute> pAttributes, final XMLEventWriter out) throws XMLStreamException {
-    String valueName = null;
-    {
-      while (pAttributes.hasNext()) {
-        final Attribute attr = pAttributes.next();
-        final String attrName = attr.getName().getLocalPart();
-        if ("value".equals(attrName)) {
-          valueName = attr.getValue();
-        }
-      }
-    }
-    {
-      final XMLEvent ev = in.nextEvent();
-
-      while (!ev.isEndElement()) {
-        if (ev.isStartElement()) {
-          throw new MessagingFormatException("Violation of schema");
-        }
-        if (ev.isAttribute()) {
-          final Attribute attr = (Attribute) ev;
-          final String attrName = attr.getName().getLocalPart();
-          if ("value".equals(attrName)) {
-            valueName = attr.getValue();
-          }
-        }
-      }
-    }
-    if (valueName != null) {
-      for(XMLEvent value: pContext.resolveElementValue(pXef, valueName)){
-        out.add(value);
-      }
-    } else {
-      throw new MessagingFormatException("Missing parameter name");
-    }
-
-  }
-
-  private static void writeAttribute(PETransformerContext pContext, XMLEventFactory xef, final XMLEventReader in, final Iterator<Attribute> pAttributes, final XMLEventWriter out) throws XMLStreamException {
-    String valueName = null;
-    String paramName = null;
-    {
-      while (pAttributes.hasNext()) {
-        final Attribute attr = pAttributes.next();
-        final String attrName = attr.getName().getLocalPart();
-        if ("value".equals(attrName)) {
-          valueName = attr.getValue();
-        } else if ("name".equals(attrName)) {
-          paramName = attr.getValue();
-        }
-      }
-    }
-    {
-      final XMLEvent ev = in.nextEvent();
-
-      while (!ev.isEndElement()) {
-        if (ev.isStartElement()) {
-          throw new MessagingFormatException("Violation of schema");
-        }
-        if (ev.isAttribute()) {
-          final Attribute attr = (Attribute) ev;
-          final String attrName = attr.getName().getLocalPart();
-          if ("value".equals(attrName)) {
-            valueName = attr.getValue();
-          } else if ("name".equals(attrName)) {
-            paramName = attr.getValue();
-          }
-        }
-      }
-    }
-    if (valueName != null) {
-      if (paramName==null) {
-        paramName = pContext.resolveAttributeName(valueName);
-      }
-      String value = pContext.resolveAttributeValue(valueName);
-      out.add(xef.createAttribute(paramName, value));
-    } else {
-      throw new MessagingFormatException("Missing parameter name");
-    }
-
   }
 
 }
