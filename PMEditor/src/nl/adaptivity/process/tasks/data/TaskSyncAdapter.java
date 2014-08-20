a9aaf3b26e481120b7cd6df651e39e91e211c459
Paul de Vrieze <paul.devrieze@gmail.com>
2014-08-20 20:58:02 +0000

    Sync labels properly. Also mark the task as up to date with respect to the server.
    git-svn-id: https://darwin.bournemouth.ac.uk/pdvrieze/svn/darwin/projects/PMEditor/trunk@1535 3bae1aa3-1c52-4c45-a669-1fb9b31e7360

--- PMEditor/src/nl/adaptivity/process/tasks/data/TaskSyncAdapter.java
+++ PMEditor/src/nl/adaptivity/process/tasks/data/TaskSyncAdapter.java
@@ -6,7 +6,6 @@ import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
