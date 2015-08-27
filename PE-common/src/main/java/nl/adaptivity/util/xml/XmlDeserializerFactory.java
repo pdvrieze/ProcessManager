8226161b12d09c33f9f3bb0e6a4dd105f5fe55b8
Paul de Vrieze <paul@devrieze.net>
2015-08-27 13:51:43 +0100

    Add a deserialization way from streamreaders (and make the serializable adapter use that). This should allow a move away from jaxb.

---
+++ PE-common/src/main/java/nl/adaptivity/util/xml/XmlDeserializerFactory.java
@@ -0,0 +1,14 @@
+package nl.adaptivity.util.xml;
+
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamWriter;
+
+
+/**
+ * Created by pdvrieze on 27/08/15.
+ */
+public interface XmlDeserializerFactory<T> {
+
+  public T deserialize(XMLStreamReader in);
+
+}
