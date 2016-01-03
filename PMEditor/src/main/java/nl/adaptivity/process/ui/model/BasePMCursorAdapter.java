0d25e58984a4d29cc2298503d885bd4574117328
Paul de Vrieze <paul.devrieze@gmail.com>
2016-01-03 20:41:29 +0000

    Split out most of the PMCursorAdapter code to a base class that is not dependent on a specific ViewDataBinding/layout.
    git-svn-id: https://darwin.bournemouth.ac.uk/pdvrieze/svn/darwin/projects/PMEditor/trunk@1827 3bae1aa3-1c52-4c45-a669-1fb9b31e7360

---
+++ PMEditor/src/main/java/nl/adaptivity/process/ui/model/BasePMCursorAdapter.java
@@ -0,0 +1,80 @@
+package nl.adaptivity.process.ui.model;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
+import android.support.annotation.LayoutRes;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import nl.adaptivity.android.util.ClickableCursorAdapter;
+import nl.adaptivity.android.util.SelectableCursorAdapter;
+import nl.adaptivity.process.editor.android.R;
+import nl.adaptivity.process.models.ProcessModelProvider.ProcessModels;
+import nl.adaptivity.process.ui.model.BasePMCursorAdapter.BasePMViewHolder;
+import nl.adaptivity.process.ui.model.PMCursorAdapter.PMViewHolder;
+
+
+/**
+ * Created by pdvrieze on 02/01/16.
+ */
+public abstract class BasePMCursorAdapter<VH extends BasePMViewHolder> extends SelectableCursorAdapter<VH> {
+
+  public abstract class BasePMViewHolder<B extends ViewDataBinding> extends ClickableCursorAdapter<BasePMViewHolder<B>>.ClickableViewHolder {
+
+    protected final B binding;
+
+    public BasePMViewHolder(@LayoutRes final int itemLayout, final LayoutInflater inflater, final ViewGroup parent) {
+      this((B) DataBindingUtil.inflate(inflater, itemLayout, parent, false));
+    }
+
+    private BasePMViewHolder(final B binding) {
+      super(binding.getRoot());
+      this.binding = binding;
+    }
+  }
+
+  private static final String TAG = "BasePMCursorAdapter";
+  protected LayoutInflater mInflater;
+  protected int mNameColumn;
+
+  public BasePMCursorAdapter(final Context context, final Cursor cursor, final boolean allowUnselection) {
+    super(context, cursor, allowUnselection);
+    mInflater = LayoutInflater.from(context);
+    updateColumnIndices(cursor);
+    setHasStableIds(true);
+  }
+
+  protected void updateColumnIndices(Cursor c) {
+    if (c == null) {
+      mNameColumn = -1;
+    } else {
+      mNameColumn = c.getColumnIndex(ProcessModels.COLUMN_NAME);
+    }
+  }
+
+  @Override
+  public void changeCursor(Cursor cursor) {
+    super.changeCursor(cursor); // This will call swapCursor, so no new update of indices is needed
+  }
+
+  @Override
+  public Cursor swapCursor(Cursor newCursor) {
+    Log.d(TAG, "Swapping processmodel cursor");
+    final Cursor result = super.swapCursor(newCursor);
+    updateColumnIndices(newCursor);
+    return result;
+  }
+
+  @Override
+  public final void onBindViewHolder(final VH viewHolder, final Cursor cursor) {
+    super.onBindViewHolder(viewHolder, cursor);
+    addPendingBindings(viewHolder, cursor);
+    viewHolder.binding.executePendingBindings();
+  }
+
+  protected void addPendingBindings(final VH viewHolder, final Cursor cursor) {
+  }
+}
--- PMEditor/src/main/java/nl/adaptivity/process/ui/model/PMCursorAdapter.java
+++ PMEditor/src/main/java/nl/adaptivity/process/ui/model/PMCursorAdapter.java
@@ -3,6 +3,8 @@ package nl.adaptivity.process.ui.model;
 import android.content.Context;
 import android.database.Cursor;
 import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
+import android.support.annotation.LayoutRes;
 import android.support.v7.widget.RecyclerView.ViewHolder;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -18,56 +20,17 @@ import nl.adaptivity.process.ui.model.PMCursorAdapter.PMViewHolder;
 /**
  * Created by pdvrieze on 28/12/15.
  */
-public final class PMCursorAdapter extends SelectableCursorAdapter<PMViewHolder> {
+public final class PMCursorAdapter extends BasePMCursorAdapter<PMViewHolder> {
 
-  private static final String TAG = "PMCursorAdapter";
-
-  public class PMViewHolder extends ClickableCursorAdapter.ClickableViewHolder {
-
-    final ModelListitemBinding binding;
+  public class PMViewHolder extends BasePMCursorAdapter<PMViewHolder>.BasePMViewHolder<ModelListitemBinding> {
 
     public PMViewHolder(final LayoutInflater inflater, final ViewGroup parent) {
-      super(inflater.inflate(R.layout.model_listitem, parent, false));
-      binding = DataBindingUtil.bind(itemView);
+      super(R.layout.model_listitem, inflater, parent);
     }
   }
-
-  private LayoutInflater mInflater;
-  private int mNameColumn;
 
   public PMCursorAdapter(Context context, Cursor c) {
     super(context, c, false);
-    mInflater = LayoutInflater.from(context);
-    updateColumnIndices(c);
-    setHasStableIds(true);
-  }
-
-  protected void updateColumnIndices(Cursor c) {
-    if (c == null) {
-      mNameColumn = -1;
-    } else {
-      mNameColumn = c.getColumnIndex(ProcessModels.COLUMN_NAME);
-    }
-  }
-
-  @Override
-  public void changeCursor(Cursor cursor) {
-    super.changeCursor(cursor);
-  }
-
-  @Override
-  public Cursor swapCursor(Cursor newCursor) {
-    Log.d(TAG, "Swapping processmodel cursor");
-    final Cursor result = super.swapCursor(newCursor);
-    updateColumnIndices(newCursor);
-    return result;
-  }
-
-  @Override
-  public void onBindViewHolder(final PMViewHolder viewHolder, final Cursor cursor) {
-    super.onBindViewHolder(viewHolder, cursor);
-    viewHolder.binding.setName(mNameColumn >= 0 ? cursor.getString(mNameColumn) : null);
-    viewHolder.binding.executePendingBindings();
   }
 
   @Override
@@ -75,8 +38,8 @@ public final class PMCursorAdapter extends SelectableCursorAdapter<PMViewHolder>
     return new PMViewHolder(mInflater, parent);
   }
 
-  @Override
-  public void onClickView(final ViewHolder viewHolder) {
-    super.onClickView(viewHolder);
+  protected void addPendingBindings(final PMViewHolder viewHolder, final Cursor cursor) {
+    viewHolder.binding.setName(mNameColumn >= 0 ? cursor.getString(mNameColumn) : null);
   }
+
 }
