87ef977838dbd87c30a24377eed270410b62b658
Paul de Vrieze <paul.devrieze@gmail.com>
2016-02-11 13:04:39 +0000

    Remove irrelevant DDL scripts.

--- darwin-sql/chatbox/boxes.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE `boxes`;
-CREATE TABLE `boxes` (
-  `boxId` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) DEFAULT NULL,
-  `owner` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`boxId`)
-) ENGINE=InnoDB CHARSET=utf8
--- darwin-sql/chatbox/tokens.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE `tokens`;
-
-CREATE TABLE `tokens` (
-  `tokenId` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`tokenId`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB CHARSET=utf8;
