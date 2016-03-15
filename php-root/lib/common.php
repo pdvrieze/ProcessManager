f4ae4eecb94c3acdcd2ffc51a9d0f20a7080ec93
Paul de Vrieze <paul.devrieze@gmail.com>
2016-03-15 21:52:30 +0000

    Handle simple warning caused by the xml declaration. Making it part of an echo statement fixes it.

--- php-root/lib/common.php
+++ php-root/lib/common.php
@@ -108,8 +108,8 @@
   function darwinHeader($title="Darwin", $pagetitle=NULL, $checkuser=TRUE, $lightweight=FALSE) {
   	if (getHeader("X-Darwin")!==NULL) {
     	header("Content-Type: text/xml");
+      echo '<?xml version="1.0" ?>'
   	?>
-<?xml version="1.0" ?>
 <root>
 <title><?php echo $title; ?></title>
 <body>
