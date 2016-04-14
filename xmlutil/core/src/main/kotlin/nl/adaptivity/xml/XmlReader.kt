d9ea7b5b187e1a85d87ba4c47ebb013b9535502c
Paul de Vrieze <paul.devrieze@gmail.com>
2016-04-14 14:11:47 +0100

    Convert to Kotlin

---
+++ xmlutil/core/src/main/kotlin/nl/adaptivity/xml/XmlReader.kt
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2016.
+ *
+ * This file is part of ProcessManager.
+ *
+ * ProcessManager is free software: you can redistribute it and/or modify it under the terms of version 3 of the
+ * GNU Lesser General Public License as published by the Free Software Foundation.
+ *
+ * ProcessManager is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with Foobar.  If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
+
+package nl.adaptivity.xml
+
+import nl.adaptivity.xml.XmlStreaming.EventType
+
+import javax.xml.namespace.NamespaceContext
+import javax.xml.namespace.QName
+
+
+/**
+ * Created by pdvrieze on 15/11/15.
+ */
+interface XmlReader {
+
+  /** Get the next tag. This must call next, not use the underlying stream.  */
+  @Throws(XmlException::class)
+  fun nextTag(): EventType
+
+  @Throws(XmlException::class)
+  operator fun hasNext(): Boolean
+
+  @Throws(XmlException::class)
+  operator fun next(): EventType?
+
+  val namespaceUri: CharSequence
+
+  val localName: CharSequence
+
+  val prefix: CharSequence
+
+  val name: QName
+
+  @Throws(XmlException::class)
+  fun require(type: EventType, namespace: CharSequence, name: CharSequence)
+
+  val depth: Int
+
+  val text: CharSequence
+
+  val attributeCount: Int
+
+  @Throws(XmlException::class)
+  fun getAttributeNamespace(i: Int): CharSequence?
+
+  @Throws(XmlException::class)
+  fun getAttributePrefix(i: Int): CharSequence?
+
+  @Throws(XmlException::class)
+  fun getAttributeLocalName(i: Int): CharSequence
+
+  @Throws(XmlException::class)
+  fun getAttributeName(i: Int): QName
+
+  @Throws(XmlException::class)
+  fun getAttributeValue(i: Int): CharSequence
+
+  val eventType: EventType
+
+  @Throws(XmlException::class)
+  fun getAttributeValue(nsUri: CharSequence, localName: CharSequence): CharSequence?
+
+  val namespaceStart: Int
+
+  val namespaceEnd: Int
+
+  @Throws(XmlException::class)
+  fun getNamespacePrefix(i: Int): CharSequence
+
+  @Throws(XmlException::class)
+  fun close()
+
+  @Throws(XmlException::class)
+  fun getNamespaceUri(i: Int): CharSequence
+
+  @Throws(XmlException::class)
+  fun getNamespacePrefix(namespaceUri: CharSequence): CharSequence?
+
+  val isWhitespace: Boolean
+
+  val isEndElement: Boolean
+
+  val isCharacters: Boolean
+
+  val isStartElement: Boolean
+
+  @Throws(XmlException::class)
+  fun getNamespaceUri(prefix: CharSequence): String?
+
+  /** Get some information on the current location in the file. This is implementation dependent.  */
+  val locationInfo: String
+
+  val namespaceContext: NamespaceContext
+
+  val encoding: CharSequence
+
+  val standalone: Boolean?
+
+  val version: CharSequence
+}
