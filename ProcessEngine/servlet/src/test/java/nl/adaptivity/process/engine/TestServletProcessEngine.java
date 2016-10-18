68ebaa8a9a02f781575a68ca4e2fd6fb3e883df2
Paul de Vrieze <paul.devrieze@gmail.com>
2016-10-18 10:37:45 +0100

    Fix compilation with context.

--- ProcessEngine/servlet/src/test/java/nl/adaptivity/process/engine/TestServletProcessEngine.java
+++ ProcessEngine/servlet/src/test/java/nl/adaptivity/process/engine/TestServletProcessEngine.java
@@ -16,13 +16,12 @@
 
 package nl.adaptivity.process.engine;
 
+import net.devrieze.util.Transaction;
 import net.devrieze.util.TransactionFactory;
 import nl.adaptivity.process.MemTransactionedHandleMap;
-import nl.adaptivity.process.StubTransaction;
 import nl.adaptivity.process.StubTransactionFactory;
 import nl.adaptivity.process.engine.processModel.ProcessNodeInstance;
 import nl.adaptivity.process.engine.servlet.ServletProcessEngine;
-import nl.adaptivity.process.processModel.engine.ProcessModelImpl;
 
 import java.net.URI;
 
@@ -32,17 +31,17 @@ import java.net.URI;
  */
 public class TestServletProcessEngine extends ServletProcessEngine {
 
-  private final MemTransactionedHandleMap<ProcessModelImpl> mProcessModels;
-  private final MemTransactionedHandleMap<ProcessInstance> mProcessInstances;
-  private final MemTransactionedHandleMap<ProcessNodeInstance> mProcessNodeInstances;
+  private final MemProcessModelMap mProcessModels;
+  private final MemTransactionedHandleMap<ProcessInstance<Transaction>> mProcessInstances;
+  private final MemTransactionedHandleMap<ProcessNodeInstance<Transaction>> mProcessNodeInstances;
   private TransactionFactory mTransactionFactory;
 
   public TestServletProcessEngine() {
     mTransactionFactory = new StubTransactionFactory();
-    mProcessModels = new MemTransactionedHandleMap<>();
+    mProcessModels = new MemProcessModelMap();
     mProcessInstances = new MemTransactionedHandleMap<>();
     mProcessNodeInstances = new MemTransactionedHandleMap<>();
-    ProcessEngine<StubTransaction> engine = ProcessEngine.newTestInstance(this, mTransactionFactory, mProcessModels, mProcessInstances, mProcessNodeInstances);
+    ProcessEngine<Transaction> engine = ProcessEngine.<Transaction>newTestInstance(this, mTransactionFactory, mProcessModels, mProcessInstances, mProcessNodeInstances, false);
     init(engine);
   }
 
