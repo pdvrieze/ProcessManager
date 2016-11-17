e292772fea4b297a7dcca98082904dafe4a83353
Paul de Vrieze <paul.devrieze@gmail.com>
2016-11-17 21:03:48 +0000

    Add a security provider that only provides access to owners, admins and the system user.

---
+++ java-common/src/main/kotlin/net/devrieze/util/security/OwnerOnlySecurityProvider.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2016.
+ *
+ * This file is part of ProcessManager.
+ *
+ * ProcessManager is free software: you can redistribute it and/or modify it under the terms of version 3 of the
+ * GNU Lesser General Public License as published by the Free Software Foundation.
+ *
+ * ProcessManager is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with Foobar.  If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
+
+package net.devrieze.util.security
+
+import net.devrieze.util.security.SecurityProvider.PermissionResult
+import java.security.Principal
+
+/**
+ * SecurityProvider that is sensible in allowing only owners and admins to perform operations.
+ */
+class OwnerOnlySecurityProvider(val adminRoles:Set<String>) : BaseSecurityProvider() {
+
+  override fun getPermission(permission: SecurityProvider.Permission, subject: Principal?, secureObject: SecureObject): PermissionResult {
+    if (subject==null) return PermissionResult.UNAUTHENTICATED
+    if (subject== SecurityProvider.SYSTEMPRINCIPAL) { return PermissionResult.GRANTED }
+    val owner = secureObject.owner
+    if (owner==null) {
+      if (subject is RolePrincipal && adminRoles.any { subject.hasRole(it) }) return PermissionResult.GRANTED
+
+      return PermissionResult.DENIED
+    }
+
+    if (subject == owner) { return PermissionResult.GRANTED }
+    return PermissionResult.DENIED
+  }
+
+  override fun getPermission(permission: SecurityProvider.Permission,
+                             subject: Principal?): PermissionResult {
+    return if (subject==null) PermissionResult.UNAUTHENTICATED else PermissionResult.GRANTED
+  }
+
+  override fun getPermission(permission: SecurityProvider.Permission, subject: Principal?, objectPrincipal: Principal): PermissionResult {
+    if (subject==null) return PermissionResult.UNAUTHENTICATED
+    if (subject== SecurityProvider.SYSTEMPRINCIPAL) { return PermissionResult.GRANTED }
+    if (subject == objectPrincipal) { return PermissionResult.GRANTED }
+    if (subject is RolePrincipal) { if (adminRoles.any { subject.hasRole(it) }) return PermissionResult.GRANTED }
+    return PermissionResult.DENIED
+  }
+}
