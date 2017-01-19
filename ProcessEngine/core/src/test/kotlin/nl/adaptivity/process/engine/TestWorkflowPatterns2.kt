0cef9f31d3b42a8468f2a28028dd0071175c06ab
Paul de Vrieze <paul.devrieze@gmail.com>
2017-01-19 15:00:18 +0000

    Initial replacement list for the test setup

--- ProcessEngine/core/src/test/kotlin/nl/adaptivity/process/engine/TestWorkflowPatterns2.kt
+++ ProcessEngine/core/src/test/kotlin/nl/adaptivity/process/engine/TestWorkflowPatterns2.kt
@@ -17,6 +17,9 @@
 package nl.adaptivity.process.engine
 
 import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.xdescribe
 import org.jetbrains.spek.api.include
 
 /**
@@ -24,5 +27,84 @@ import org.jetbrains.spek.api.include
  */
 class TestWorkflowPatterns2 : Spek(
   {
-    include(WCP1())
+    describe("Control flow patterns") {
+
+      describe("Basic control-flow patterns") {
+
+        describe("WCP1: A sequential process") {
+          include(WCP1())
+        }
+
+        xdescribe("WCP2: Parallel split") {
+//          testWCP2()
+        }
+
+        xdescribe("WCP3: Synchronization / And join") {
+//          testWCP3()
+        }
+
+        xdescribe("WCP4: XOR split") {
+//          testWCP4()
+        }
+
+        xdescribe("WCP5: simple-merge") {
+//          testWCP5()
+        }
+      }
+
+      xdescribe("Advanced branching and synchronization patterns") {
+        describe("WCP6: multi-choice / or-split") {
+          given("ac1.condition=true, ac2.condition=false") {
+//            testWCP6(true, false)
+          }
+          given("ac1.condition=false, ac2.condition=true") {
+//            testWCP6(false, true)
+          }
+          given("ac1.condition=true, ac2.condition=true") {
+//            testWCP6(true, true)
+          }
+
+        }
+
+        describe("WCP7: structured synchronized merge") {
+          given("ac1.condition=true, ac2.condition=false") {
+//            testWCP7(true, false)
+          }
+          given("ac1.condition=false, ac2.condition=true") {
+//            testWCP7(false, true)
+          }
+          given("ac1.condition=true, ac2.condition=true") {
+//            testWCP7(true, true)
+          }
+
+        }
+
+        xdescribe("WCP8: Multi-merge", "Multiple instantiations of a single node are not yet supported") {
+//          testWCP8()
+        }
+
+        describe("WCP9: Structured Discriminator") {
+//          testWCP9()
+        }
+      }
+
+      xdescribe("Structural patterns") {
+        xdescribe("WCP10: arbitrary cycles", "Multiple instantiations of a single node are not yet supported") {
+
+        }
+
+        describe("WCP11: Implicit termination") {
+//          testWCP11()
+        }
+      }
+    }
+
+    xdescribe("Abstract syntax patterns") {
+      describe("WASP4: Vertical modularisation (subprocesses)") {
+//        testWASP4()
+
+      }
+    }
+
+
   })
