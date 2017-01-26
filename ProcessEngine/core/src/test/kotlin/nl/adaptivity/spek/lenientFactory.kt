d4cfcf6fe0737459f2be739a0d7ae184a102c993
Paul de Vrieze <paul.devrieze@gmail.com>
2017-01-26 09:52:07 +0000

    Create an object that is more lenient in creating instances of a spek class. It will find the no-arg constructor instead of assuming it's the primary.

---
+++ ProcessEngine/core/src/test/kotlin/nl/adaptivity/spek/lenientFactory.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2017.
+ *
+ * This file is part of ProcessManager.
+ *
+ * ProcessManager is free software: you can redistribute it and/or modify it under the terms of version 3 of the
+ * GNU Lesser General Public License as published by the Free Software Foundation.
+ *
+ * ProcessManager is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with ProcessManager.  If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
+
+package nl.adaptivity.spek
+
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.lifecycle.InstanceFactory
+import kotlin.reflect.KClass
+import kotlin.reflect.primaryConstructor
+
+/**
+ * Created by pdvrieze on 26/01/17.
+ */
+object lenientFactory : InstanceFactory {
+  override fun <T : Spek> create(spek: KClass<T>): T {
+    return spek.objectInstance ?: spek.constructors.first { it.parameters.isEmpty() }.call()
+  }
+}
