5cef65d7d5c178abeafa6d81f0a62d986ddf88c8
Paul de Vrieze <paul.devrieze@gmail.com>
2018-04-07 15:37:36 +0100

    Convert to Kotlin to try to fix various nullability/initialisation issues.

---
+++ PMEditor/src/main/java/nl/adaptivity/process/ui/model/ProcessModelListFragment.kt
@@ -0,0 +1,289 @@
+/*
+ * Copyright (c) 2018.
+ *
+ * This file is part of ProcessManager.
+ *
+ * ProcessManager is free software: you can redistribute it and/or modify it under the terms of version 3 of the
+ * GNU Lesser General Public License as published by the Free Software Foundation.
+ *
+ * ProcessManager is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with ProcessManager.  If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
+
+package nl.adaptivity.process.ui.model
+
+import android.app.Activity
+import android.content.ContentUris
+import android.content.Intent
+import android.content.SyncStatusObserver
+import android.database.Cursor
+import android.net.Uri
+import android.os.Bundle
+import android.provider.BaseColumns
+import android.support.v4.app.LoaderManager.LoaderCallbacks
+import android.support.v4.content.CursorLoader
+import android.support.v4.content.Loader
+import android.support.v4.widget.SwipeRefreshLayout
+import android.support.v4.widget.SwipeRefreshLayout.OnRefreshListener
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.view.*
+import android.widget.AdapterView
+import nl.adaptivity.android.recyclerview.ClickableAdapter
+import nl.adaptivity.android.recyclerview.ClickableAdapter.OnItemClickListener
+import nl.adaptivity.android.recyclerview.ClickableViewHolder
+import nl.adaptivity.android.recyclerview.SelectableAdapter
+import nl.adaptivity.android.util.GetNameDialogFragment
+import nl.adaptivity.android.util.GetNameDialogFragment.GetNameDialogFragmentCallbacks
+import nl.adaptivity.android.util.MasterListFragment
+import nl.adaptivity.process.diagram.RootDrawableProcessModel
+import nl.adaptivity.process.ui.ProcessSyncManager
+import nl.adaptivity.process.diagram.DrawableProcessNode
+import nl.adaptivity.process.diagram.LayoutAlgorithm
+import nl.adaptivity.process.editor.android.PMEditor
+import nl.adaptivity.process.editor.android.PMParser
+import nl.adaptivity.process.editor.android.R
+import nl.adaptivity.process.models.ProcessModelProvider
+import nl.adaptivity.process.models.ProcessModelProvider.ProcessModels
+import nl.adaptivity.process.ui.main.SettingsActivity
+import nl.adaptivity.process.ui.model.PMCursorAdapter.PMViewHolder
+import nl.adaptivity.sync.RemoteXmlSyncAdapter
+import nl.adaptivity.sync.RemoteXmlSyncAdapter.XmlBaseColumns
+import nl.adaptivity.sync.SyncManager.SyncStatusObserverData
+
+import java.io.IOException
+import java.util.ArrayList
+import java.util.UUID
+
+
+/**
+ * A list fragment representing a list of ProcessModels. This fragment also
+ * supports tablet devices by allowing list items to be given an 'activated'
+ * state upon selection. This helps indicate which item is currently being
+ * viewed in a [ProcessModelDetailFragment].
+ *
+ *
+ * Activities containing this fragment MUST implement the [ListCallbacks]
+ * interface.
+ */
+/**
+ * Mandatory empty constructor for the fragment manager to instantiate the
+ * fragment (e.g. upon screen orientation changes).
+ */
+class ProcessModelListFragment : MasterListFragment<ProcessSyncManager>(), LoaderCallbacks<Cursor>, GetNameDialogFragmentCallbacks, OnRefreshListener, SelectableAdapter.OnSelectionListener<ClickableViewHolder>, OnItemClickListener<PMViewHolder> {
+
+    private lateinit var swipeRefresh: SwipeRefreshLayout
+    private var syncObserver: SyncStatusObserver? = null
+    private var syncObserverHandle: SyncStatusObserverData? = null
+    private var manualSync: Boolean = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setHasOptionsMenu(true)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        loaderManager.initLoader(LOADERID, null, this)
+        val adapter = PMCursorAdapter(activity, null)
+        adapter.onSelectionListener = this
+        adapter.setOnItemClickListener(this)
+        setListAdapter(adapter)
+
+        syncObserver = SyncStatusObserver { activity!!.runOnUiThread { updateSyncState() } }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.refreshablerecyclerview, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView!!.layoutManager = LinearLayoutManager(activity)
+        swipeRefresh = view.findViewById<View>(R.id.swipeRefresh) as SwipeRefreshLayout
+        swipeRefresh.setOnRefreshListener(this)
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onSelectionChanged(adapter: SelectableAdapter<ClickableViewHolder>) {
+        if (adapter.selectedId != RecyclerView.NO_ID) {
+            doOnItemSelected(adapter.selectedPos, adapter.selectedId)
+        }
+
+    }
+
+    override fun onClickItem(adapter: ClickableAdapter<out PMViewHolder>, viewHolder: PMViewHolder): Boolean {
+        return doOnItemClicked(viewHolder.adapterPosition, viewHolder.itemId)
+    }
+
+    override fun getListAdapter(): PMCursorAdapter? {
+        return super.getListAdapter() as PMCursorAdapter
+    }
+
+    override fun onResume() {
+        super.onResume()
+        syncObserverHandle = callbacks.syncManager?.addOnStatusChangeObserver(ProcessModelProvider.AUTHORITY,
+                                                                               syncObserver!!)
+        syncObserver!!.onStatusChanged(0) // trigger status sync
+    }
+
+    override fun onPause() {
+        super.onPause()
+        val syncManager = callbacks.syncManager
+        if (syncObserverHandle != null) {
+            syncManager!!.removeOnStatusChangeObserver(syncObserverHandle!!)
+            syncObserverHandle = null
+        }
+        syncManager?.verifyNoObserversActive()
+
+    }
+
+    /**
+     * Called by the refresh view when a manual refresh is requested.
+     */
+    override fun onRefresh() {
+        doManualRefresh()
+    }
+
+    private fun doManualRefresh() {
+        callbacks.syncManager?.run {
+          requestSyncProcessModelList(true, ProcessSyncManager.DEFAULT_MIN_AGE)
+          manualSync = true
+          updateSyncState()
+        }
+
+    }
+
+    private fun updateSyncState() {
+        callbacks.syncManager?.let { syncManager ->
+          if (!syncManager.isSyncable(ProcessModelProvider.AUTHORITY)) {
+            swipeRefresh.isRefreshing = false
+          } else {
+            val syncActive = syncManager.isProcessModelSyncActive
+            val syncPending = syncManager.isProcessModelSyncPending
+            if (syncActive || !syncPending) {
+              manualSync = false
+            }
+            val sync = syncActive || manualSync
+            swipeRefresh.isRefreshing = sync
+          }
+        }
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        inflater!!.inflate(R.menu.pmlist_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item!!.itemId) {
+            R.id.menu_add_pm    -> {
+                createNewPM()
+                return true
+            }
+            R.id.ac_import      -> {
+                val importIntent = Intent(Intent.ACTION_GET_CONTENT)
+                importIntent.addCategory(Intent.CATEGORY_OPENABLE)
+                importIntent.type = "*/*"
+                startActivityForResult(Intent.createChooser(importIntent, "Import from"), REQUEST_IMPORT)
+                return true
+            }
+            R.id.menu_settings  -> {
+                val settingsIntent = Intent(activity, SettingsActivity::class.java)
+                startActivity(settingsIntent)
+                return true
+            }
+            R.id.ac_sync_models -> {
+                doManualRefresh()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == REQUEST_IMPORT) {
+                try {
+                    val `in` = activity!!.contentResolver.openInputStream(data!!.data!!)
+                    try {
+                        val pm = PMParser.parseProcessModel(`in`, LayoutAlgorithm.nullalgorithm<DrawableProcessNode>(),
+                                                            LayoutAlgorithm())
+                        pm!!.handle = -1L
+                        val uri = ProcessModelProvider.newProcessModel(activity, pm.build())
+                        val id = ContentUris.parseId(uri)
+                        if (isVisible && !isRemoving) {
+                            doOnItemSelected(AdapterView.INVALID_POSITION, id)
+                        }
+                    } finally {
+                        `in`!!.close()
+                    }
+                } catch (e: IOException) {
+                    Log.e(TAG, "Failure to import file", e)
+                }
+
+            }
+        }
+    }
+
+    private fun createNewPM() {
+        GetNameDialogFragment.show(fragmentManager!!, DLG_NEW_PM_NAME, "Model name", "Provide the new name", this)
+    }
+
+    override fun onNameDialogCompletePositive(dialog: GetNameDialogFragment, id: Int, string: String) {
+        createNewPM(string)
+    }
+
+    override fun onNameDialogCompleteNegative(dialog: GetNameDialogFragment, id: Int) {
+        // ignore
+    }
+
+    internal fun createNewPM(name: String) {
+
+        val model = RootDrawableProcessModel(UUID.randomUUID(), name, ArrayList())
+        val uri: Uri
+        try {
+            uri = ProcessModelProvider.newProcessModel(activity, model)
+        } catch (e: IOException) {
+            throw RuntimeException(e)
+        }
+
+        val editIntent = Intent(activity, PMEditor::class.java)
+        editIntent.data = ContentUris.withAppendedId(ProcessModels.CONTENT_ID_STREAM_BASE, ContentUris.parseId(uri))
+        startActivity(editIntent)
+    }
+
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
+        return CursorLoader(activity!!, ProcessModelProvider.ProcessModels.CONTENT_ID_URI_BASE,
+                            arrayOf(BaseColumns._ID, ProcessModels.COLUMN_NAME),
+                            XmlBaseColumns.COLUMN_SYNCSTATE + " IS NULL OR ( " + XmlBaseColumns.COLUMN_SYNCSTATE + " != " + RemoteXmlSyncAdapter.SYNC_DELETE_ON_SERVER + " AND " + XmlBaseColumns.COLUMN_SYNCSTATE + " != " + RemoteXmlSyncAdapter.SYNC_NEWDETAILSPENDING + " )",
+                            null, null)
+    }
+
+    override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
+        if (data != null) {
+            listAdapter!!.changeCursor(data)
+        }
+    }
+
+    override fun onLoaderReset(loader: Loader<Cursor>) {
+        listAdapter!!.changeCursor(null)
+    }
+
+    companion object {
+
+        private val LOADERID = 3
+
+        private val REQUEST_IMPORT = 31
+
+        private val TAG = ProcessModelListFragment::class.java.simpleName
+
+        private val DLG_NEW_PM_NAME = 2
+    }
+}
---
+++ PMEditor/src/main/java/nl/adaptivity/process/ui/model/ProcessModelListOuterFragment.kt
@@ -0,0 +1,159 @@
+/*
+ * Copyright (c) 2018.
+ *
+ * This file is part of ProcessManager.
+ *
+ * ProcessManager is free software: you can redistribute it and/or modify it under the terms of version 3 of the
+ * GNU Lesser General Public License as published by the Free Software Foundation.
+ *
+ * ProcessManager is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with ProcessManager.  If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
+
+package nl.adaptivity.process.ui.model
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import nl.adaptivity.android.util.MasterDetailOuterFragment
+import nl.adaptivity.android.util.MasterListFragment.ListCallbacks
+import nl.adaptivity.process.editor.android.R
+import nl.adaptivity.process.ui.ProcessSyncManager
+import nl.adaptivity.process.ui.model.ProcessModelDetailFragment.ProcessModelDetailFragmentCallbacks
+import nl.adaptivity.sync.SyncManager
+
+
+/**
+ * An activity representing a list of ProcessModels. This activity has different
+ * presentations for handset and tablet-size devices. On handsets, the activity
+ * presents a list of items, which when touched, lead to a
+ * [ProcessModelDetailActivity] representing item details. On tablets, the
+ * activity presents the list of items and item details side-by-side using two
+ * vertical panes.
+ *
+ *
+ * The activity makes heavy use of fragments. The list of items is a
+ * [ProcessModelListFragment] and the item details (if present) is a
+ * [ProcessModelDetailFragment].
+ *
+ *
+ * This activity also implements the required
+ * [ListCallbacks] interface to listen for item
+ * selections.
+ */
+class ProcessModelListOuterFragment : MasterDetailOuterFragment(R.layout.outer_processmodel_list,
+                                                                R.id.processmodel_list_container,
+                                                                R.id.processmodel_detail_container), ListCallbacks<SyncManager>, ProcessModelDetailFragmentCallbacks {
+
+  private var mCallbacks: ProcessModelListCallbacks? = null
+
+    interface ProcessModelListCallbacks {
+
+        val syncManager: ProcessSyncManager
+
+        fun requestSyncProcessModelList(immediate: Boolean, minAge: Long)
+
+        fun onInstantiateModel(id: Long, suggestedName: String)
+    }
+
+    public override fun createListFragment(args: Bundle?): ProcessModelListFragment {
+        val listFragment = ProcessModelListFragment()
+        if (args != null) {
+            listFragment.arguments = args
+        }
+        return listFragment
+    }
+
+    override fun onAttach(activity: Activity?) {
+        super.onAttach(activity)
+        if (activity is ProcessModelListCallbacks) {
+            mCallbacks = activity
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (mCallbacks != null) {
+            mCallbacks!!.requestSyncProcessModelList(true, ProcessSyncManager.DEFAULT_MIN_AGE)
+        }
+    }
+
+    /**
+     * Callback method from [ListCallbacks] indicating
+     * that the item with the given ID was selected.
+     */
+    override fun onProcessModelSelected(processModelId: Long) {
+        if (isTwoPane) {
+            if (processModelId >= 0) {
+                // In two-pane mode, show the detail view in this activity by
+                // adding or replacing the detail fragment using a
+                // fragment transaction.
+                val arguments = Bundle()
+                arguments.putLong(ProcessModelDetailFragment.ARG_ITEM_ID, processModelId)
+                val fragment = ProcessModelDetailFragment()
+                fragment.arguments = arguments
+                childFragmentManager.beginTransaction()
+                        .replace(R.id.processmodel_detail_container, fragment)
+                        .commit()
+            } else {
+                val frag = childFragmentManager.findFragmentById(R.id.processmodel_detail_container)
+                if (frag != null) {
+                    fragmentManager!!.beginTransaction()
+                            .remove(frag)
+                            .commit()
+                }
+            }
+
+        } else {
+            if (processModelId >= 0) {
+                // In single-pane mode, simply start the detail activity
+                // for the selected item ID.
+                val detailIntent = Intent(activity, ProcessModelDetailActivity::class.java)
+                detailIntent.putExtra(ProcessModelDetailFragment.ARG_ITEM_ID, processModelId)
+                startActivity(detailIntent)
+            }
+        }
+    }
+
+    override fun getSyncManager(): ProcessSyncManager? {
+        return mCallbacks!!.syncManager
+    }
+
+    override fun createDetailFragment(itemId: Long): ProcessModelDetailFragment {
+        val fragment = ProcessModelDetailFragment()
+        val arguments = Bundle()
+        arguments.putLong(ProcessModelDetailFragment.ARG_ITEM_ID, itemId)
+        fragment.arguments = arguments
+        return fragment
+    }
+
+    override fun getDetailIntent(itemId: Long): Intent {
+        val detailIntent = Intent(activity, ProcessModelDetailActivity::class.java)
+        detailIntent.putExtra(ProcessModelDetailFragment.ARG_ITEM_ID, itemId)
+        return detailIntent
+    }
+
+    override fun getTitle(context: Context): CharSequence {
+        return context.getString(R.string.title_processmodel_list)
+    }
+
+    override fun onInstantiateModel(modelId: Long, suggestedName: String) {
+        mCallbacks!!.onInstantiateModel(modelId, suggestedName)
+    }
+
+    companion object {
+
+        fun newInstance(modelId: Long): ProcessModelListOuterFragment {
+            val result = ProcessModelListOuterFragment()
+            if (modelId != 0L) {
+                result.arguments = MasterDetailOuterFragment.addArgs(null, modelId)
+            }
+            return result
+        }
+    }
+}
