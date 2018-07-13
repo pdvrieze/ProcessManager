092eb971a3a245d5e6e1f8eaf98847dcf7c2a48f
Paul de Vrieze <paul.devrieze@gmail.com>
2018-07-13 03:27:23 +0100

    Fix warnings and reindent.

--- ProcessEngine/core/src/test/kotlin/nl/adaptivity/process/engine/patterns/WCP8.kt
+++ ProcessEngine/core/src/test/kotlin/nl/adaptivity/process/engine/patterns/WCP8.kt
@@ -22,62 +22,63 @@ import nl.adaptivity.process.processModel.invoke
 import nl.adaptivity.spek.lenientFactory
 import org.jetbrains.spek.api.CreateWith
 import org.jetbrains.spek.api.dsl.it
-import org.jetbrains.spek.subject.dsl.SubjectDsl
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Tag
 
 @CreateWith(lenientFactory::class)
 @Tag("slow")
 class WCP8(maxValidTraces:Int, maxInvalidTraces: Int = maxValidTraces): ModelSpek(run {
-  val model = object : ConfigurableModel("WCP8") {
-    val start1 by startNode
-    val start2 by startNode
-    val ac1    by activity(start1)
-    val ac2    by activity(ac1)
+    val model = object : ConfigurableModel("WCP8") {
+        val start1 by startNode
+        val start2 by startNode
+        val ac1    by activity(start1)
+        val ac2    by activity(ac1)
 
-    val ac3 by activity(start2)
-    val ac4 by activity(ac3)
-    val join   by join(ac2, ac4) { min = 1; max = 1; isMultiMerge=true }
-    val ac5 by activity(join) { isMultiInstance=true }
-    val end    by endNode(ac5) { isMultiInstance=true }
-  }
+        val ac3 by activity(start2)
+        val ac4 by activity(ac3)
+        val join   by join(ac2, ac4) { min = 1; max = 1; isMultiMerge=true }
+        val ac5 by activity(join) { isMultiInstance=true }
+        val end    by endNode(ac5) { isMultiInstance=true }
+    }
 
-  val validTraces = with(model) { trace {
-    (start1 % start2) .. (
-      (((ac1 .. ac2)/(+ac3)) .. join[1] .. ((ac5[1] .. end[1])/(ac4 .. join[2] .. ac5[2] .. end[2]))) or
-      (((+ac1)/(ac3 .. ac4)) .. join[1] .. ((ac5[1] .. end[1])/(ac2 .. join[2] .. ac5[2] .. end[2])))
-    )
-  }}
+    val validTraces = with(model) { trace {
+        (start1 % start2) * (
+            ((((ac1 .. ac2) / (+ac3)) .. join[1]) * ((ac5[1] .. end[1]) / (ac4 .. join[2] .. ac5[2] .. end[2]))) or
+                ((((+ac1) / (ac3 .. ac4)) .. join[1]) * ((ac5[1] .. end[1]) / (ac2 .. join[2] .. ac5[2] .. end[2])))
+                            )
+    }}
 
-  val invalidTraces = with(model) { trace{
-    ac2 or ac4 or ac5 or end or join or
-      (((start1 % start2) or start1 or start2) ..(ac2 or ac4 or join or ac5 or end))
-  }}
+    val invalidTraces = with(model) { trace{
+        ac2 or ac4 or ac5 or end or join or
+            (((start1 % start2) or start1 or start2) * (ac2 or ac4 or join or ac5 or end))
+    }}
 
-  ModelData(model, validTraces, invalidTraces)
+    ModelData(model, validTraces, invalidTraces)
 }, {
-  val t: CustomDsl = this
-  group("When join is not multiInstance") {
-    val modifiedModel = model.update { join("join")!! { isMultiMerge = false } }
-    it("should not have a multiInstance join") {
-      assertFalse((modifiedModel.getNode("join") as? Join<*,*>)?.isMultiMerge ?: true)
+    val t: CustomDsl = this
+    group("When join is not multiInstance") {
+        val modifiedModel = model.update { join("join")!! { isMultiMerge = false } }
+        it("should not have a multiInstance join") {
+            assertFalse((modifiedModel.getNode("join") as? Join<*,*>)?.isMultiMerge ?: true)
+        }
+        for (trace in valid) {
+            t.testInvalidTrace(this, modifiedModel, model.owner, trace)
+        }
     }
-    for (trace in valid) {
-      t.testInvalidTrace(this, modifiedModel, model.owner, trace)
+    group("When ac3 is not multiInstance") {
+        val modifiedModel = model.update { activity("ac5")!! { isMultiInstance = false } }
+        for (trace in valid) {
+            t.testInvalidTrace(this, modifiedModel, model.owner, trace)
+        }
     }
-  }
-  group("When ac3 is not multiInstance") {
-    val modifiedModel = model.update { activity("ac5")!! { isMultiInstance = false } }
-    for (trace in valid) {
-      t.testInvalidTrace(this, modifiedModel, model.owner, trace)
+    group("When end is not multiInstance") {
+        val modifiedModel = model.update { endNode("end")!! { isMultiInstance = false } }
+        for (trace in valid) {
+            t.testInvalidTrace(this, modifiedModel, model.owner, trace)
+        }
     }
-  }
-  group("When end is not multiInstance") {
-    val modifiedModel = model.update { endNode("end")!! { isMultiInstance = false } }
-    for (trace in valid) {
-      t.testInvalidTrace(this, modifiedModel, model.owner, trace)
-    }
-  }
 }, maxValidTraces, maxInvalidTraces) {
-  constructor() : this(Int.MAX_VALUE)
+    // Used when instantiating as an independent test
+    @Suppress("unused")
+    constructor() : this(Int.MAX_VALUE)
 }
