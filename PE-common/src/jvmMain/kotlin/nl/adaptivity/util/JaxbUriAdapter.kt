8dcbb2055342b8099caa29335bdeeed4ff4fe550
Paul de Vrieze <paul.devrieze@gmail.com>
2019-03-18 16:00:52 +0000

    Convert to Kotlin.

--- PE-common/src/jvmMain/kotlin/nl/adaptivity/util/JaxbUriAdapter.kt
+++ PE-common/src/jvmMain/kotlin/nl/adaptivity/util/JaxbUriAdapter.kt
@@ -14,25 +14,21 @@
  * see <http://www.gnu.org/licenses/>.
  */
 
-package nl.adaptivity.util;
+package nl.adaptivity.util
 
-import org.jetbrains.annotations.NotNull;
+import javax.xml.bind.annotation.adapters.XmlAdapter
 
-import javax.xml.bind.annotation.adapters.XmlAdapter;
-
-import java.net.URI;
+import java.net.URI
 
 
-public class JaxbUriAdapter extends XmlAdapter<String, URI> {
+class JaxbUriAdapter : XmlAdapter<String, URI>() {
 
-  @Override
-  public URI unmarshal(@NotNull final String v) throws Exception {
-    return URI.create(v);
-  }
+    override fun unmarshal(v: String): URI {
+        return URI.create(v)
+    }
 
-  @Override
-  public String marshal(@NotNull final URI v) throws Exception {
-    return v.toString();
-  }
+    override fun marshal(v: URI): String {
+        return v.toString()
+    }
 
 }
