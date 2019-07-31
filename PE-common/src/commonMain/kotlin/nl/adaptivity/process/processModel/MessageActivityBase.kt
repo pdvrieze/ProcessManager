b6efccf7da26ba5a3c9f313a81539b9a510ff767
Paul de Vrieze <paul.devrieze@gmail.com>
2019-07-31 15:38:36 +0200

    Create subclasses of ActivityBase for later delegation.

---
+++ PE-common/src/commonMain/kotlin/nl/adaptivity/process/processModel/MessageActivityBase.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2019.
+ *
+ * This file is part of ProcessManager.
+ *
+ * ProcessManager is free software: you can redistribute it and/or modify it under the terms of version 3 of the
+ * GNU Lesser General Public License as published by the Free Software Foundation.
+ *
+ * ProcessManager is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with ProcessManager.  If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
+
+package nl.adaptivity.process.processModel
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+abstract class MessageActivityBase : ActivityBase, MessageActivity {
+
+    constructor(builder: MessageActivity.Builder, buildHelper: ProcessModel.BuildHelper<*, *, *, *>) :
+        super(builder, buildHelper)
+}
