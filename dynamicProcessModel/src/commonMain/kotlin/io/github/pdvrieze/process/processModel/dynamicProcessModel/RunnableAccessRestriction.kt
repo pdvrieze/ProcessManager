40e325d7e4fc684c1cc376974b5151e6c33b4768
Paul de Vrieze <paul.devrieze@gmail.com>
2023-03-13 12:33:23 +0000

    Finish implementing binding of duty/separation of duty

--- dynamicProcessModel/src/commonMain/kotlin/io/github/pdvrieze/process/processModel/dynamicProcessModel/RunnableAccessRestriction.kt
+++ dynamicProcessModel/src/commonMain/kotlin/io/github/pdvrieze/process/processModel/dynamicProcessModel/RunnableAccessRestriction.kt
@@ -83,15 +83,17 @@ class PrincipalRestriction(val allowedPrincipals: Set<String>) : RunnableAccessR
 class BindingOfDutyRestriction(val referenceNode: Identified): RunnableAccessRestriction() {
     override fun hasAccess(context: ActivityInstanceContext?, principal: PrincipalCompat): Boolean {
         val refInst: IProcessNodeInstance = context?.processContext?.instancesForName(referenceNode)
-            ?.firstOrNull { it.state == NodeInstanceState.Complete } ?: return false
-        val referencePrincipal = refInst
-
-        TODO("not implemented")
+            ?.singleOrNull { it.state == NodeInstanceState.Complete } ?: return false
+        val referencePrincipal = refInst.assignedUser
+        return principal.name == referencePrincipal?.name
     }
 }
 
 class SeparationOfDutyRestriction(val referenceNode: Identified) : RunnableAccessRestriction() {
     override fun hasAccess(context: ActivityInstanceContext?, principal: PrincipalCompat): Boolean {
-        TODO("not implemented")
+        val refInsts: List<IProcessNodeInstance> = context?.processContext?.instancesForName(referenceNode)
+            ?.filter { it.state == NodeInstanceState.Complete } ?: emptyList()
+
+        return refInsts.none { it.assignedUser?.name == principal.name }
     }
 }
